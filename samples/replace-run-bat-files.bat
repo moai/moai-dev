5c792b74940a06e0737b5d36c6ecfa58c1c60db9
joshlytle <josh@ziplinegames.com>
2011-04-06 13:11:46 -0700

    added copyright text to head of file

--- samples/replace-run-bat-files.bat
+++ samples/replace-run-bat-files.bat
@@ -1,3 +1,9 @@
+::----------------------------------------------------------------::
+:: Copyright (c) 2010-2011 Zipline Games, Inc.
+:: All Rights Reserved.
+:: http://getmoai.com
+::----------------------------------------------------------------::
+
 @echo off
 if not exist run.bat (
 	echo.
