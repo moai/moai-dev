224d4ae07466215ff99725298ad5afea43479149
joshlytle <josh@ziplinegames.com>
2011-04-08 17:23:59 -0700

    improved installer

--- nsis/make_installer.bat
+++ nsis/make_installer.bat
@@ -12,6 +12,7 @@ xcopy /cery ..\vs2008\bin\Win32\release\moai.exe release\bin\
 xcopy /cery ..\vs2008\bin\Win32\release\lua-5.1.3.dll release\bin\
 xcopy /cery ..\vs2008\bin\Win32\release\lua-5.1.3.dll.lib release\bin\
 xcopy /cery ..\vs2008\bin\Win32\release\glut32.dll release\bin\
+pause
 
 xcopy /cery ..\doxygen\html-lua\html release\docs\html\
 xcopy /cery ..\samples\basics release\samples\lua\
@@ -20,7 +21,7 @@ xcopy /cery ..\samples\config release\samples\config\
 xcopy /cery ..\src\aku\AKUGlut.* release\samples\src\aku\
 
 xcopy /cery ..\src\aku\AKU.h release\include\aku\
-
+ 
 ::read version from text file
 pushd ..
 set /a count=0
