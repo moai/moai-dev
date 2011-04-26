46a4ee15c362ae54155286ef4fd4cfa13f13c4be
Patrick Meehan <patrick@ziplinegames.com>
2011-04-25 23:38:12 -0700

    fixed crash bug on vertex format buffering; fixed index problem in gfxQuadDeck2D

--- samples/basics/anim-basic/main.lua
+++ samples/basics/anim-basic/main.lua
@@ -8,7 +8,7 @@ MOAISim.openWindow ( "test", 320, 480 )
 
 viewport = MOAIViewport.new ()
 viewport:setSize ( 320, 480 )
-viewport:setScale ( 320, 480 )
+viewport:setScale ( 320, -480 )
 
 layer = MOAILayer2D.new ()
 layer:setViewport ( viewport )
@@ -17,6 +17,7 @@ MOAISim.pushRenderPass ( layer )
 gfxQuad = MOAIGfxQuad2D.new ()
 gfxQuad:setTexture ( "cathead.png" )
 gfxQuad:setRect ( -64, -64, 64, 64 )
+gfxQuad:setUVRect ( 0, 0, 1, 1 )
 
 prop = MOAIProp2D.new ()
 prop:setDeck ( gfxQuad )
