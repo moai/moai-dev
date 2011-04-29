72847434872e974ab2c87b1de75c656b1a780566
Patrick Meehan <patrick@ziplinegames.com>
2011-04-29 16:06:32 -0700

    fixed compiler warning in MOAITimer

--- src/moaicore/MOAITimer.cpp
+++ src/moaicore/MOAITimer.cpp
@@ -296,7 +296,7 @@ void MOAITimer::GenerateCallbacks ( float t0, float t1, bool end ) {
 			}
 			else {
 			
-				for ( ; keyID != -1; --keyID ) {
+				for ( ; ( int )keyID >= -1; --keyID ) {
 					USAnimKey& key = ( *this->mCurve )[ keyID ];
 				
 					if (( end && ( key.mTime <= t1 )) || (( key.mTime <= t0 ) && ( key.mTime > t1 ))) {
