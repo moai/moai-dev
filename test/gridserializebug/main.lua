457712eaabcc697d3f95c1e2ca60610a415e686d
Jon Winkler <jon@ziplinegames.com>
2011-05-05 12:01:21 -0700

    added test files

---
+++ test/gridserializebug/main.lua
@@ -0,0 +1,32 @@
+----------------------------------------------------------------
+-- Copyright (c) 2010-2011 Zipline Games, Inc. 
+-- All Rights Reserved. 
+-- http://getmoai.com
+----------------------------------------------------------------
+
+SCREEN_SCALE = 1/16
+
+camera = MOAITransform.new ()
+
+viewport = MOAIViewport.new ()
+viewport:setSize ( 960, 640 )
+viewport:setScale ( 960*SCREEN_SCALE, 640*SCREEN_SCALE )
+
+layer = MOAILayer2D.new ()
+layer:setViewport ( viewport )
+layer:setCamera ( camera )
+MOAISim.pushRenderPass ( layer )
+
+MOAISim.openWindow ( "test", 960, 640 )
+
+tileDeck = MOAITileDeck2D.new ()
+tileDeck:setTexture ( "building_tile_big.png" )
+tileDeck:setSize ( 8, 16 )
+
+grid = dofile ( "testMOAIbig_block_2grid.lua" )
+
+prop = MOAIProp2D.new ()
+prop:setDeck ( tileDeck )
+prop:setGrid ( grid )
+prop:setLoc ( -960 * SCREEN_SCALE, -320 * SCREEN_SCALE )
+layer:insertProp ( prop )
