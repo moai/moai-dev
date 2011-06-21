c9982186a3a520467cb89a364a7e02247d10fa4d
Josh Lytle <josh@ziplinegames.com>
2011-06-21 14:40:07 -0700

    finally fixed android build on mac

--- eclipse/pack-lua.sh
+++ eclipse/pack-lua.sh
@@ -12,15 +12,21 @@ cd ..
 rm -rf eclipse/lua-prep
 
 cp -r "$LUA_FOLDER" "eclipse/lua-prep"
+rm -f eclipse/lua-prep/.DS_Store
 
 # adapt your project to use the below command, and it will filter out svn directories
 #find "$LUA_FOLDER" ! -name "/.svn/" | cpio -admvp "eclipse/lua-prep"
+
+echo ----------------------------------------------------------------
+echo Packaging Resources
+echo ----------------------------------------------------------------
 
 cd eclipse/lua-prep
 rm -f ../android-project/res/raw/lua
 zip -9 -r ../android-project/res/raw/lua . *.*
 
-cd ../../eclipse/android-project/res/raw
-mv lua.zip lua
+cd ..
+rm -rf lua-prep
 
-#rm -rf eclipse/lua-prep
+cd ../eclipse/android-project/res/raw
+mv lua.zip lua
