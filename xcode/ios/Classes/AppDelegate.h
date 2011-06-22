62c96327a95350b9ce3bd8304c358b40f30d16a1
Josh Lytle <josh@ziplinegames.com>
2011-06-21 20:55:43 -0700

    adding new folders

---
+++ xcode/ios/Classes/AppDelegate.h
@@ -0,0 +1,24 @@
+//----------------------------------------------------------------//
+// Copyright (c) 2010-2011 Zipline Games, Inc. 
+// All Rights Reserved. 
+// http://getmoai.com
+//----------------------------------------------------------------//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import "RefPtr.h"
+
+@class MoaiView;
+
+//================================================================//
+// AppDelegate
+//================================================================//
+@interface AppDelegate : NSObject < UIApplicationDelegate > {
+@private
+
+	IBOutlet MoaiView*	mMoaiView;
+	IBOutlet UIWindow*	mWindow;
+}
+
+@end
---
+++ xcode/ios/Classes/MoaiVC.mm
@@ -0,0 +1,34 @@
+//----------------------------------------------------------------//
+// Copyright (c) 2010-2011 Zipline Games, Inc. 
+// All Rights Reserved. 
+// http://getmoai.com
+//----------------------------------------------------------------//
+
+#import "MoaiVC.h"
+
+//================================================================//
+// MoaiVC
+//================================================================//
+@implementation MoaiVC
+
+	//----------------------------------------------------------------//
+	- ( id ) init {
+	
+		self = [ super init ];
+		if ( self ) {
+		
+		}
+		return self;
+	}
+
+	//----------------------------------------------------------------//
+	- ( BOOL ) shouldAutorotateToInterfaceOrientation :( UIInterfaceOrientation )interfaceOrientation {
+		
+		if ( interfaceOrientation == UIInterfaceOrientationLandscapeRight ) {
+			return true;
+		}
+
+		return false;
+	}
+	
+@end
