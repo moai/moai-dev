01270d1465e17f3f6f5366132cf6cd03335780a9
joshlytle <josh@ziplinegames.com>
2011-06-28 10:28:28 -0700

    removing files that should be copied into the sdk, reintroducing the gitignore file

--- distribute/make-sdk.sh
+++ distribute/make-sdk.sh
@@ -19,3 +19,16 @@ cp -R ../../src/samples sdk/samples/hosts/src
 
 # android host
 cp -R ../../eclipse/android-project sdk/samples/hosts/eclipse/android-project/
+
+# ios host
+cp -R ../../xcode/ios/Classes sdk/samples/hosts/xcode/ios/Classes
+cp ../../xcode/ios/Entitlements.plist sdk/samples/hosts/xcode/ios/Entitlements.plist
+cp ../../xcode/ios/Icon.png sdk/samples/hosts/xcode/ios/Icon.png
+cp ../../xcode/ios/Icon@2x.png sdk/samples/hosts/xcode/ios/Icon@2x.png
+cp ../../xcode/ios/Icon-72.png sdk/samples/hosts/xcode/ios/Icon-72.png
+cp ../../xcode/ios/Icon-Small.png sdk/samples/hosts/xcode/ios/Icon-Small.png
+cp ../../xcode/ios/Icon-Small@2x.png sdk/samples/hosts/xcode/ios/Icon-Small@2x.png
+cp ../../xcode/ios/Icon-Small-50.png sdk/samples/hosts/xcode/ios/Icon-Small-50.png
+cp ../../xcode/ios/Info.plist sdk/samples/hosts/xcode/ios/Info.plist
+cp ../../xcode/ios/MainWindow-iPad.xib sdk/samples/hosts/xcode/ios/MainWindow-iPad.xib
+cp ../../xcode/ios/MainWindow-iPhone.xib sdk/samples/hosts/xcode/ios/MainWindow-iPhone.xib
