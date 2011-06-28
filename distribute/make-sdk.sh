1bf92ad315d9205d25b2e54d3d58d3c1550afa88
joshlytle <josh@ziplinegames.com>
2011-06-27 17:04:50 -0700

    expanding on the make sdk script

--- distribute/make-sdk.sh
+++ distribute/make-sdk.sh
@@ -6,6 +6,7 @@
 
 ./clean-sdk.sh
 
+# general files
 cp -R ../../3rdparty/glut-3.7.6-bin sdk/3rdparty/glut-3.7.6/
 
 cp -R ../../samples/basics sdk/samples/lua/
@@ -13,3 +14,8 @@ cp -R ../../samples/hello-moai sdk/samples/lua/hello-moai/
 cp -R ../../samples/config sdk/samples/lua/config/
 
 cp -R ../../src/aku/*.h sdk/include/aku/
+
+cp -R ../../src/samples sdk/samples/hosts/src
+
+# android host
+cp -R ../../eclipse/android-project sdk/samples/hosts/eclipse/android-project/
