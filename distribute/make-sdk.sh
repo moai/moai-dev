cfbe67e14f4d937c99a4ff4ceb5264c91daff274
Josh Lytle <josh@ziplinegames.com>
2011-06-28 10:50:32 -0700

    fixing the make-sdk script

--- distribute/make-sdk.sh
+++ distribute/make-sdk.sh
@@ -7,28 +7,41 @@
 ./clean-sdk.sh
 
 # general files
-cp -R ../../3rdparty/glut-3.7.6-bin sdk/3rdparty/glut-3.7.6/
+mkdir -p sdk/3rdparty/glut-3.7.6
+cp -R ../3rdparty/glut-3.7.6-bin sdk/3rdparty/glut-3.7.6
 
-cp -R ../../samples/basics sdk/samples/lua/
-cp -R ../../samples/hello-moai sdk/samples/lua/hello-moai/
-cp -R ../../samples/config sdk/samples/lua/config/
+mkdir -p sdk/samples
+cp -R ../samples/basics sdk/samples
 
-cp -R ../../src/aku/*.h sdk/include/aku/
+mkdir -p sdk/samples/hello-moai
+cp -R ../samples/hello-moai sdk/samples/hello-moai
 
-cp -R ../../src/samples sdk/samples/hosts/src
+mkdir -p sdk/samples/config
+cp -R ../samples/config sdk/samples/config
+
+mkdir -p sdk/include/aku
+cp -R ../src/aku/*.h sdk/include/aku
+
+mkdir -p sdk/hosts/src
+cp -R ../src/hosts sdk/hosts/src
 
 # android host
-cp -R ../../eclipse/android-project sdk/samples/hosts/eclipse/android-project/
+mkdir -p sdk/hosts/eclipse
+cp -R ../eclipse sdk/hosts/eclipse/android-project
 
 # ios host
-cp -R ../../xcode/ios/Classes sdk/samples/hosts/xcode/ios/Classes
-cp ../../xcode/ios/Entitlements.plist sdk/samples/hosts/xcode/ios/Entitlements.plist
-cp ../../xcode/ios/Icon.png sdk/samples/hosts/xcode/ios/Icon.png
-cp ../../xcode/ios/Icon@2x.png sdk/samples/hosts/xcode/ios/Icon@2x.png
-cp ../../xcode/ios/Icon-72.png sdk/samples/hosts/xcode/ios/Icon-72.png
-cp ../../xcode/ios/Icon-Small.png sdk/samples/hosts/xcode/ios/Icon-Small.png
-cp ../../xcode/ios/Icon-Small@2x.png sdk/samples/hosts/xcode/ios/Icon-Small@2x.png
-cp ../../xcode/ios/Icon-Small-50.png sdk/samples/hosts/xcode/ios/Icon-Small-50.png
-cp ../../xcode/ios/Info.plist sdk/samples/hosts/xcode/ios/Info.plist
-cp ../../xcode/ios/MainWindow-iPad.xib sdk/samples/hosts/xcode/ios/MainWindow-iPad.xib
-cp ../../xcode/ios/MainWindow-iPhone.xib sdk/samples/hosts/xcode/ios/MainWindow-iPhone.xib
+mkdir -p sdk/hosts/xcode-ios/Classes
+cp -R ../xcode/ios/Classes sdk/hosts/xcode-ios/Classes
+
+cp ../xcode/ios/Entitlements.plist sdk/hosts/xcode-ios/Entitlements.plist
+cp ../xcode/ios/Icon.png sdk/hosts/xcode-ios/Icon.png
+cp ../xcode/ios/Icon@2x.png sdk/hosts/xcode-ios/Icon@2x.png
+cp ../xcode/ios/Icon-72.png sdk/hosts/xcode-ios/Icon-72.png
+cp ../xcode/ios/Icon-Small.png sdk/hosts/xcode-ios/Icon-Small.png
+cp ../xcode/ios/Icon-Small@2x.png sdk/hosts/xcode-ios/Icon-Small@2x.png
+cp ../xcode/ios/Icon-Small-50.png sdk/hosts/xcode-ios/Icon-Small-50.png
+cp ../xcode/ios/Info.plist sdk/hosts/xcode-ios/Info.plist
+cp ../xcode/ios/MainWindow-iPad.xib sdk/hosts/xcode-ios/MainWindow-iPad.xib
+cp ../xcode/ios/MainWindow-iPhone.xib sdk/hosts/xcode-ios/MainWindow-iPhone.xib
+
+# binaries
