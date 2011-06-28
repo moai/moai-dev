cb1c48b310f6cd93e5d6efdbb88b9f44617f5358
Josh Lytle <josh@ziplinegames.com>
2011-06-28 10:15:55 -0700

    changed the name of iphone to ios in the build

--- xcode/libmoai/clean-standalone-bin.sh
+++ xcode/libmoai/clean-standalone-bin.sh
@@ -6,15 +6,15 @@
 
 #!/bin/bash
 
-xcodebuild -nodistribute -configuration Release -target libmoai-iphone-3rdparty -sdk iphonesimulator4.3 clean
-xcodebuild -nodistribute -configuration Release -target libmoai-iphone -sdk iphonesimulator4.3 clean
-xcodebuild -nodistribute -configuration Release -target libmoai-iphone-luaext -sdk iphonesimulator4.3 clean
-xcodebuild -nodistribute -configuration Release -target libmoai-iphone-untz -sdk iphonesimulator4.3 clean
+xcodebuild -nodistribute -configuration Release -target libmoai-ios-3rdparty -sdk iphonesimulator4.3 clean
+xcodebuild -nodistribute -configuration Release -target libmoai-ios -sdk iphonesimulator4.3 clean
+xcodebuild -nodistribute -configuration Release -target libmoai-ios-luaext -sdk iphonesimulator4.3 clean
+xcodebuild -nodistribute -configuration Release -target libmoai-ios-untz -sdk iphonesimulator4.3 clean
 
-xcodebuild -nodistribute -configuration Release -target libmoai-iphone-3rdparty -sdk iphoneos4.3 clean
-xcodebuild -nodistribute -configuration Release -target libmoai-iphone -sdk iphoneos4.3 clean
-xcodebuild -nodistribute -configuration Release -target libmoai-iphone-luaext -sdk iphoneos4.3 clean
-xcodebuild -nodistribute -configuration Release -target libmoai-iphone-untz -sdk iphoneos4.3 clean
+xcodebuild -nodistribute -configuration Release -target libmoai-ios-3rdparty -sdk iphoneos4.3 clean
+xcodebuild -nodistribute -configuration Release -target libmoai-ios -sdk iphoneos4.3 clean
+xcodebuild -nodistribute -configuration Release -target libmoai-ios-luaext -sdk iphoneos4.3 clean
+xcodebuild -nodistribute -configuration Release -target libmoai-ios-untz -sdk iphoneos4.3 clean
 
 xcodebuild -nodistribute -configuration Release -target libmoai-osx-3rdparty -sdk macosx10.6 clean
 xcodebuild -nodistribute -configuration Release -target libmoai-osx -sdk macosx10.6 clean
