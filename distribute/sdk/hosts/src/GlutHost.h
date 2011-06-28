c6e2a21a600fb34e058c2658e28612e854ef9ae9
Patrick Meehan <patrick@ziplinegames.com>
2011-06-27 17:00:44 -0700

    moved 'hosts' folder to root of sdk; renamed 'samples' to 'hosts' in main src folder

---
+++ distribute/sdk/hosts/src/GlutHost.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2010-2011 Zipline Games, Inc. All Rights Reserved.
+// http://getmoai.com
+
+#ifndef	GLUTHOST
+#define	GLUTHOST
+
+#include <aku/AKU.h>
+
+//----------------------------------------------------------------//
+int GlutHost ( int argc, char** argv );
+
+#endif
