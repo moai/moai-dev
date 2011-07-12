424d904fefbb4bd3950fe9e9d98a568324d15f52
Josh Lytle <josh@ziplinegames.com>
2011-07-11 17:09:18 -0700

    fixing script

--- xcode/ios/package.sh
+++ xcode/ios/package.sh
@@ -6,12 +6,22 @@ fi
 
 read LUA_FOLDER < moai-target
 
+chmod -R 777 ./lua
+
 rm -rf lua
 mkdir -p lua
 
 cp -r ../../$LUA_FOLDER/* lua
 
-chmod -R 555 lua
+chmod -R 555 ./lua/*
+
+if [ -f ./lua/run.bat ]; then 
+	chmod 777 ./lua/run.bat
+fi
+
+if [ -f ./lua/run.sh ]; then 
+	chmod 777 ./lua/run.sh
+fi
 
 rm -f lua/run.bat
 rm -f lua/run.sh
