6a8c5504b39ea59269720deb26bb7514a72d200b
Josh Lytle <josh@ziplinegames.com>
2011-07-12 17:17:09 -0700

    improving ios package script so now it can package any number of folders specified (one per line) into the lua folder. this allows easy use of lua-modules in a separate folder

--- xcode/ios/package.sh
+++ xcode/ios/package.sh
@@ -4,24 +4,24 @@ if [ ! -f moai-target ]; then
 	cp mt.default moai-target
 fi
 
-read LUA_FOLDER < moai-target
-
 chmod -R 777 ./lua
-
 rm -rf lua
 mkdir -p lua
 
-cp -r ../../$LUA_FOLDER/* lua
+for i in $(cat moai-target); do 
 
-chmod -R 555 ./lua/*
-
-if [ -f ./lua/run.bat ]; then 
-	chmod 777 ./lua/run.bat
-fi
-
-if [ -f ./lua/run.sh ]; then 
-	chmod 777 ./lua/run.sh
-fi
-
-rm -f lua/run.bat
-rm -f lua/run.sh
+	cp -r ../../$i/* lua
+	
+	chmod -R 555 ./lua/*
+	
+	if [ -f ./lua/run.bat ]; then 
+		chmod 777 ./lua/run.bat
+	fi
+	
+	if [ -f ./lua/run.sh ]; then 
+		chmod 777 ./lua/run.sh
+	fi
+	
+	rm -f lua/run.bat
+	rm -f lua/run.sh
+done
