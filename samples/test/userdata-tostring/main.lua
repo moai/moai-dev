2c305047c44b4627869d637da6f53316239c223c
Patrick Meehan <patrick@ziplinegames.com>
2011-10-02 23:24:52 -0700

    added demo of new _tostring in samples/test/userdata-tostring

---
+++ samples/test/userdata-tostring/main.lua
@@ -0,0 +1,15 @@
+----------------------------------------------------------------
+-- Copyright (c) 2010-2011 Zipline Games, Inc. 
+-- All Rights Reserved. 
+-- http://getmoai.com
+----------------------------------------------------------------
+
+print ( "hello, moai!" )
+
+print ( MOAILayer2D.new ())
+print ( MOAIViewport.new ())
+print ( MOAITransform.new ())
+print ( MOAIProp2D.new ())
+
+
+
