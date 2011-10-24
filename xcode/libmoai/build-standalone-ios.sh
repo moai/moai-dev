edbff440a719f5b4f80f7e11a581ce9e3262a207
Patrick Meehan <patrick@ziplinegames.com>
2011-10-23 20:05:19 -0700

    removed 'nodistribute' from iOS standalong build (remove after fixing build server error)

--- xcode/libmoai/build-standalone-ios.sh
+++ xcode/libmoai/build-standalone-ios.sh
@@ -6,15 +6,15 @@
 
 #!/bin/bash
 
-xcodebuild -nodistribute -configuration Release -target libmoai-ios-3rdparty -sdk iphonesimulator4.3 build
-xcodebuild -nodistribute -configuration Release -target libmoai-ios -sdk iphonesimulator4.3 build
-xcodebuild -nodistribute -configuration Release -target libmoai-ios-luaext -sdk iphonesimulator4.3 build
-xcodebuild -nodistribute -configuration Release -target libmoai-ios-untz -sdk iphonesimulator4.3 build
+xcodebuild -configuration Release -target libmoai-ios-3rdparty -sdk iphonesimulator4.3 build
+xcodebuild -configuration Release -target libmoai-ios -sdk iphonesimulator4.3 build
+xcodebuild -configuration Release -target libmoai-ios-luaext -sdk iphonesimulator4.3 build
+xcodebuild -configuration Release -target libmoai-ios-untz -sdk iphonesimulator4.3 build
 
-xcodebuild -nodistribute -configuration Release -target libmoai-ios-3rdparty -sdk iphoneos4.3 build
-xcodebuild -nodistribute -configuration Release -target libmoai-ios -sdk iphoneos4.3 build
-xcodebuild -nodistribute -configuration Release -target libmoai-ios-luaext -sdk iphoneos4.3 build
-xcodebuild -nodistribute -configuration Release -target libmoai-ios-untz -sdk iphoneos4.3 build
+xcodebuild -configuration Release -target libmoai-ios-3rdparty -sdk iphoneos4.3 build
+xcodebuild -configuration Release -target libmoai-ios -sdk iphoneos4.3 build
+xcodebuild -configuration Release -target libmoai-ios-luaext -sdk iphoneos4.3 build
+xcodebuild -configuration Release -target libmoai-ios-untz -sdk iphoneos4.3 build
 
 cd build
 mkdir -p Release
