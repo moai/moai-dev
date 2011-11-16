0b1e79bed5f0e8bbe2a73009074d8fa39c1199f2
Josh Lytle <jlytle@distinctdev.com>
2011-11-16 11:02:23 -0800

    fixing the logcat calls

--- android/host-source/d.run.sh
+++ android/host-source/d.run.sh
@@ -196,7 +196,8 @@
 			ant clean
 			$install_cmd
 			adb shell am start -a android.intent.action.MAIN -n $package/$package.MoaiActivity
-			adb logcat -c MoaiLog:V AndroidRuntime:E *:S
+			adb logcat -c
+			adb logcat MoaiLog:V AndroidRuntime:E *:S
 		popd > /dev/null
 	fi
 	
