9152e41064881a8661a2b76fd88ca8324ee407cf
Sean B House <sean@ziplinegames.com>
2011-11-17 15:29:21 -0800

    Fix clean scripts and OSX host.

--- xcode/libmoai/clean-standalone-ios.sh
+++ xcode/libmoai/clean-standalone-ios.sh
@@ -12,11 +12,15 @@ xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspa
 xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-ios -sdk iphonesimulator clean OBJROOT=build SYMROOT=build
 xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-ios-luaext -sdk iphonesimulator clean OBJROOT=build SYMROOT=build
 xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-ios-untz -sdk iphonesimulator clean OBJROOT=build SYMROOT=build
+xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-ios-tapjoy -sdk iphonesimulator clean OBJROOT=build SYMROOT=build
+xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-ios-zipfs -sdk iphonesimulator clean OBJROOT=build SYMROOT=build
 
 xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-ios-3rdparty -sdk iphoneos clean OBJROOT=build SYMROOT=build
 xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-ios -sdk iphoneos clean OBJROOT=build SYMROOT=build
 xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-ios-luaext -sdk iphoneos clean OBJROOT=build SYMROOT=build
 xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-ios-untz -sdk iphoneos clean OBJROOT=build SYMROOT=build
+xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-ios-tapjoy -sdk iphoneos clean OBJROOT=build SYMROOT=build
+xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-ios-zipfs -sdk iphoneos clean OBJROOT=build SYMROOT=build
 
 rm -rf build/Release
 rm -rf build/Release-armv7
