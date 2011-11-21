562c115dbcc64edcb2273afc15c97cc6061850eb
Josh Lytle <jlytle@distinctdev.com>
2011-11-21 11:55:49 -0800

    integrating libogg and libvorbis to android build

---
+++ 3rdparty/libogg-1.2.2/include/ogg/config_types.h
@@ -0,0 +1,13 @@
+#ifndef __CONFIG_TYPES_H__
+#define __CONFIG_TYPES_H__
+#include <stdint.h>
+
+/* These would normally be filled in by configure, but we're just hardcoding
+ * them here. */
+typedef int16_t ogg_int16_t;
+typedef uint16_t ogg_uint16_t;
+typedef int32_t ogg_int32_t;
+typedef uint32_t ogg_uint32_t;
+typedef int64_t ogg_int64_t;
+
+#endif
