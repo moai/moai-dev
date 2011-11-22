aaa79a764ea8b67efde7aae48c2eb910192f7452
Robert Dalton <rdaltonjr@gmail.com>
2011-11-21 20:44:52 -0800

    enable ogg support for android. wav parsing fixes for android

--- 3rdparty/untz/src/native/android/WaveFile.h
+++ 3rdparty/untz/src/native/android/WaveFile.h
@@ -1,33 +1,60 @@
+//
+//  WaveFile.h
+//  Part of UNTZ
+//
+//  Created by Zach Saul (zach@retronyms.com) on 06/01/2011.
+//  Copyright 2011 Retronyms. All rights reserved.
+//
+
+#ifndef WAVEFILE_H_
+#define WAVEFILE_H_
+
 #include "RiffFile.h"
+
+
+enum 
+{
+	R_WAVEFORMAT_UNKNOWN = 0x00,
+	R_WAVEFORMAT_PCM = 0x01,
+	R_WAVEFORMAT_IEEE = 0x03,
+	R_WAVEFORMAT_ALAW = 0x06,
+	R_WAVEFORMAT_MULAW = 0x07,
+	R_WAVEFORMAT_EXTENSIBLE = 0xFE
+};
 
 typedef struct
 {
-   UInt16 compressionCode;
-   UInt16 numChannels;
-   UInt32 sampleRate;
-   UInt32 averageBytesPerSecond;
-   UInt16 bytesPerFrame;
-   UInt16 significantBitsPerSample;
+	UInt16 formatTag;
+	UInt16 numChannels;
+	UInt32 samplesPerSecond;
+	UInt32 averageBytesPerSecond;
+	UInt16 bytesPerFrame;
+	UInt16 bitsPerSample;
+	// -- 16 bytes
+	UInt16 cbSize;  // size of the extension (0 or 22)
+	// -- 18 bytes
+	UInt16 validBitsPerSample;
+	UInt32 channelMask;
+	UInt32 subFormat;
+	// -- 40 bytes
 } WaveHeader;
 
-class WaveFile
+class WaveFile : public RiffFile
 {
 public:
    WaveFile();
    ~WaveFile();
-   static WaveFile *create(const char *path);
-   UInt16 compressionCode();
-   UInt16 numChannels();
-   UInt32 sampleRate();
-   UInt32 averageBytesPerSecond();
-   UInt16 bytesPerFrame();
-   UInt16 significantBitsPerSample();
-   double numberOfFrames();
 
-   void rewind();
-   UInt32 readSamples(void *buf, UInt32 numBytes);
+   // RiffFile
+   virtual int open(const char* path);
 
-private:
-   RiffFile *mpRiff;
+   // WaveFile
+   WaveHeader getHeader() const;
+   UInt32 getNumberOfFrames();
+   void setPosition(UInt32 byteOffset);
+
+protected:
    WaveHeader mHeader;
 };
+
+#endif // WAVEFILE_H_
