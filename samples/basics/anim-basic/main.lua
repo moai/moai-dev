a78bc309b48a5221e589e2c289ce92196c8c41f6
Patrick Meehan <patrick@ziplinegames.com>
2011-12-31 18:44:44 -0800

    reverted samples/basics/anim-basic to 2D

--- samples/basics/anim-basic/main.lua
+++ samples/basics/anim-basic/main.lua
@@ -8,7 +8,7 @@ MOAISim.openWindow ( "test", 320, 480 )
 
 viewport = MOAIViewport.new ()
 viewport:setSize ( 320, 480 )
-viewport:setScale ( 320, 480 )
+viewport:setScale ( 320, -480 )
 
 layer = MOAILayer2D.new ()
 layer:setViewport ( viewport )
@@ -17,13 +17,10 @@ MOAISim.pushRenderPass ( layer )
 gfxQuad = MOAIGfxQuad2D.new ()
 gfxQuad:setTexture ( "cathead.png" )
 gfxQuad:setRect ( -64, -64, 64, 64 )
-gfxQuad:setUVRect ( 0, 1, 1, 0 )
+gfxQuad:setUVRect ( 0, 0, 1, 1 )
 
 prop = MOAIProp2D.new ()
 prop:setDeck ( gfxQuad )
 layer:insertProp ( prop )
 
-prop:moveRot ( 360, 180, 360, 3 )
-prop:moveLoc ( 0, 0, 128, 3 )
-
---camera = MOAICamera3D.new ()
+prop:moveRot ( 360, 1.5 )
