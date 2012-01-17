f47b81865b53f74d731aa0d1aca7792379a0f51f
Sean B House <sean@ziplinegames.com>
2012-01-16 18:02:13 -0800

    Fix osx script.

--- xcode/libmoai/build-standalone-osx.sh
+++ xcode/libmoai/build-standalone-osx.sh
@@ -8,8 +8,8 @@
 
 set -e
 
-xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-osx-3rdparty -sdk macosx build OBJROOT=build SYMROOT=build
 xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-osx -sdk macosx build OBJROOT=build SYMROOT=build
+xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-osx-3rdparty -sdk macosx build OBJROOT=build SYMROOT=build
 xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-osx-luaext -sdk macosx build OBJROOT=build SYMROOT=build
 xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-osx-untz -sdk macosx build OBJROOT=build SYMROOT=build
 xcodebuild -configuration Release -workspace libmoai.xcodeproj/project.xcworkspace -scheme libmoai-osx-zipfs -sdk macosx build OBJROOT=build SYMROOT=build
