ea1831b3ba4b525c8ddf223d6bb169aa35f8d9e0
Patrick Meehan <patrick@ziplinegames.com>
2012-02-13 11:51:44 -0800

    forgot to add MOAITextDesigner

---
+++ src/moaicore/MOAITextDesigner.cpp
@@ -0,0 +1,212 @@
+// Copyright (c) 2010-2011 Zipline Games, Inc. All Rights Reserved.
+// http://getmoai.com
+
+#include "pch.h"
+#include <contrib/utf8.h>
+#include <moaicore/MOAIFont.h>
+#include <moaicore/MOAITextDesigner.h>
+#include <moaicore/MOAITextStyle.h>
+#include <moaicore/MOAITextStyleMap.h>
+
+//================================================================//
+// MOAITextDesigner
+//================================================================//
+
+//----------------------------------------------------------------//
+//void MOAITextDesigner::FlushLine () {
+//
+//	if ( this->mLineCount >= this->mTotalLines ) return;
+//
+//	float x = this->mFrame.mXMin;
+//	float width = this->mFrame.Width ();
+//	
+//	float lineWidth = this->mRightToLeft ? -this->mLineXMax : this->mLineXMax;
+//	float lineHeight = this->mFont->GetLineSpacing () * this->mPoints * this->mLineSpacing;
+//
+//	switch ( this->mJustify ) {
+//		
+//		case CENTER_JUSTIFY:
+//			x = ( x + ( width * 0.5f )) - ( lineWidth * 0.5f );
+//			
+//		case LEFT_JUSTIFY:
+//			break;
+//
+//		case RIGHT_JUSTIFY:
+//			x = this->mFrame.mXMax - lineWidth;
+//	}
+//	
+//	MOAIAnimCurve* curve = 0;
+//	if ( this->mCurves ) {
+//		curve = this->mCurves [ this->mLineCount % this->mTotalCurves ];
+//	}
+//	
+//	if ( this->mRightToLeft ) {
+//		x -= this->mLineXMax;
+//	}
+//	
+//	for ( u32 i = this->mLineBottom; i < this->mLineTop; ++i ) {
+//		
+//		MOAITextSprite& sprite = ( *this->mLayout )[ i ];
+//		
+//		float yOff = 0.0f;
+//		if ( curve ) {
+//			yOff = curve->GetFloatValue ( sprite.mX / width );
+//		}
+//
+//		sprite.mX += x;
+//		sprite.mY = this->mPen.mY + yOff;
+//	}
+//	
+//	this->mLineBottom = this->mLineTop;
+//	this->mLineXMax = 0.0f;
+//	this->mPen.mY += lineHeight;
+//	this->mLineCount++;
+//	this->mGlyph = 0;
+//	
+//	// move token to origin
+//	u32 tokenTop = this->mLayout->GetTop ();
+//	for ( u32 i = this->mLineTop; i < tokenTop; ++i ) {
+//		
+//		MOAITextSprite& sprite = ( *this->mLayout )[ i ];
+//		sprite.mX -= this->mTokenXMin;
+//	}
+//
+//	this->mPen.mX -= this->mTokenXMin;
+//	this->mTokenXMax -= this->mTokenXMin;
+//	this->mTokenXMin = 0.0f;
+//}
+
+//----------------------------------------------------------------//
+//void MOAITextDesigner::FlushToken () {
+//	
+//	float width = this->mFrame.Width ();
+//	float tokenMax = this->mRightToLeft ? -this->mTokenXMax : this->mTokenXMax;
+//	
+//	if (( this->mLineXMax != 0.0f ) && ( tokenMax >= width )) {
+//		this->FlushLine ();
+//	}
+//	
+//	if ( this->mLineCount < this->mTotalLines ) {
+//		this->mLineTop = this->mLayout->GetTop ();
+//		this->mLineXMax = this->mTokenXMax;
+//	}
+//}
+
+//----------------------------------------------------------------//
+bool MOAITextDesigner::IsWhitespace ( u32 c ) {
+
+	if ( !c ) return true;
+	if ( c == ' ' ) return true;
+	if ( c == '\t' ) return true;
+	if ( c == '\n' ) return true;
+	
+	return false;
+}
+
+//----------------------------------------------------------------//
+void MOAITextDesigner::Layout ( cc8* str, MOAITextStyleMap& styleMap ) {
+	
+	this->mStr = str;
+	
+	this->mSpanIdx = 0;
+	this->mStyleMap = &styleMap;
+	this->mStyleSpan = &this->mStyleMap->Elem ( 0 );
+	this->mIdx = this->mStyleSpan->mBase;
+	
+	this->UpdateStyle ();
+	
+	//float xScaleAdvance = this->mRightToLeft ? -1.0f : 1.0f;
+	float xScaleAdvance = 1.0f;
+
+	USVec2D pen;
+	pen.Init ( 0.0f, 0.0f );
+	
+	float scale = 1.0f;
+	
+	bool inWhitespace = false;
+	bool gobbleWhitespace = true;
+	const MOAIGlyph* glyph = 0;
+	const MOAIGlyph* prevGlyph = 0;
+	
+	u32 spanIdx = 0;
+	u32 idx = 0;
+	
+	bool more = true;
+	while ( more ) {
+	
+		u32 c = this->NextChar ();
+		
+		//inWhitespace = this->IsWhitespace ( c );
+		
+		if ( c == 0 ) break;
+		putc ( c, stdout );
+		
+		prevGlyph = glyph;
+		glyph = this->mDeck->GetGlyph ( c );
+		if ( !glyph ) continue;
+		
+		// apply kerning
+		if ( prevGlyph ) {
+			MOAIKernVec kernVec = prevGlyph->GetKerning ( glyph->mCode );
+			pen.mX += kernVec.mX * scale * xScaleAdvance;
+		}
+		
+		// push the glyph
+		float penX = pen.mX + (( glyph->mWidth + glyph->mBearingX ) * scale * xScaleAdvance );
+		float glyphX = pen.mX;
+		//float glyphX = this->mRightToLeft ? penX : this->mPen.mX;
+
+		//this->mLayout->PushGlyph ( glyph, 0, glyphX, 0.0f, points, 0xffffffff );
+		//this->mTokenXMax = penX;
+
+		pen.mX += glyph->mAdvanceX * scale * xScaleAdvance;
+		
+		//this->WriteChar ( c );
+	}
+	
+	printf ( "\n" );
+}
+
+//----------------------------------------------------------------//
+MOAITextDesigner::MOAITextDesigner () {
+}
+
+//----------------------------------------------------------------//
+MOAITextDesigner::~MOAITextDesigner () {
+}
+
+//----------------------------------------------------------------//
+u32 MOAITextDesigner::NextChar () {
+
+	if ( !this->mStyleSpan ) return 0;
+
+	u32 c = u8_nextchar ( this->mStr, &this->mIdx );
+
+	if ( this->mIdx >= this->mStyleSpan->mTop ) {
+		
+		this->mSpanIdx++;
+		this->mStyleSpan = 0;
+		
+		if ( this->mSpanIdx < this->mStyleMap->GetTop ()) {
+			this->mStyleSpan = &this->mStyleMap->Elem ( this->mSpanIdx );
+			this->mIdx = this->mStyleSpan->mBase;
+			this->UpdateStyle ();
+		}
+	}
+	
+	return c;
+}
+
+//----------------------------------------------------------------//
+void MOAITextDesigner::UpdateStyle () {
+
+	assert ( this->mStyleSpan );
+	
+	MOAITextStyle* style = this->mStyleSpan->mStyle;
+	assert ( style );
+	
+	MOAIFont* font = style->mFont;
+	assert ( this->mStyleSpan->mStyle->mFont );
+	
+	this->mDeck = font->GetGlyphDeck ( style->mPoints );
+}
---
+++ src/moaicore/MOAITextDesigner.h
@@ -0,0 +1,74 @@
+// Copyright (c) 2010-2011 Zipline Games, Inc. All Rights Reserved.
+// http://getmoai.com
+
+#ifndef	MOAITEXTDESIGNER_H
+#define	MOAITEXTDESIGNER_H
+
+class MOAITextStyleMap;
+class MOAITextStyleSpan;
+
+//================================================================//
+// MOAITextDesigner
+//================================================================//
+class MOAITextDesigner {
+private:
+	
+	MOAITextStyleMap*		mStyleMap;
+	MOAITextStyleSpan*		mStyleSpan;
+	u32						mSpanIdx;
+	int						mIdx;
+	
+	cc8*					mStr;
+	
+	MOAIGlyphDeck*			mDeck;
+	
+	//u32					mLineBottom;
+	//u32					mLineTop;
+	//
+	//u32					mLineCount;
+	//u32					mTotalLines;
+	//
+	//float				mLineXMax;
+	//
+	//USVec2D				mPen;
+	//cc8*				mStr;
+	//USRect				mFrame;
+	//float				mLineSpacing;
+	//u32					mJustify;
+	//MOAITextLayout*		mLayout;
+	//
+	//MOAIAnimCurve**		mCurves;
+	//u32					mTotalCurves;
+	//bool				mRightToLeft;
+
+	//----------------------------------------------------------------//
+	//u32			DecodeChar			();
+	//void			FinishChar			( u32 c );
+	//void			FinishLine			();
+	//void			FinishWord			();
+	//void			Flush				();
+	//void			FlushLine			();
+	//void			FlushToken			();
+	static bool		IsWhitespace		( u32 c );
+	u32				NextChar			();
+	//void			Parse				();
+	void			UpdateStyle			();
+
+public:
+
+	//SET ( cc8*, Text, mStr )
+	//SET ( MOAIFont*, Font, mFont )
+	//SET ( const USRect&, Frame, mFrame )
+	//SET ( float, LineSpacing, mLineSpacing )
+	//SET ( float, Points, mPoints )
+	//SET ( u32, Alignment, mJustify )
+	//SET ( bool, RightToLeft, mRightToLeft )
+
+	//----------------------------------------------------------------//
+	void			Layout					( cc8* str, MOAITextStyleMap& styleMap );
+	//void			SetCurves				( MOAIAnimCurve** curves, u32 totalCurves );
+					MOAITextDesigner		();
+	virtual			~MOAITextDesigner		();
+};
+
+#endif
