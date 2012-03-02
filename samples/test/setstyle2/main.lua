c70693a24c662f4a2ed5d129b8b069072505dc66
Dana Drew <dana@ziplinegames.com>
2012-03-02 13:15:43 -0800

    Added a repro for a bug where text isn't being updated when I change the text style's attributes.

--- samples/test/setstyle2/main.lua
+++ samples/test/setstyle2/main.lua
@@ -14,58 +14,62 @@ layer = MOAILayer2D.new ()
 layer:setViewport ( viewport )
 MOAISim.pushRenderPass ( layer )
 
+charcodes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 .,:;!?()&/-'
+
 font1 = MOAIFont.new ()
 font1:load ( 'arial-rounded.TTF' )
+font1:preloadGlyphs ( charcodes, 12 )
 
 font2 = MOAIFont.new ()
 font2:load ( 'Dwarves.TTF' )
-
-font3 = MOAIFont.new ()
-font3:load ( 'EBOLA-KI.TTF' )
+font2:preloadGlyphs ( charcodes, 24 )
 
 continue = false
 
 function setStyleTests ()
 	while 1 do
-		text = 'This is <green>some test</> text to print out while testing different styles.'
+		text = 'This is some test text to print out while testing different styles.'
+		
+		print ( "Set the default style" )
 		
 		local textbox = MOAITextBox.new ()
 		textbox:setString ( text )
-		textbox:setRect ( -150, -230, 150, 230 )
+		textbox:setRect ( -150, 70, 150, 230 )
 		textbox:setYFlip ( true )
 		layer:insertProp ( textbox )
 		
 		local style1 = MOAITextStyle.new ()
 		style1:setColor ( 1, 0, 0, 1 )
 		style1:setFont ( font1 )
-		style1:setSize ( 24 )
+		style1:setSize ( 12 )
 		textbox:setStyle ( style1 )
-		
-		local style2 = MOAITextStyle.new ()
-		style2:setColor ( 0, 1, 0, 1 )
-		style2:setFont ( font2 )
-		style2:setSize ( 24 )
-		textbox:setStyle ( 'green', style2 )
 		
 		continue = false
 		repeat coroutine.yield () until continue
-		print ( "switch to broken text" )
+		print ( "Change the font and size of the default style without calling setStyle" )
 		
 		style1:setFont ( font2 )
+		style1:setSize ( 24 )
+		
+		continue = false
+		repeat coroutine.yield () until continue
+		print ( "Call setStyle to make an unused style" )
+		
 		textbox:setStyle ( 'red', style1 )
 		
 		continue = false
 		repeat coroutine.yield () until continue
-		print ( "switch to original" )
 		textbox:setReveal ( 0 )
 	end
 end
 
 -- tests
 function onKeyboardEvent ( key, down )
-	if down == true then
+	if down then
 		if key == 32 then -- continue
 			continue = true
+		else
+			return
 		end
 	end
 end
