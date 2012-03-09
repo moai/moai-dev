faae6b2c2e350b01cb225c5e097cba7320f330b1
Dana Drew <dana@ziplinegames.com>
2012-03-09 11:10:02 -0800

    Added a sample for a bug where setHighlight doesn't always work.

---
+++ samples/test/textbox-highlight/main.lua
@@ -0,0 +1,60 @@
+----------------------------------------------------------------
+-- Copyright (c) 2010-2011 Zipline Games, Inc. 
+-- All Rights Reserved. 
+-- http://getmoai.com
+----------------------------------------------------------------
+
+MOAISim.openWindow ( "test", 320, 480 )
+
+viewport = MOAIViewport.new ()
+viewport:setSize ( 320, 480 )
+viewport:setScale ( 320, 480 )
+
+layer = MOAILayer2D.new ()
+layer:setViewport ( viewport )
+MOAISim.pushRenderPass ( layer )
+
+font1 = MOAIFont.new ()
+font1:load ( 'arial-rounded.TTF' )
+
+continue = false
+
+function setHighlightTests ()
+	while 1 do
+		text = '1234567890 1234567890 1234567890 1234567890'
+		
+		local textbox = MOAITextBox.new ()
+		textbox:setString ( text )
+		textbox:setFont ( font1 )
+		textbox:setTextSize ( 12, 163 )
+		textbox:setRect ( -150, 70, 150, 230 )
+		textbox:setYFlip ( true )
+		layer:insertProp ( textbox )
+		
+		textbox:setHighlight ( 12, 10, 1, 1, 0 )
+		
+		continue = false
+		repeat coroutine.yield () until continue
+		
+		textbox:setHighlight ( 12, 10, 1, 1, 0 )
+		
+		continue = false
+		repeat coroutine.yield () until continue
+		textbox:setReveal ( 0 )
+	end
+end
+
+-- tests
+function onKeyboardEvent ( key, down )
+	if down then
+		if key == 32 then -- continue
+			continue = true
+		else
+			return
+		end
+	end
+end
+
+MOAIInputMgr.device.keyboard:setCallback ( onKeyboardEvent )
+thread = MOAIThread.new ()
+thread:run ( setHighlightTests )
