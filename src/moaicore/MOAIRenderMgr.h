fca98a9dcdeba9652c7faef9a7c8bfcdf09d6380
Patrick Meehan <patrick@ziplinegames.com>
2012-03-20 18:34:12 -0700

    forgot to add MOAIRenderMgr and MOAIRendarable source files

---
+++ src/moaicore/MOAIRenderMgr.h
@@ -0,0 +1,46 @@
+// Copyright (c) 2010-2011 Zipline Games, Inc. All Rights Reserved.
+// http://getmoai.com
+
+#ifndef	MOAIRENDERMGR_H
+#define	MOAIRENDERMGR_H
+
+#include <moaicore/MOAIEaseDriver.h>
+#include <moaicore/MOAILua.h>
+
+class MOAIProp;
+
+//================================================================//
+// MOAIRenderMgr
+//================================================================//
+/**	@name	MOAIRenderMgr
+	@text	Manages render table.
+*/
+class MOAIRenderMgr :
+	public MOAIGlobalClass < MOAIRenderMgr, MOAILuaObject > {
+private:
+	
+	u32				mRenderCounter;	// increments every render
+	MOAILuaRef		mRenderTable;
+	
+	//----------------------------------------------------------------//
+	static int		_getRenderTable				( lua_State* L );
+	static int		_setRenderTable				( lua_State* L );
+
+	//----------------------------------------------------------------//
+	void			RenderTable					( MOAILuaState& state, int idx );
+
+public:
+
+	DECL_LUA_SINGLETON ( MOAIRenderMgr )
+	
+	GET ( u32, RenderCounter, mRenderCounter )
+	
+	//----------------------------------------------------------------//
+					MOAIRenderMgr				();
+					~MOAIRenderMgr				();
+	void			RegisterLuaClass			( MOAILuaState& state );
+	void			RegisterLuaFuncs			( MOAILuaState& state );
+	void			Render						();
+};
+
+#endif
---
+++ src/moaicore/MOAIRenderable.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2010-2011 Zipline Games, Inc. All Rights Reserved.
+// http://getmoai.com
+
+#include "pch.h"
+#include <moaicore/MOAIRenderable.h>
+
+//================================================================//
+// MOAIRenderable
+//================================================================//
+
+//----------------------------------------------------------------//
+MOAIRenderable::MOAIRenderable () {
+	
+	RTTI_SINGLE ( MOAILuaObject )
+}
+
+//----------------------------------------------------------------//
+MOAIRenderable::~MOAIRenderable () {
+}
