cb408aeae755c6788ef306e2ff6b5096381a9a1d
Christopher Kohnert <ckohnert@hbs-studios.com>
2012-03-20 17:32:27 -0700

    Adding a MO file TextBundle lookup implementation.

---
+++ src/moaicore/MOAITextBundle.cpp
@@ -0,0 +1,9 @@
+//
+//  MOAITextBundle.cpp
+//  libmoai
+//
+//  Created by Christopher Kohnert on 3/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ src/moaicore/MOAITextBundle.h
@@ -0,0 +1,14 @@
+//
+//  MOAITextBundle.h
+//  libmoai
+//
+//  Created by Christopher Kohnert on 3/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef libmoai_MOAITextBundle_h
+#define libmoai_MOAITextBundle_h
+
+
+
+#endif
