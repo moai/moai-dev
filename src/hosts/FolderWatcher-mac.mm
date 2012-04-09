a8eb9b7c19c1185bb8258de223068b4afcaddbee
Dana Drew <dana@ziplinegames.com>
2012-04-09 15:25:04 -0700

    Fixed the FW on apple so that it refreshes the context if it's reloaded.

--- src/hosts/FolderWatcher-mac.mm
+++ src/hosts/FolderWatcher-mac.mm
@@ -296,6 +296,7 @@ void FWReloadChangedLuaFiles(void)
 		NSString* fileExtension = [[NSString stringWithUTF8String: filename] pathExtension];
 		if([fileExtension caseInsensitiveCompare: @"lua"] == NSOrderedSame)
 		{
+			GlutRefreshContext();
 			AKURunScript(filename);
 			printf("%s reloaded.\n", filename);
 		}
