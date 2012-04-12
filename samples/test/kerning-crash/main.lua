66d824556d7cd1bd40c3cec506f6cd36211b7a0a
Dana Drew <dana@ziplinegames.com>
2012-04-12 11:58:45 -0700

    Added a sample where the rebuildKerningTables function causes a crash.

---
+++ samples/test/kerning-crash/main.lua
@@ -0,0 +1,22 @@
+----------------------------------------------------------------
+-- Copyright (c) 2010-2011 Zipline Games, Inc. 
+-- All Rights Reserved. 
+-- http://getmoai.com
+----------------------------------------------------------------
+
+MOAISim.openWindow ( "test", 320, 480 )
+
+viewport = MOAIViewport.new ()
+viewport:setSize ( 320, 480 )
+viewport:setScale ( 320, 480 )
+
+layer = MOAILayer2D.new ()
+layer:setViewport ( viewport )
+MOAISim.pushRenderPass ( layer )
+
+charcodes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 .,:;!?()&/-'
+
+font = MOAIFont.new ()
+font:loadFromTTF ( 'arial-rounded.TTF', charcodes, 12, 163 )
+
+font:rebuildKerningTables ()
