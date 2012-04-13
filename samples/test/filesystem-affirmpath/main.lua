f9a832ce67a07007544bc894122e28112579e018
Dana Drew <dana@ziplinegames.com>
2012-04-13 15:36:45 -0700

    Added a sample where affirmPath doesn't create a directory properly after a virtual file is mounted.

---
+++ samples/test/filesystem-affirmpath/main.lua
@@ -0,0 +1,10 @@
+----------------------------------------------------------------
+-- Copyright (c) 2010-2011 Zipline Games, Inc. 
+-- All Rights Reserved. 
+-- http://getmoai.com
+----------------------------------------------------------------
+
+MOAISim.openWindow ( "test", 512, 512 )
+
+MOAIFileSystem.mountVirtualDirectory ( "testzip", "test.zip" )
+MOAIFileSystem.affirmPath ( "test1" )
