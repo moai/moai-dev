cf71ea29fdffc40814079218b575ee7174b4e0d5
Sean B House <sean@ziplinegames.com>
2012-04-17 17:41:14 -0700

    Command line scripts to build iOS/OSX sample projects.

---
+++ xcode/osx/clean.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#----------------------------------------------------------------#
+# Copyright (c) 2010-2011 Zipline Games, Inc.
+# All Rights Reserved.
+# http://getmoai.com
+#----------------------------------------------------------------#
+
+set -e
+
+usage="usage: $0 -j <jobName>"
+job="default"
+
+while [ $# -gt 0 ];	do
+    case "$1" in
+		-j)  job="$2"; shift;;
+		-*)
+	    	echo >&2 \
+	    		$usage
+	    	exit 1;;
+		*)  break;;
+    esac
+    shift
+done
+
+if ! [[ $job =~ ^[a-zA-Z0-9_\-]+$ ]]; then
+	echo -e "*** Illegal job name specified: $job..."
+	echo -e "    > Job names may only contain letters, numbers, dashes and underscores"
+	echo
+	exit 1
+fi
+
+xcodebuild -configuration Debug -workspace MoaiSample.xcodeproj/project.xcworkspace -scheme moai -sdk macosx clean CONFIGURATION_BUILD_DIR=/tmp/osx/$job/MoaiSample/moai/macosx/Debug
+xcodebuild -configuration Release -workspace MoaiSample.xcodeproj/project.xcworkspace -scheme moai -sdk macosx clean CONFIGURATION_BUILD_DIR=/tmp/osx/$job/MoaiSample/moai/macosx/Release
