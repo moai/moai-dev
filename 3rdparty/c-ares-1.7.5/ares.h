840e8ea3724ae1fd5882c41a325e6aaca5df70ff
Adam Villalobos <adam@ziplinegames.com>
2012-04-25 18:33:30 -0700

    refactoring ares change to be set from MOAI, default dns server is no longer hardcoded

--- 3rdparty/c-ares-1.7.5/ares.h
+++ 3rdparty/c-ares-1.7.5/ares.h
@@ -302,6 +302,10 @@ typedef int  (*ares_sock_create_callback)(ares_socket_t socket_fd,
                                           int type,
                                           void *data);
 
+CARES_EXTERN void ares_set_default_dns_addr(int addr);
+	
+CARES_EXTERN int ares_get_default_dns_addr();
+	
 CARES_EXTERN int ares_library_init(int flags);
 
 CARES_EXTERN void ares_library_cleanup(void);
