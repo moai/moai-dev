b289f538326b9309cb3c0d4e8a3d3d6674813d76
Eric Edelman <eric@ziplinegames.com>
2012-05-30 14:44:56 -0700

    updated load-jpg sample with broken jpg

--- samples/test/load-jpg/main.lua
+++ samples/test/load-jpg/main.lua
@@ -23,4 +23,26 @@ prop = MOAIProp2D.new ()
 prop:setDeck ( gfxQuad )
 layer:insertProp ( prop )
 
-prop:moveRot ( 360, 1.5 )
+action = prop:moveRot ( 360, 1.5 )
+
+
+local threadFunc = function ()
+	
+	repeat coroutine.yield () until not action:isBusy ()
+	
+	layer:removeProp ( prop )
+
+	gfxQuad2 = MOAIGfxQuad2D.new ()
+	gfxQuad2:setTexture ( "prof-pic.jpg" )
+	gfxQuad2:setRect ( -25, -25, 25, 25 )
+	gfxQuad2:setUVRect ( 0, 0, 1, 1 )
+
+	prop2 = MOAIProp2D.new ()
+	prop2:setDeck ( gfxQuad2 )
+	layer:insertProp ( prop2 )
+
+	prop2:moveRot ( 360, 1.5 )
+end
+
+thread = MOAIThread.new ()
+thread:run ( threadFunc )
