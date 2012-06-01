1f27532f1897ad00f7dc3a734021c7072ea2ea91
Dana Drew <dana@ziplinegames.com>
2012-05-31 18:13:11 -0700

    Added a sample that shows that the getRelativePath function isn't working properly.

---
+++ samples/test/filesystem-getrelativepath/main.lua
@@ -0,0 +1,21 @@
+----------------------------------------------------------------
+-- Copyright (c) 2010-2011 Zipline Games, Inc. 
+-- All Rights Reserved. 
+-- http://getmoai.com
+----------------------------------------------------------------
+
+abspath = MOAIFileSystem.getAbsoluteDirectoryPath ( 'test/blue' )
+print ( abspath )
+print ()
+
+relpath = MOAIFileSystem.getRelativePath ( abspath )
+print ( relpath )
+print ()
+
+abspath = MOAIFileSystem.getAbsoluteFilePath ( 'test/blue/dummy.txt' )
+print ( abspath )
+print ()
+
+relpath = MOAIFileSystem.getRelativePath ( abspath )
+print ( relpath )
+print ()
