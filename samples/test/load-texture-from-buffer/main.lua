e73b73dc078eb431ab56a745e13cfe83be9ff8d6
Patrick Meehan <patrick@ziplinegames.com>
2012-06-11 02:19:54 -0700

    fixed 'load-texture-from-buffer' test; fixed bug where byte stream length was not set in MOAITexture::Init () from USStream (issue #405)

--- samples/test/load-texture-from-buffer/main.lua
+++ samples/test/load-texture-from-buffer/main.lua
@@ -8,52 +8,23 @@ MOAISim.openWindow ( "test", 320, 480 )
 
 viewport = MOAIViewport.new ()
 viewport:setSize ( 320, 480 )
-viewport:setScale ( 320, -480 )
+viewport:setScale ( 320, 480 )
 
 layer = MOAILayer2D.new ()
 layer:setViewport ( viewport )
 MOAISim.pushRenderPass ( layer )
 
+buffer = MOAIDataBuffer.new ()
+buffer:load ( "moai.png" )
+
+texture = MOAITexture.new () 
+texture:load ( buffer )
+
+w, h = texture:getSize () 
 gfxQuad = MOAIGfxQuad2D.new ()
--- gfxQuad:setTexture ( "cathead.jpg" ) -- crashes
+gfxQuad:setTexture ( texture )
+gfxQuad:setRect ( -(w/2), -(h/2), (w/2), (h/2) )
 
- 
-local texture = MOAITexture.new () 
-texture:load ( "moai.png" )
--- texture:setFilter ( MOAITexture.GL_LINEAR ) 
--- local w, h = texture:getSize () 
--- local gfxQuad = MOAIGfxQuad2D.new ()
--- gfxQuad:setTexture ( texture )
--- gfxQuad:setRect ( -(w/2), -(h/2), (w/2), (h/2) )
-
--- prop = MOAIProp2D.new ()
--- prop:setDeck ( gfxQuad )
--- layer:insertProp ( prop )
-
--- action = prop:moveRot ( 360, 1.5 )
-
-
--- local threadFunc = function ()
-	
-	-- repeat coroutine.yield () until not action:isBusy ()
-	
-	-- layer:removeProp ( prop )
-
-	-- gfxQuad2 = MOAIGfxQuad2D.new ()
-	
-	-- texture:load ( "prof-pic.jpg" )
-	-- texture:setFilter ( MOAITexture.GL_LINEAR ) 
-	-- local w, h = texture:getSize () 
-	-- local gfxQuad2 = MOAIGfxQuad2D.new ()
-	-- gfxQuad2:setTexture ( texture )
-	-- gfxQuad2:setRect ( -(w/2), -(h/2), (w/2), (h/2) )
-
-	-- prop2 = MOAIProp2D.new ()
-	-- prop2:setDeck ( gfxQuad2 )
-	-- layer:insertProp ( prop2 )
-
-	-- prop2:moveRot ( 360, 1.5 )
--- end
-
--- thread = MOAIThread.new ()
--- thread:run ( threadFunc )
+prop = MOAIProp2D.new ()
+prop:setDeck ( gfxQuad )
+layer:insertProp ( prop )
