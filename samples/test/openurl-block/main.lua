daf628f0f6c5e853a13478deb7c02de3dd4dee97
Dana <dana@ziplinegames.com>
2012-06-13 16:22:25 -0700

    Added a sample where openURL doesn't pause script execution on android.

---
+++ samples/test/openurl-block/main.lua
@@ -0,0 +1,13 @@
+----------------------------------------------------------------
+-- Copyright (c) 2010-2011 Zipline Games, Inc. 
+-- All Rights Reserved. 
+-- http://getmoai.com
+----------------------------------------------------------------
+
+MOAIAppAndroid.openURL ( "http://getmoai.com/" )
+
+i = 1
+while true do
+	print ( i )
+	i = i + 1
+end
