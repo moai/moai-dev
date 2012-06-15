8bc3a04f9a295af2d3dd339d22d73f910a9bbdf2
Dana Drew <dana@ziplinegames.com>
2012-06-15 16:14:12 -0700

    Fixed the sample for the data buffer crash a little, and added a text file for it to use.

--- samples/test/dataBuffer - open - crash/main.lua
+++ samples/test/dataBuffer - open - crash/main.lua
@@ -4,22 +4,8 @@
 -- http://getmoai.com
 ----------------------------------------------------------------
 
-function test ()
-	
-	MOAITestMgr.beginTest ( 'MOAIDataBufferStream' )
-	
-	---------------------------------------------------------------
-	
-	buffer = MOAIDataBuffer.new () 
-    buffer:load ( 'test.txt' ) 
-    stream = MOAIDataBufferStream.new () 
-	 
-    stream:open ( buffer )
-
-	---------------------------------------------------------------------------
-	
-end
-
-MOAITestMgr.setStagingFunc ( stage )
-MOAITestMgr.setTestFunc ( test )
-MOAITestMgr.setFilter ( MOAITestMgr.UTIL )
+buffer = MOAIDataBuffer.new () 
+buffer:load ( 'test.txt' ) 
+stream = MOAIDataBufferStream.new () 
+ 
+stream:open ( buffer )
