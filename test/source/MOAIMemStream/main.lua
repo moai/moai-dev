419aa3d87c3a64322eb53653de60f89c950cbc7a
Trevor Ransom <trevor@ziplinegames.com>
2012-06-25 14:44:51 -0700

    MOAIMemStream fixed seek back problem

--- test/source/MOAIMemStream/main.lua
+++ test/source/MOAIMemStream/main.lua
@@ -44,11 +44,10 @@ function test ()
 		evaluate (false, '8 bit seek forwards')
 	end
 	
-	stream:seek ( -31, MOAIMemStream.SEEK_CUR )
-	print ( stream:read8 ())
-	-- if stream:read8 () ~= 13 then
-		-- evaluate (false, '8 bit seek back')
-	-- end
+	stream:seek ( -32, MOAIMemStream.SEEK_CUR ) 
+	if stream:read8 () ~= 13 then
+		evaluate (false, '8 bit seek back')
+	end
 
 	stream:seek ()
 	
@@ -101,8 +100,8 @@ function test ()
 		evaluate (false, '16 bit seek forwards	')
 	end
 	
-	stream:seek ( -40 )
-	if stream:read16 () ~= 4 then 
+	stream:seek ( -40, MOAIMemStream.SEEK_CUR )
+	if stream:read16 () ~= 5 then 
 		evaluate (false, '16 bit seek back')
 	end
 	
@@ -156,8 +155,8 @@ function test ()
 		evaluate (false, '32 bit seek forwards')
 	end
 	
-	stream:seek ( -100 )
-	if stream:read32 () ~= -25 then 
+	stream:seek ( -100, MOAIMemStream.SEEK_CUR )
+	if stream:read32 () ~= -26 then 
 		evaluate (false, '32 bit seek back')
 	end
 
@@ -211,8 +210,8 @@ function test ()
 		evaluate (false, 'Double seek forward')
 	end
 	
-	stream:seek ( -480 )
-	if stream:readDouble () ~= 45 then 
+	stream:seek ( -480, MOAIMemStream.SEEK_CUR )
+	if stream:readDouble () ~= 46 then 
 		evaluate (false, 'Double seek back')
 	end
 
@@ -265,8 +264,8 @@ function test ()
 		evaluate (false, 'Float seek forwards')
 	end
 	
-	stream:seek ( -4 ) 
-	if stream:readFloat () ~= 59 then
+	stream:seek ( -4, MOAIMemStream.SEEK_CUR ) 
+	if stream:readFloat () ~= 60 then
 		evaluate (false, 'Float seek back')
 	end
 
@@ -320,9 +319,9 @@ function test ()
 		evaluate (false, 'Format seek forwards')
 	end
 	
-	stream:seek ( -78 )
+	stream:seek ( -78, MOAIMemStream.SEEK_CUR )
 	val1, val2, val3, val4, val5, val6, val7, val8 = stream:readFormat ( 's8,s16,s32,u8,u16,u32,f,d' )
-	if val1 ~= 2 or val2 ~= 3 or val3 ~= 3 or val4 ~= 4 or val5 ~= 14 or val6 ~= 6 or val7 ~= 1.5 or val8 ~= 4.5 then 
+	if val1 ~= 3 or val2 ~= 4 or val3 ~= 3 or val4 ~= 4 or val5 ~= 15 or val6 ~= 6 or val7 ~= 1.5 or val8 ~= 5.5 then 
 		evaluate (false, 'Format seek back')
 	end
 
@@ -376,8 +375,8 @@ function test ()
 		evaluate (false, 'Unsigned 8 bit seek forward')
 	end
 	
-	stream:seek ( -19 )
-	if stream:readU8 () ~= 240  then 
+	stream:seek ( -19, MOAIMemStream.SEEK_CUR )
+	if stream:readU8 () ~= 239  then 
 		evaluate (false, 'Unsigned 8 bit seek back')
 	end
 
@@ -430,8 +429,8 @@ function test ()
 		evaluate (false, 'Unsigned 16 bit seek forward')
 	end
 	
-	stream:seek ( -10 )
-	if stream:readU16 () ~= 65455 then 
+	stream:seek ( -10, MOAIMemStream.SEEK_CUR )
+	if stream:readU16 () ~= 65454 then 
 		evaluate (false, 'Unsigned 16 bit seek back')
 	end
 	
@@ -484,8 +483,8 @@ function test ()
 		evaluate (false, 'Unsigned 32 bit seek forward')
 	end
 
-	stream:seek ( -8 )
-	if stream:readU32 () ~= 13  then 
+	stream:seek ( -8, MOAIMemStream.SEEK_CUR )
+	if stream:readU32 () ~= 14  then 
 		evaluate (false, 'Unsigned 32 bit seek back')
 	end
 	
@@ -537,7 +536,7 @@ function test ()
 	if stream:read ( 15 ) ~= 'uberstrikewrite'  then 
 		evaluate (false, 'Read seek')
 	end
-
+	
 	stream:seek ()
 	
 	i = 0
@@ -593,8 +592,7 @@ function test ()
 	if reader:read ( len ) ~= 'This is a stream writer test. Detta ar en strom forfattare prov.' then
 		evaluate (false, 'StreamWriter-openDeflate-seekforward')
 	end
-	print ( reader:read ( len ))
-	
+		
 	reader:seek ()
 	
 	i = 0
@@ -648,7 +646,6 @@ function test ()
 	if reader:read ( len ) ~= 'This is a stream writer test. Ceci est un test ecrivain flux.' then
 		evaluate (false, 'StreamWriter-base64-seekforward')
 	end
-	-- print ( reader:read ( len ))
 	
 	reader:seek ()
 	
