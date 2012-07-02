b047b15f0bca743ff1ff53c6cdc32471d1cf4ec6
Brendan A R Sechter <sgeos@hotmail.com>
2012-07-02 13:52:03 +0900

    Added Lua-SQLite3 backend to OSX client.

--- src/aku/AKU-luaext.h
+++ src/aku/AKU-luaext.h
@@ -14,5 +14,6 @@ AKU_API void	AKUExtLoadLuacurl			();
 AKU_API void	AKUExtLoadLuafilesystem		();
 AKU_API void	AKUExtLoadLuasocket			();
 AKU_API void	AKUExtLoadLuasql			();
+AKU_API void	AKUExtLoadLuasqlite3		();
 
 #endif
