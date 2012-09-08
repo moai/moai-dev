724bda201efd903a9cc4b7272fba20b764324cbb
Eric Edelman <eric@ziplinegames.com>
2012-09-08 11:25:41 -0700

    changed the way we calculate for the android status bar

--- ant/host-source/source/project/src/moai/Moai.java
+++ ant/host-source/source/project/src/moai/Moai.java
@@ -16,6 +16,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings.Secure;
+import android.util.DisplayMetrics;
 
 import java.lang.reflect.Method;
 import java.lang.Runtime;
@@ -543,10 +544,28 @@ public class Moai {
 	
 	//----------------------------------------------------------------//
 	public static int getStatusBarHeight () {
-	
-		// At medium density ( density == 1 ) the height is 42. Use this fact to convert...
-		int statusBarHeight = ( int ) Math.ceil ( 42 * sActivity.getResources ().getDisplayMetrics ().density );
-		return statusBarHeight;
+
+		int myHeight = 0;
+		switch ( sActivity.getResources ().getDisplayMetrics ().densityDpi ) {
+	        case DisplayMetrics.DENSITY_HIGH:
+
+	            myHeight = 54;
+	            break;
+	        case DisplayMetrics.DENSITY_MEDIUM:
+
+	            myHeight = 36;
+	            break;
+	        case DisplayMetrics.DENSITY_LOW:
+
+	            myHeight = 26;
+	            break;
+			default:
+				
+				myHeight = 0;
+				break;
+			}
+			
+		return myHeight;
 	}
 	
 	//----------------------------------------------------------------//
