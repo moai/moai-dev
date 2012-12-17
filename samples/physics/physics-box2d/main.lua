96a7249111a2d83a92ace6402f0c7005731a3366
Matthias Sweertvaegher <matthias.sweertvaegher@gmail.com>
2012-12-17 21:55:24 +0100

    updated box2d physics sample
    adding mouse manipulation using mouse joint

--- samples/physics/physics-box2d/main.lua
+++ samples/physics/physics-box2d/main.lua
@@ -44,26 +44,8 @@ world:setUnitsToMeters ( 2 )
 world:start ()
 layer:setBox2DWorld ( world )
 
-body = world:addBody ( MOAIBox2DBody.DYNAMIC )
-
-poly = {
-	0, -1,
-	1, 0,
-	0, 1,
-	-1, 0,
-}
-
-fixture = body:addPolygon ( poly )
-fixture:setDensity ( 1 )
-fixture:setFriction ( 0.3 )
-fixture:setFilter ( 0x01 )
-fixture:setCollisionHandler ( onCollide, MOAIBox2DArbiter.BEGIN + MOAIBox2DArbiter.END, 0x02 )
-
-body:resetMassData ()
-body:applyAngularImpulse ( 2 )
-
-body2 = world:addBody ( MOAIBox2DBody.STATIC )
-fixture2 = body2:addRect ( -5, -5, 5, -3 )
+worldBody = world:addBody ( MOAIBox2DBody.STATIC )
+fixture2 = worldBody:addRect ( -5, -5, 5, -3 )
 fixture2:setFilter ( 0x02 )
 fixture2:setCollisionHandler ( onCollide, MOAIBox2DArbiter.BEGIN + MOAIBox2DArbiter.END, 0x00 )
 
@@ -71,7 +53,66 @@ texture = MOAIGfxQuad2D.new ()
 texture:setTexture ( 'moai.png' )
 texture:setRect ( -0.5, -0.5, 0.5, 0.5 )
 
-sprite = MOAIProp2D.new ()
-sprite:setDeck ( texture )
-sprite:setParent ( body )
-layer:insertProp ( sprite )
+function addSprite()
+	local body = world:addBody ( MOAIBox2DBody.DYNAMIC )
+
+	local poly = {
+		0, -1,
+		1, 0,
+		0, 1,
+		-1, 0,
+	}
+
+	local fixture = body:addPolygon ( poly )
+	fixture:setDensity ( 1 )
+	fixture:setFriction ( 0.3 )
+	fixture:setFilter ( 0x01 )
+	fixture:setCollisionHandler ( onCollide, MOAIBox2DArbiter.BEGIN + MOAIBox2DArbiter.END, 0x02 )
+
+	body:resetMassData ()
+	body:applyAngularImpulse ( 2 )
+
+	local sprite = MOAIProp2D.new ()
+	sprite:setDeck ( texture )
+	sprite.body = body
+	sprite:setParent ( body )
+	layer:insertProp ( sprite )
+end
+
+function pointerCallback ( x, y )
+	worldX, worldY = layer:wndToWorld ( x, y )
+
+	if pick then
+	  mouseJoint:setTarget(worldX, worldY)
+	end
+end
+
+function clickCallback ( down )
+	if down then
+		pick = layer:getPartition():propForPoint ( worldX, worldY )
+		if pick then
+			mouseBody = world:addBody( MOAIBox2DBody.DYNAMIC )
+			mouseBody:setTransform(worldX, worldY)
+
+			mouseJoint = world:addMouseJoint(mouseBody, pick.body, worldX, worldY,  10000.0 * pick.body:getMass());
+			mouseJoint:setDampingRatio(2);
+		end
+	else
+		if pick then
+			--also destroys joint
+			mouseBody:destroy()
+			mouseBody = nil
+			pick = nil
+		end
+	end
+end
+
+function rightclickCallback ( down )
+	if down then
+	  addSprite()
+	end
+end
+
+MOAIInputMgr.device.pointer:setCallback ( pointerCallback )
+MOAIInputMgr.device.mouseLeft:setCallback ( clickCallback )
+MOAIInputMgr.device.mouseRight:setCallback ( rightclickCallback )
