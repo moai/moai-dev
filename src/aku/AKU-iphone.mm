df95e8151feeddac845f5c9530809d147a35a22a
Alexey Zankevich <zancudero@gmail.com>
2013-01-22 11:59:19 +0300

    Added screen size fix

--- src/aku/AKU-iphone.mm
+++ src/aku/AKU-iphone.mm
@@ -141,8 +141,20 @@ void AKUIphoneInit ( UIApplication* application ) {
 	environment.SetValue ( MOAI_ENV_osVersion,			[[ UIDevice currentDevice ].systemVersion UTF8String ]);
 	environment.SetValue ( MOAI_ENV_resourceDirectory,	[[[ NSBundle mainBundle ] resourcePath ] UTF8String ]);
 	environment.SetValue ( MOAI_ENV_openUdid,			[[ MOAIOpenUDID value] UTF8String ]);
-	environment.SetValue ( MOAI_ENV_horizontalResolution, [[ UIScreen mainScreen ] bounds ].size.width * [[ UIScreen mainScreen ] scale ] );
-	environment.SetValue ( MOAI_ENV_verticalResolution, [[ UIScreen mainScreen ] bounds ].size.height * [[ UIScreen mainScreen ] scale ] );
+	
+	//Screen orientation fix. TODO: support other orientation masks.
+	BOOL landscapeOnly = ([[[application keyWindow] rootViewController] supportedInterfaceOrientations] == UIInterfaceOrientationMaskLandscape);
+	CGFloat screenWidth = [[ UIScreen mainScreen ] bounds ].size.width * [[ UIScreen mainScreen ] scale ];
+	CGFloat screenHeight = [[ UIScreen mainScreen ] bounds ].size.height * [[ UIScreen mainScreen ] scale ];
+	if (landscapeOnly) {
+		if (screenHeight > screenWidth) {
+			CGFloat temp = screenHeight;
+			screenHeight = screenWidth;
+			screenWidth = temp;
+		}
+	}
+	environment.SetValue ( MOAI_ENV_horizontalResolution, screenWidth);
+	environment.SetValue ( MOAI_ENV_verticalResolution,  screenHeight);
 	environment.SetValue ( MOAI_ENV_screenDpi, AKUCalcScreenDpi( [ UIScreen mainScreen ] ));
 }
 
