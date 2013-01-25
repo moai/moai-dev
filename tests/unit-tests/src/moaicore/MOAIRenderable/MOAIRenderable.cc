bb720ebf53e2a0f0a051de86fb23e8f973d4614f
Francisco Tufró <contacto@franciscotufro.com.ar>
2013-01-25 09:36:12 -0300

    Dummy test.

---
+++ tests/unit-tests/src/moaicore/MOAIRenderable/MOAIRenderable.cc
@@ -0,0 +1,10 @@
+// Copyright (c) 2010-2011 Zipline Games, Inc. All Rights Reserved.
+// http://getmoai.com
+
+#include "gtest/gtest.h"
+
+TEST(MOAIRenderable, CanBeCreated) {
+  //MOAIRenderable o = new MOAIRenderable ();
+
+  EXPECT_TRUE(true);
+}
