ae6c9f7dc96723dd6bb445789f1c3c55872b56b5
Jetha Chan <jethachan@gmail.com>
2013-02-15 16:18:24 +0800

    Window creation seems to be working.

--- vs2010/moai-sdl/SDLHost.cpp
+++ vs2010/moai-sdl/SDLHost.cpp
@@ -1,8 +1,44 @@
 #include "SDLHost.h"
 
-
 #include <aku/AKU.h>
 #include <lua-headers/moai_lua.h>
+
+#ifndef UNUSED
+#define UNUSED(p) (( void )p)
+#endif
+
+#ifdef SDLHOST_USE_DEBUGGER
+#include <aku/AKU-debugger.h>
+#endif
+
+#ifdef SDLHOST_USE_FMOD
+#include <aku/AKU-fmod-ex.h>//<AKU-fmod-ex.h>//<aku/AKU-fmod.h>
+#endif
+
+#ifdef SDLHOST_USE_LUAEXT
+#include <aku/AKU-luaext.h>
+#endif
+
+#ifdef SDLHOST_USE_UNTZ
+#include <aku/AKU-untz.h>
+#endif
+
+#ifdef SDLHOST_USE_AUDIOSAMPLER
+#include <aku/AKU-audiosampler.h>
+#endif
+
+#ifdef SDLHOST_USE_PARTICLE_PRESETS
+#include <ParticlePresets.h>
+#endif
+
+#ifdef _WIN32
+#include <glut.h>
+//#include <FolderWatcher-win.h>
+#else
+#include <GLUT/glut.h>
+#endif
+
+
 
 static bool sHasWindow = false;
 static bool sExitFullscreen = false;
@@ -16,6 +52,16 @@ static int sWinHeight;
 
 
 static void _cleanup() {
+	AKUFinalize ();
+	/*
+	if ( sDynamicallyReevaluateLuaFiles ) {
+#ifdef _WIN32
+		winhostext_CleanUp ();
+#elif __APPLE__
+		FWStopAll ();
+#endif
+	}
+	*/
 	
 }
 
@@ -77,15 +123,136 @@ void _AKUOpenWindowFunc(const char* title, int width, int height)
 
 #pragma endregion
 
+
+unsigned int _onTick(unsigned int millisec)
+{
+	printf("boo/n");
+
+	return 0;
+}
+
 int SdlHost(int argc, char** arg)
 {
+	// @todo	maybe we should use AKUInitMemPool here
+	atexit(_cleanup);
+
 	// init SDL
-	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+
+	SdlRefreshContext();
+
+	// @todo	un-dumb this
+	char* lastScript = NULL;
+	for (int i = 1; i < argc; ++i)
+	{
+		char* thisarg = arg[i];
+		if ( strcmp( thisarg, "-e" ) == 0 ) {
+			sDynamicallyReevaluateLuaFiles = true;
+		}
+		else if ( strcmp( thisarg, "-s" ) == 0 && ++i < argc ) {
+			char* script = arg [ i ];
+			AKURunString ( script );
+		}
+		else {
+			AKURunScript ( thisarg );
+			lastScript = thisarg;
+		}
+	}
+
+	/*
+	if ( lastScript && sDynamicallyReevaluateLuaFiles ) {
+#ifdef _WIN32
+		winhostext_WatchFolder ( lastScript );
+#elif __APPLE__
+		FWWatchFolder( lastScript );
+#endif
+	}
+	*/
+
+	unsigned int timerInterval =
+		(unsigned int)(AKUGetSimStep() * 1000.0);
+	
+	// sdl-style loop
+	SDL_Event event;
+	unsigned int tick_start = SDL_GetTicks();
+	unsigned int tick_end = 0;
+	unsigned int tick_delta = 0;
+	float dt = 0.0f;
+	bool bGameRunning = true;
+	if(sHasWindow)
+	{
+		SDL_SetTimer(timerInterval, _onTick);
+		while(bGameRunning)
+		{
+			while(SDL_PollEvent(&event))
+			{
+				switch(event.type) {
+				case SDL_KEYDOWN:
+					printf("Keypress!\n");
+					break;
+				case SDL_QUIT:
+					bGameRunning = false;
+					break;
+				default:
+					//printf("Unhandled event.\n");
+					break;
+				}
+			};
+
+			// Update the clock.
+			tick_end = SDL_GetTicks();
+			tick_delta = tick_end - tick_start;
+
+			// Do on-tick events.
+			//_onTick(tick_delta);
+
+			dt = (float)(tick_delta) * 0.001f;/// 1000.0f;
+			tick_start = tick_end;
+		}
+	}
+
 
 	return 0;
 }
 
 void SdlRefreshContext()
 {
+	AKUContextID context = AKUGetContext();
+	if(context) {
+		AKUDeleteContext(context);
+	}
+	AKUCreateContext();
 
+	#ifdef SDLHOST_USE_FMOD
+	//AKUFmodExInit();
+	#endif
+
+	#ifdef SDLHOST_USE_LUAEXT
+	AKUExtLoadLuacrypto ();
+	AKUExtLoadLuacurl ();
+	AKUExtLoadLuafilesystem ();
+	AKUExtLoadLuasocket ();
+	AKUExtLoadLuasql ();
+	#endif
+
+	#ifdef SDLHOST_USE_UNTZ
+	AKUUntzInit();
+	#endif
+
+	#ifdef SDLHOST_USE_AUDIOSAMPLER
+	AKUAudioSamplerInit();	
+	#endif
+
+	#ifdef SDLHOST_USE_PARTICLE_PRESETS
+	//ParticlePresets();
+	#endif
+
+	AKUSetInputConfigurationName ( "AKUSdl" );
+
+	//AKUSetFunc_EnterFullscreenMode ( _AKUEnterFullscreenModeFunc );
+	//AKUSetFunc_ExitFullscreenMode ( _AKUExitFullscreenModeFunc );
+	AKUSetFunc_OpenWindow ( _AKUOpenWindowFunc );
+
+
+	AKURunBytecode ( moai_lua, moai_lua_SIZE );
 }
