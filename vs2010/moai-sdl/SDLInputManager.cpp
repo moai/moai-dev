5712a10da3ada860cf57d716cab27de8ef10b2e6
Jetha Chan <jethachan@gmail.com>
2013-02-17 23:26:20 +0800

    Forgot a file.

--- vs2010/moai-sdl/SDLInputManager.cpp
+++ vs2010/moai-sdl/SDLInputManager.cpp
@@ -17,6 +17,39 @@ void SdlInputManager::doAKUInit()
 
 	doAKUDeviceInit(SDLInputDevice::ID_DEVICE);
 
+	// disable controller events; we'll manually poll, thanks
+	SDL_GameControllerEventState(SDL_IGNORE);
+
+
+	num_joysticks = SDL_NumJoysticks();
+	printf("joystick count: %d\n", num_joysticks);
+	int controllerIdx = 1;
+	for (int i = 0; i < num_joysticks; ++i)
+	{
+		printf("\t[controller %d]\n", i+1);
+		if (SDL_IsGameController(i))
+		{
+			printf("\t\tGame controller?\tYES\n");
+			SDL_GameControllerOpen(i);
+			doAKUDeviceInit((SDLInputDevice::InputDevice_ID)(i+1));
+		} else {
+			printf("\t\tGame controller?\tNO\n");
+		}
+
+		char* controllerName =
+			const_cast<char*>(SDL_GameControllerNameForIndex(i));
+		
+
+		if (controllerName) {
+			printf(
+				"\t\tname: '%s'\n",
+				controllerName
+			);
+		} else {
+			printf("\t\tname: NONE\n");
+		}
+		
+	}
 }
 
 void SdlInputManager::doAKUDeviceInit(
@@ -34,35 +67,7 @@ void SdlInputManager::doAKUDeviceInit(
 	switch (devicetype)
 	{
 	case SDLInputDeviceType::IDT_DEVICE:
-		AKUReserveInputDeviceSensors(
-			p_id,
-			SDLInputDeviceSensor::IDS_TOTAL
-			);
-		AKUSetInputDeviceKeyboard(
-			p_id,
-			SDLInputDeviceSensor::IDS_KEYBOARD,
-			SDLInputDeviceSensor::SensorName[SDLInputDeviceSensor::IDS_KEYBOARD]
-			);
-		AKUSetInputDevicePointer(
-			p_id,
-			SDLInputDeviceSensor::IDS_POINTER,
-			SDLInputDeviceSensor::SensorName[SDLInputDeviceSensor::IDS_POINTER]
-			);
-		AKUSetInputDeviceButton(
-			p_id,
-			SDLInputDeviceSensor::IDS_MOUSE_LEFT,
-			SDLInputDeviceSensor::SensorName[SDLInputDeviceSensor::IDS_MOUSE_LEFT]
-			);
-		AKUSetInputDeviceButton(
-			p_id,
-			SDLInputDeviceSensor::IDS_MOUSE_MIDDLE,
-			SDLInputDeviceSensor::SensorName[SDLInputDeviceSensor::IDS_MOUSE_MIDDLE]
-			);
-		AKUSetInputDeviceButton(
-			p_id,
-			SDLInputDeviceSensor::IDS_MOUSE_RIGHT,
-			SDLInputDeviceSensor::SensorName[SDLInputDeviceSensor::IDS_MOUSE_RIGHT]
-			);
+		initDevice(p_id);
 		break;
 
 	// @todo	actually implement this
@@ -73,6 +78,39 @@ void SdlInputManager::doAKUDeviceInit(
 		printf("Unhandled device type.");
 		break;
 	}
+}
+
+void SdlInputManager::initDevice(SDLInputDevice::InputDevice_ID p_id)
+{
+	AKUReserveInputDeviceSensors(
+		p_id,
+		SDLInputDeviceSensor::IDS_TOTAL
+	);
+	AKUSetInputDeviceKeyboard(
+		p_id,
+		SDLInputDeviceSensor::IDS_KEYBOARD,
+		SDLInputDeviceSensor::SensorName[SDLInputDeviceSensor::IDS_KEYBOARD]
+	);
+	AKUSetInputDevicePointer(
+		p_id,
+		SDLInputDeviceSensor::IDS_POINTER,
+		SDLInputDeviceSensor::SensorName[SDLInputDeviceSensor::IDS_POINTER]
+	);
+	AKUSetInputDeviceButton(
+		p_id,
+		SDLInputDeviceSensor::IDS_MOUSE_LEFT,
+		SDLInputDeviceSensor::SensorName[SDLInputDeviceSensor::IDS_MOUSE_LEFT]
+	);
+	AKUSetInputDeviceButton(
+		p_id,
+		SDLInputDeviceSensor::IDS_MOUSE_MIDDLE,
+		SDLInputDeviceSensor::SensorName[SDLInputDeviceSensor::IDS_MOUSE_MIDDLE]
+	);
+	AKUSetInputDeviceButton(
+		p_id,
+		SDLInputDeviceSensor::IDS_MOUSE_RIGHT,
+		SDLInputDeviceSensor::SensorName[SDLInputDeviceSensor::IDS_MOUSE_RIGHT]
+	);
 }
 
 void SdlInputManager::inputNotify_onMouseMove(SDL_MouseMotionEvent* p_event)
