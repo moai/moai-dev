9aed4cb75b1ea7967c7749c4e518f7b0d4e96899
Francisco Tufró <contacto@franciscotufro.com.ar>
2013-05-14 18:55:05 -0300

    [CI] Fixed build script for vs2010

--- bin/build-vs2010.bat
+++ bin/build-vs2010.bat
@@ -1,3 +1,3 @@
 call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 cd vs2010
-msbuild moai-open.sln /p:Configuration=Release /verbosity:quiet
+msbuild moai.sln /p:Configuration=Release /verbosity:quiet
