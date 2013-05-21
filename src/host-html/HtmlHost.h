eee3c81aa54b7e9dfaddbdf1f525e526d7fdfdda
David Pershouse <dpershouse@gmail.com>
2013-05-21 13:59:15 +1000

    start of html host

---
+++ src/host-html/HtmlHost.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2010-2011 Zipline Games, Inc. All Rights Reserved.
+// http://getmoai.com
+
+#ifndef	HTMLHOST
+#define	HTMLHOST
+
+//----------------------------------------------------------------//
+int		HtmlHost				( int argc, char** arg );
+void	HtmlRefreshContext		();
+
+#endif
