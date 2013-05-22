f0dc5381386d74b2a9a07aebecb6f5faebfae8da
JayAustria <jay@mindsnacks.com>
2013-05-21 17:05:12 -0700

    Cleaning up code a bit to match moai conventions

--- src/uslscore/USInterpolate.cpp
+++ src/uslscore/USInterpolate.cpp
@@ -81,12 +81,12 @@ float USInterpolate::Curve ( u32 mode, float t ) {
 		{
 			const float overshoot = 1.70158f;
 			t = t - 1;
-			return t * t * ((overshoot + 1) * t + overshoot) + 1;
+			return t * t * ( ( overshoot + 1 ) * t + overshoot ) + 1;
 		}
 		case kBackEaseOut:
 		{
 			const float overshoot = 1.70158f;
-			return t * t * ((overshoot + 1) * t - overshoot);
+			return t * t * ( ( overshoot + 1 ) * t - overshoot );
 		}
 		case kBackSmooth:
 		{
@@ -94,23 +94,26 @@ float USInterpolate::Curve ( u32 mode, float t ) {
 			
 			t = t * 2;
 			
-			if (t < 1) {
-				return (t * t * ((overshoot + 1) * t - overshoot)) / 2;
+			if ( t < 1 ) {
+				return ( t * t * ( ( overshoot + 1 ) * t - overshoot )) / 2;
 			}
 			else {
 				t = t - 2;
-				return (t * t * ((overshoot + 1) * t + overshoot)) / 2 + 1;
+				return ( t * t * ( ( overshoot + 1 ) * t + overshoot )) / 2 + 1;
 			}
 		}
 			
 		case kSineEaseIn:
-			return sinf(t * (float)M_PI_2);
+			
+			return sinf( t * (float)M_PI_2 );
 			
 		case kSineEaseOut:
-			return -1 * cosf(t * (float)M_PI_2) + 1;
+			
+			return -1 * cosf( t * (float)M_PI_2 ) + 1;
 			
 		case kSineSmooth:
-			return -0.5f*(cosf( (float)M_PI*t) - 1);
+			
+			return -0.5f * ( cosf( (float)M_PI * t ) - 1 );
 	}
 
 	return 0.0f;
