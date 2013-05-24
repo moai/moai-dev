f88a8dc46e83dd107f615a9e1fa75aec3d192f87
Aaron Barrett <aaron@mindsnacks.com>
2013-05-24 15:26:10 -0700

    Removed the ATTR_A_COL attribute and replaced it with ATTR_OPACITY.  Alpha and alpha-multiplied color channels are no longer exposed to Lua; only opacity and the unmultiplied channels, as in Cocos2D.  Removed seekColor and moveColor because they would break, and they're silly anyway.  ADD_COLOR, INHERIT_COLOR, and COLOR_TRAIT might also be broken, but I have no idea how they work.  Lulz.

--- src/moaicore/MOAIColor.cpp
+++ src/moaicore/MOAIColor.cpp
@@ -11,107 +11,39 @@
 //================================================================//
 
 //----------------------------------------------------------------//
-/**	@name	moveColor
-	@text	Animate the color by applying a delta. Creates and returns
-			a MOAIEaseDriver initialized to apply the delta.
+/**	@name	getColor
+ @text	Returns the current color.
+ 
+ @in		MOAIColor self
+ @out	number r
+ @out	number g
+ @out	number b
+ @out	number opacity
+ */
+int	MOAIColor::_getColor ( lua_State* L ) {
+	MOAI_LUA_SETUP ( MOAIColor, "U" )
 	
-	@in		MOAIColor self
-	@in		number rDelta		Delta to be added to r.
-	@in		number gDelta		Delta to be added to g.
-	@in		number bDelta		Delta to be added to b.
-	@in		number aDelta		Delta to be added to a.
-	@in		number length		Length of animation in seconds.
-	@opt	number mode			The ease mode. One of MOAIEaseType.EASE_IN, MOAIEaseType.EASE_OUT, MOAIEaseType.FLAT MOAIEaseType.LINEAR,
-								MOAIEaseType.SMOOTH, MOAIEaseType.SOFT_EASE_IN, MOAIEaseType.SOFT_EASE_OUT, MOAIEaseType.SOFT_SMOOTH,
-								MOAIEaseType.BACK_EASE_IN, MOAIEaseType.BACK_EASE_OUT, MOAIEaseType.BACK_SMOOTH, MOAIEaseType.SINE_EASE_IN,
-								MOAIEaseType.SINE_EASE_OUT, or MOAIEaseType.SINE_SMOOTH. Defaults to MOAIEaseType.SMOOTH.
-	@out	MOAIEaseDriver easeDriver
-*/
-int MOAIColor::_moveColor ( lua_State* L ) {
-	MOAI_LUA_SETUP ( MOAIColor, "UNNNNN" )
-
-	float delay		= state.GetValue < float >( 6, 0.0f );
+	lua_pushnumber ( state, self->mUnmultipliedR );
+	lua_pushnumber ( state, self->mUnmultipliedG );
+	lua_pushnumber ( state, self->mUnmultipliedB );
+	lua_pushnumber ( state, self->mA );
 	
-	if ( delay > 0.0f ) {
-	
-		u32 mode = state.GetValue < u32 >( 7, USInterpolate::kSmooth );
-		
-		MOAIEaseDriver* action = new MOAIEaseDriver ();
-		
-		action->ParseForMove ( state, 2, self, 4, mode,
-			MOAIColorAttr::Pack ( ATTR_R_COL ), 0.0f,
-			MOAIColorAttr::Pack ( ATTR_G_COL ), 0.0f,
-			MOAIColorAttr::Pack ( ATTR_B_COL ), 0.0f,
-			MOAIColorAttr::Pack ( ATTR_A_COL ), 0.0f
-		);
-		
-		action->SetSpan ( delay );
-		action->Start ();
-		action->PushLuaUserdata ( state );
-
-		return 1;
-	}
-	
-	self->mR += state.GetValue < float >( 2, 0.0f );
-	self->mG += state.GetValue < float >( 3, 0.0f );
-	self->mB += state.GetValue < float >( 4, 0.0f );
-	self->mA += state.GetValue < float >( 5, 0.0f );
-	self->ScheduleUpdate ();
-	
-	return 0;
+	return 4;
 }
 
 //----------------------------------------------------------------//
-/**	@name	seekColor
-	@text	Animate the color by applying a delta. Delta is computed
-			given a target value. Creates and returns a MOAIEaseDriver
-			initialized to apply the delta.
+/**	@name	getOpacity
+ @text	Returns the current opacity.
+ 
+ @in		MOAIColor self
+ @out	number opacity
+ */
+int MOAIColor::_getOpacity( lua_State* L ) {
+	MOAI_LUA_SETUP ( MOAIColor, "U" )
 	
-	@in		MOAIColor self
-	@in		number rGoal		Desired resulting value for r.
-	@in		number gGoal		Desired resulting value for g.
-	@in		number bGoal		Desired resulting value for b.
-	@in		number aGoal		Desired resulting value for a.
-	@in		number length		Length of animation in seconds.
-	@opt	number mode			The ease mode. One of MOAIEaseType.EASE_IN, MOAIEaseType.EASE_OUT, MOAIEaseType.FLAT MOAIEaseType.LINEAR,
-								MOAIEaseType.SMOOTH, MOAIEaseType.SOFT_EASE_IN, MOAIEaseType.SOFT_EASE_OUT, MOAIEaseType.SOFT_SMOOTH,
-								MOAIEaseType.BACK_EASE_IN, MOAIEaseType.BACK_EASE_OUT, MOAIEaseType.BACK_SMOOTH, MOAIEaseType.SINE_EASE_IN,
-								MOAIEaseType.SINE_EASE_OUT, or MOAIEaseType.SINE_SMOOTH. Defaults to MOAIEaseType.SMOOTH.
-	@out	MOAIEaseDriver easeDriver
-*/
-int MOAIColor::_seekColor ( lua_State* L ) {
-	MOAI_LUA_SETUP ( MOAIColor, "UNNNNN" )
-
-	float delay		= state.GetValue < float >( 6, 0.0f );
+	lua_pushnumber ( state, self->mA );
 	
-	
-	if ( delay > 0.0f ) {
-	
-		u32 mode = state.GetValue < u32 >( 7, USInterpolate::kSmooth );
-		
-		MOAIEaseDriver* action = new MOAIEaseDriver ();
-		
-		action->ParseForSeek ( state, 2, self, 4, mode,
-			MOAIColorAttr::Pack ( ATTR_R_COL ), self->mR, 0.0f,
-			MOAIColorAttr::Pack ( ATTR_G_COL ), self->mG, 0.0f,
-			MOAIColorAttr::Pack ( ATTR_B_COL ), self->mB, 0.0f,
-			MOAIColorAttr::Pack ( ATTR_A_COL ), self->mA, 0.0f
-		);
-		
-		action->SetSpan ( delay );
-		action->Start ();
-		action->PushLuaUserdata ( state );
-
-		return 1;
-	}
-	
-	self->mR = state.GetValue < float >( 2, 0.0f );
-	self->mG = state.GetValue < float >( 3, 0.0f );
-	self->mB = state.GetValue < float >( 4, 0.0f );
-	self->mA = state.GetValue < float >( 5, 0.0f );
-	self->ScheduleUpdate ();
-	
-	return 0;
+	return 1;
 }
 
 //----------------------------------------------------------------//
@@ -122,7 +54,7 @@ int MOAIColor::_seekColor ( lua_State* L ) {
 	@in		number r	Default value is 0.
 	@in		number g	Default value is 0.
 	@in		number b	Default value is 0.
-	@opt	number a	Default value is 1.
+	@opt	number o	Default value is 1.
 	@out	nil
 */
 int MOAIColor::_setColor ( lua_State* L ) {
@@ -131,11 +63,34 @@ int MOAIColor::_setColor ( lua_State* L ) {
 	float r = state.GetValue < float >( 2, 0.0f );
 	float g = state.GetValue < float >( 3, 0.0f );
 	float b = state.GetValue < float >( 4, 0.0f );
-	float a = state.GetValue < float >( 5, 1.0f );
-
-	self->Set ( r, g, b, a );
+	float o = state.GetValue < float >( 5, 1.0f );
+	
+	self->mUnmultipliedR = r;
+	self->mUnmultipliedG = g;
+	self->mUnmultipliedB = b;
+	
+	self->Set ( r * o, g * o, b * o, o );
 	self->ScheduleUpdate ();
 
+	return 0;
+}
+
+//----------------------------------------------------------------//
+/**	@name	setOpacity
+ @text	Set the opacity.
+ 
+ @in		MOAIColor self
+ @in		number o	Default value is 0.
+ @out	nil
+ */
+int MOAIColor::_setOpacity ( lua_State* L ) {
+	MOAI_LUA_SETUP ( MOAIColor, "UN" )
+	
+	float o = state.GetValue < float >( 2, 1.0f );
+	
+	self->Set ( self->mUnmultipliedR * o, self->mUnmultipliedG * o, self->mUnmultipliedB * o, o );
+	self->ScheduleUpdate ();
+	
 	return 0;
 }
 
@@ -170,17 +125,37 @@ bool MOAIColor::ApplyAttrOp ( u32 attrID, MOAIAttrOp& attrOp, u32 op ) {
 
 		switch ( UNPACK_ATTR ( attrID )) {
 			case ATTR_R_COL:
-				this->mR = USFloat::Clamp ( attrOp.Apply ( this->mR, op, MOAIAttrOp::ATTR_READ_WRITE ), 0.0f, 1.0f );
+			{
+				float r = USFloat::Clamp ( attrOp.Apply ( this->mUnmultipliedR, op, MOAIAttrOp::ATTR_READ_WRITE ), 0.0f, 1.0f );
+				this->mUnmultipliedR = r;
+				this->mR = r * this->mA;
 				return true;
+			}
 			case ATTR_G_COL:
-				this->mG = USFloat::Clamp ( attrOp.Apply ( this->mG, op, MOAIAttrOp::ATTR_READ_WRITE ), 0.0f, 1.0f );
+			{
+				float g = USFloat::Clamp ( attrOp.Apply ( this->mUnmultipliedG, op, MOAIAttrOp::ATTR_READ_WRITE ), 0.0f, 1.0f );
+				this->mUnmultipliedG = g;
+				this->mG = g * this->mA;
 				return true;
+			}
 			case ATTR_B_COL:
-				this->mB = USFloat::Clamp ( attrOp.Apply ( this->mB, op, MOAIAttrOp::ATTR_READ_WRITE ), 0.0f, 1.0f );
+			{
+				float b = USFloat::Clamp ( attrOp.Apply ( this->mUnmultipliedB, op, MOAIAttrOp::ATTR_READ_WRITE ), 0.0f, 1.0f );
+				this->mUnmultipliedB = b;
+				this->mB = b * this->mA;
 				return true;
-			case ATTR_A_COL:
-				this->mA = USFloat::Clamp ( attrOp.Apply ( this->mA, op, MOAIAttrOp::ATTR_READ_WRITE ), 0.0f, 1.0f );
+			}
+			case ATTR_OPACITY:
+			{
+				float o = USFloat::Clamp ( attrOp.Apply ( this->mA, op, MOAIAttrOp::ATTR_READ_WRITE ), 0.0f, 1.0f );
+				this->mA = o;
+				
+				this->mR = this->mUnmultipliedR * o;
+				this->mG = this->mUnmultipliedG * o;
+				this->mB = this->mUnmultipliedB * o;
+				
 				return true;
+			}
 			case COLOR_TRAIT:
 				attrOp.ApplyNoAdd < USColorVec* >( &this->mColor, op, MOAIAttrOp::ATTR_READ );
 				return true;
@@ -201,6 +176,10 @@ MOAIColor::MOAIColor () {
 	RTTI_BEGIN
 		RTTI_EXTEND ( MOAINode )
 	RTTI_END
+	
+	this->mUnmultipliedR = 1.0f;
+	this->mUnmultipliedG = 1.0f;
+	this->mUnmultipliedB = 1.0f;
 	
 	this->Set ( 1.0f, 1.0f, 1.0f, 1.0f );
 	this->mColor.Set ( 1.0f, 1.0f, 1.0f, 1.0f );
@@ -236,7 +215,7 @@ void MOAIColor::RegisterLuaClass ( MOAILuaState& state ) {
 	state.SetField ( -1, "ATTR_R_COL", MOAIColorAttr::Pack ( ATTR_R_COL ));
 	state.SetField ( -1, "ATTR_G_COL", MOAIColorAttr::Pack ( ATTR_G_COL ));
 	state.SetField ( -1, "ATTR_B_COL", MOAIColorAttr::Pack ( ATTR_B_COL ));
-	state.SetField ( -1, "ATTR_A_COL", MOAIColorAttr::Pack ( ATTR_A_COL ));
+	state.SetField ( -1, "ATTR_OPACITY", MOAIColorAttr::Pack ( ATTR_OPACITY ));
 	
 	state.SetField ( -1, "ADD_COLOR", MOAIColorAttr::Pack ( ADD_COLOR ));
 	state.SetField ( -1, "INHERIT_COLOR", MOAIColorAttr::Pack ( INHERIT_COLOR ));
@@ -249,9 +228,10 @@ void MOAIColor::RegisterLuaFuncs ( MOAILuaState& state ) {
 	MOAINode::RegisterLuaFuncs ( state );
 	
 	luaL_Reg regTable [] = {
-		{ "moveColor",				_moveColor },
-		{ "seekColor",				_seekColor },
+		{ "getColor",				_getColor },
+		{ "getOpacity",				_getOpacity},
 		{ "setColor",				_setColor },
+		{ "setOpacity",				_setOpacity },
 		{ "setParent",				_setParent },
 		{ NULL, NULL }
 	};
