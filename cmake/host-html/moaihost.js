d6889d24a59b3ba9f3e6094902c2d9b2cece33e7
Halfnelson <dpershouse@gmail.com>
2013-05-26 23:44:45 +1000

    fixed keyboard support, now has shift key

--- cmake/host-html/moaihost.js
+++ cmake/host-html/moaihost.js
@@ -8,6 +8,8 @@ moaijs.AKURunString = Module.cwrap('AKURunString','number',['string']);
 moaijs.AKURunScript = Module.cwrap('AKURunScript','number',['string']);
 moaijs.AKUSetWorkingDirectory = Module.cwrap('AKUSetWorkingDirectory','number',['string']);
 moaijs.AKUGetSimStep = Module.cwrap('AKUGetSimStep','number',null);
+moaijs.AKUEnqueueKeyboardShiftEvent = Module.cwrap('AKUEnqueueKeyboardShiftEvent','number',['number','number','number']);
+
 
 moaijs.onPaint = Module.cwrap('onPaint','number',null);
 
@@ -46,21 +48,75 @@ moaijs.mousemove = function(e) {
     moaijs.onMouseMove(canX,canY);
 }
 
+
+var getASCIIKey= function(event) {
+      if (event['ctrlKey'] || event['altKey'] || event['metaKey']) return null;
+
+      var keycode = event['keyCode'];
+
+      /* The exact list is soooo hard to find in a canonical place! */
+
+      if (48 <= keycode && keycode <= 57)
+        return keycode; // numeric  TODO handle shift?
+      if (65 <= keycode && keycode <= 90)
+	return event['shiftKey'] ? keycode : keycode + 32;
+      if (106 <= keycode && keycode <= 111)
+	return keycode - 106 + 42; // *,+-./  TODO handle shift?
+
+      switch (keycode) {
+        case 27: // escape
+        case 32: // space
+        case 61: // equal
+          return keycode;
+      }
+
+      var s = event['shiftKey'];
+      switch (keycode) {
+        case 186: return s ? 58 : 59; // colon / semi-colon
+        case 187: return s ? 43 : 61; // add / equal (these two may be wrong)
+        case 188: return s ? 60 : 44; // less-than / comma
+        case 189: return s ? 95 : 45; // dash
+        case 190: return s ? 62 : 46; // greater-than / period
+        case 191: return s ? 63 : 47; // forward slash
+        case 219: return s ? 123 : 91; // open bracket
+        case 220: return s ? 124 : 47; // back slash
+        case 221: return s ? 125 : 93; // close braket
+        case 222: return s ? 34 : 39; // single quote
+      }
+
+      return null;
+    };
+
+
+
+
 moaijs.keydown = function(e) {
 
 	console.log("keydown",e.keyCode);
-	moaijs.onKeyDown(e.keyCode);
+	if (e.keycode == 16) {
+		moaijs.AKUEnqueueKeyboardShiftEvent(0,0,1); //device, keyboard, true
+	} else {
+		var key = getASCIIKey(e);
+		moaijs.onKeyDown(key?key:e.keyCode);
+	}
+	e.preventDefault();
+
 	if (e.keyCode == 8) {
-		e.preventDefault();
-		e.stopPropagation();
         return false; //eat backspace !
     }
 }
 
 moaijs.keyup = function(e) {
 	console.log("keyup",e.keyCode);
-	moaijs.onKeyUp(e.keyCode);
+	if (e.keyCode == 16) {
+		moaijs.AKUEnqueueKeyboardShiftEvent(0,0,0); //device, keyboard, false
+	} else {
+		var key = getASCIIKey(e);
+		moaijs.onKeyUp(key?key:e.keyCode);
+	}
+	e.preventDefault();
 }
+
 moaijs.keypress = function(e) {
 	if (e.keyCode == 8) {
 		return false;
@@ -84,7 +140,7 @@ moaijs.OpenWindowFunc = function(title,width,height) {
 
 	//grab focus on hover
 	canvas.addEventListener("mouseover",function() { canvas.focus(); },false);
-	canvas.addEventListener("mouseout",function() { canvas.blur(); },false);
+	canvas.addEventListener("mouseout",function() { canvas.blur(); moaijs.mouseup(); },false);
 
 	//grab keys
 	canvas.addEventListener("keydown",moaijs.keydown,false);
