a19b3ec7f4bf7019797541507c6acd2eb7298a09
Isaac Barrett <ikebart9999@gmail.com>
2013-05-29 17:44:42 -0400

    Added some function signatures and member variables to MOAIFreeTypeFont

--- src/moaicore/MOAIFreeTypeFont.cpp
+++ src/moaicore/MOAIFreeTypeFont.cpp
@@ -1,9 +1,35 @@
-//
-//  MOAIFreeTypeFont.cpp
-//  libmoai
-//
-//  Created by Isaac Barrett on 5/29/13.
-//
-//
+// Copyright (c) 2010-2011 Zipline Games, Inc. All Rights Reserved.
+// http://getmoai.com
 
-#include "MOAIFreeTypeFont.h"
+
+
+#include "pch.h"
+#include <contrib/utf8.h>
+#include <moaicore/MOAIFreeTypeFont.h>
+
+//================================================================//
+// local
+//================================================================//
+int	MOAIFreeTypeFont::_load(lua_State *L){
+	MOAI_LUA_SETUP ( MOAIFreeTypeFont, "US" );
+	
+	return 0;
+}
+
+int	MOAIFreeTypeFont::_setReader	( lua_State* L ){
+	MOAI_LUA_SETUP ( MOAIFreeTypeFont, "U" );
+	
+	return 0;
+}
+
+//================================================================//
+// MOAIFreeTypeFont
+//================================================================//
+
+void MOAIFreeTypeFont::RegisterLuaClass ( MOAILuaState& state ) {
+	UNUSED(state);
+}
+
+void MOAIFreeTypeFont::RegisterLuaFuncs(MOAILuaState &state){
+	UNUSED(state);
+}
--- src/moaicore/MOAIFreeTypeFont.h
+++ src/moaicore/MOAIFreeTypeFont.h
@@ -7,6 +7,8 @@
 
 #include <moaicore/MOAILua.h>
 
+class MOAIFontReader;
+
 #define DPI 72
 #define POINTS_TO_PIXELS(points,dpi) (( points * dpi ) / DPI )
 #define PIXELS_TO_POINTS(pixels,dpi) (( pixels * DPI ) / dpi )
@@ -23,10 +25,27 @@
 class MOAIFreeTypeFont :
 	public MOAILuaObject {
 protected:
+	STLString mFilename;
+	u32 mFlags;
+		
+	MOAILuaSharedPtr < MOAIFontReader > mReader;
+		
+	float mDefaultSize;
+		
+		
+	//----------------------------------------------------------------//
+	static int			_load					( lua_State* L );
+	static int			_setReader				( lua_State* L );
+	//----------------------------------------------------------------//
+		
 		
 public:
+		
+		
+	void				RegisterLuaClass		( MOAILuaState& state );
+	void				RegisterLuaFuncs		( MOAILuaState& state );
 
-}
+};
 
 
 #endif /* defined(MOAIFREETYPEFONT_H) */
