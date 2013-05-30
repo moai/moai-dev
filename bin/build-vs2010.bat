2bdf5920fb7da82b1bd6eb48fdf03c238994597d
Francisco Tufró <contacto@franciscotufro.com.ar>
2013-05-30 17:23:40 -0300

    [CI] Avoid cleaning Visual Studio builds, it was taking forever.

--- bin/build-vs2010.bat
+++ bin/build-vs2010.bat
@@ -1,20 +1,20 @@
 call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 cd vs2010
 
-msbuild moai.sln /t:Clean,Build /property:Config=Debug-Lua-5.1
+msbuild moai.sln /property:Config=Debug-Lua-5.1
 if ERRORLEVEL 1 exit
 
-msbuild moai.sln /t:Clean,Build /property:Config=Release-Lua-5.1
+msbuild moai.sln /property:Config=Release-Lua-5.1
 if ERRORLEVEL 1 exit
 
-msbuild moai.sln /t:Clean,Build /property:Config=Debug-Lua-5.2
+msbuild moai.sln /property:Config=Debug-Lua-5.2
 if ERRORLEVEL 1 exit
 
-msbuild moai.sln /t:Clean,Build /property:Config=Release-Lua-5.2
+msbuild moai.sln /property:Config=Release-Lua-5.2
 if ERRORLEVEL 1 exit
 
-msbuild moai.sln /t:Clean,Build /property:Config=Debug-LuaJIT
+msbuild moai.sln /property:Config=Debug-LuaJIT
 if ERRORLEVEL 1 exit
 
-msbuild moai.sln /t:Clean,Build /property:Config=Release-LuaJIT
+msbuild moai.sln /property:Config=Release-LuaJIT
 if ERRORLEVEL 1 exit
