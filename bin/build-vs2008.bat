83e2bebf14830a4645597529399a8153bd698d23
Francisco Tufró <contacto@franciscotufro.com.ar>
2013-06-04 14:57:11 -0300

    [CI] Copy artifacts on visual studio builds to release/

--- bin/build-vs2008.bat
+++ bin/build-vs2008.bat
@@ -18,3 +18,29 @@ if ERRORLEVEL 1 exit
 
 msbuild moai.sln /property:Configuration=Release-LuaJIT
 if ERRORLEVEL 1 exit
+
+cd ..
+
+rmdir release /s
+
+mkdir release
+mkdir release/vs2008
+mkdir release/vs2008/bin/
+mkdir release/vs2008/bin/Lua-5.1/
+mkdir release/vs2008/bin/Lua-5.2/
+mkdir release/vs2008/bin/Lua-JIT/
+
+cp vs2008\bin\Win32\Release-Lua-5.1\moai.exe release/vs2008/bin/Lua-5.1/
+cp vs2008\bin\Win32\Release-Lua-5.1\glut32.dll release/vs2008/bin/Lua-5.1/
+cp vs2008\bin\Win32\Release-Lua-5.2\moai.exe release/vs2008/bin/Lua-5.2/
+cp vs2008\bin\Win32\Release-Lua-5.2\glut32.dll release/vs2008/bin/Lua-5.2/
+cp vs2008\bin\Win32\Release-LuaJIT\moai.exe release/vs2008/bin/Lua-JIT/
+cp vs2008\bin\Win32\Release-LuaJIT\glut32.dll release/vs2008/bin/Lua-JIT/
+
+mkdir release/vs2008/lib/
+mkdir release/vs2008/lib/Lua-5.1/
+mkdir release/vs2008/lib/Lua-5.2/
+mkdir release/vs2008/lib/Lua-JIT/
+cp vs2008\bin\Win32\Release-Lua-5.1\*.lib release/vs2008/lib/Lua-5.1/
+cp vs2008\bin\Win32\Release-Lua-5.2\*.lib release/vs2008/lib/Lua-5.2/
+cp vs2008\bin\Win32\Release-LuaJIT\*.lib release/vs2008/lib/Lua-JIT/
--- bin/build-vs2010.bat
+++ bin/build-vs2010.bat
@@ -18,3 +18,29 @@ if ERRORLEVEL 1 exit
 
 msbuild moai.sln /property:Configuration=Release-LuaJIT
 if ERRORLEVEL 1 exit
+
+cd ..
+
+rmdir release /s
+
+mkdir release
+mkdir release/vs2010
+mkdir release/vs2010/bin/
+mkdir release/vs2010/bin/Lua-5.1/
+mkdir release/vs2010/bin/Lua-5.2/
+mkdir release/vs2010/bin/Lua-JIT/
+
+cp vs2010\bin\Win32\Release-Lua-5.1\moai.exe release/vs2010/bin/Lua-5.1/
+cp vs2010\bin\Win32\Release-Lua-5.1\glut32.dll release/vs2010/bin/Lua-5.1/
+cp vs2010\bin\Win32\Release-Lua-5.2\moai.exe release/vs2010/bin/Lua-5.2/
+cp vs2010\bin\Win32\Release-Lua-5.2\glut32.dll release/vs2010/bin/Lua-5.2/
+cp vs2010\bin\Win32\Release-LuaJIT\moai.exe release/vs2010/bin/Lua-JIT/
+cp vs2010\bin\Win32\Release-LuaJIT\glut32.dll release/vs2010/bin/Lua-JIT/
+
+mkdir release/vs2010/lib/
+mkdir release/vs2010/lib/Lua-5.1/
+mkdir release/vs2010/lib/Lua-5.2/
+mkdir release/vs2010/lib/Lua-JIT/
+cp vs2010\bin\Win32\Release-Lua-5.1\*.lib release/vs2010/lib/Lua-5.1/
+cp vs2010\bin\Win32\Release-Lua-5.2\*.lib release/vs2010/lib/Lua-5.2/
+cp vs2010\bin\Win32\Release-LuaJIT\*.lib release/vs2010/lib/Lua-JIT/
