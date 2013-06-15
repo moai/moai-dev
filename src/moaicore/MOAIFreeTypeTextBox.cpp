4469bd07456beeedd2db4acfc2e71c6f11aefa65
Isaac Barrett <ikebart9999@gmail.com>
2013-06-14 20:38:07 -0400

    Swapped width and height arguments in call to InitBitmapData in GenerateTexture. Fixed rendering errors when using non-square textures.

--- src/moaicore/MOAIFreeTypeTextBox.cpp
+++ src/moaicore/MOAIFreeTypeTextBox.cpp
@@ -162,7 +162,7 @@ void MOAIFreeTypeTextBox::BuildLayout(){
 	FT_Int imgHeight = (FT_Int)height;
 
 	// create the image data buffer
-	MOAIFreeTypeImageBuffer imageBuffer = InitBitmapData(imgHeight, imgWidth);
+	MOAIFreeTypeImageBuffer imageBuffer = InitBitmapData(imgWidth, imgHeight);
 
 	// create the lines of text
 	vector<MOAIFreeTypeTextLine> lines = GenerateLines(face, imgWidth, text, wordbreak);
