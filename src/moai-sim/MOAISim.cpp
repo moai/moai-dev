be78e6d7f386d9ded8e528cc035737b6a5e14ce6
Patrick Meehan <patrick@ziplinegames.com>
2013-10-16 13:53:20 -0700

    moved incremental gc call to stepSim

--- src/moai-sim/MOAISim.cpp
+++ src/moai-sim/MOAISim.cpp
@@ -923,12 +923,26 @@ double MOAISim::StepSim ( double step, u32 multiplier ) {
 
 	double time = ZLDeviceTime::GetTimeInSeconds ();
 
+	MOAIScopedLuaState state = MOAILuaRuntime::Get ().State ();
+
 	for ( u32 s = 0; s < multiplier; ++s ) {
-			
+
+		if ( this->mForceGC ) {
+			MOAILuaRuntime::Get ().ForceGarbageCollection ();
+			this->mForceGC = false;
+		}
+		
+		lua_gc ( state, LUA_GCSTOP, 0 );
+		
 		MOAIInputMgr::Get ().Update ();
 		MOAIActionMgr::Get ().Update (( float )step );		
 		MOAINodeMgr::Get ().Update ();
 		this->mSimTime += step;
+		
+		if ( this->mGCActive ) {
+			// crank the garbage collector
+			lua_gc ( state, LUA_GCSTEP, this->mGCStep );
+		}
 	}
 
 	return ZLDeviceTime::GetTimeInSeconds () - time;
@@ -948,13 +962,6 @@ void MOAISim::Update () {
 		lua_pushcfunction ( state, _collectgarbage );
 		lua_setglobal ( state, LUA_GC_FUNC_NAME );
 	}
-
-	if ( this->mForceGC ) {
-		MOAILuaRuntime::Get ().ForceGarbageCollection ();
-		this->mForceGC = false;
-	}
-
-	lua_gc ( state, LUA_GCSTOP, 0 );
 
 	// Measure performance
 	double simStartTime = ZLDeviceTime::GetTimeInSeconds ();
@@ -1088,9 +1095,4 @@ void MOAISim::Update () {
 	// Measure performance
 	double simEndTime = ZLDeviceTime::GetTimeInSeconds ();
 	this->mSimDuration = simEndTime - simStartTime;
-	
-	if ( this->mGCActive ) {
-		// crank the garbage collector
-		lua_gc ( state, LUA_GCSTEP, this->mGCStep );
-	}
 }
