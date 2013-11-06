186e19f62b0bce30ffc049b9048bfa7a1ecf09a2
Isaac Barrett <ikebart9999@gmail.com>
2013-11-06 14:15:08 -0500

    Added basic functions and member variables to MOAITextRenderer.
    Conflicts:
    	xcode/libmoai/libmoai.xcodeproj/project.pbxproj

--- src/moaicore/MOAITextRenderer.cpp
+++ src/moaicore/MOAITextRenderer.cpp
@@ -6,4 +6,77 @@
 //
 //
 
-#include "MOAITextRenderer.h"
+#include "pch.h"
+#include <moaicore/MOAITextRenderer.h>
+#include <moaicore/MOAIFreeTypeFont.h>
+
+//================================================================//
+// local
+//================================================================//
+//----------------------------------------------------------------//
+int	MOAITextRenderer::_render ( lua_State *L ){
+	return 0;
+}
+
+//----------------------------------------------------------------//
+int MOAITextRenderer::_renderSingleLine ( lua_State *L ){
+	return 0;
+}
+
+//----------------------------------------------------------------//
+int MOAITextRenderer::_setAlignment ( lua_State *L ){
+	return 0;
+}
+
+//----------------------------------------------------------------//
+int MOAITextRenderer::_setFont ( lua_State *L ){
+	return 0;
+}
+
+//----------------------------------------------------------------//
+int MOAITextRenderer::_setFontSize ( lua_State *L ){
+	return 0;
+}
+
+//----------------------------------------------------------------//
+int MOAITextRenderer::_setHeight ( lua_State *L ){
+	return 0;
+}
+
+//----------------------------------------------------------------//
+int MOAITextRenderer::_setReturnGlyphBounds ( lua_State *L ){
+	return 0;
+}
+
+//----------------------------------------------------------------//
+int MOAITextRenderer::_setWidth ( lua_State *L ){
+	return 0;
+}
+
+//----------------------------------------------------------------//
+
+int MOAITextRenderer::_setWordBreak ( lua_State *L ){
+	return 0;
+}
+
+//----------------------------------------------------------------//
+
+MOAITextRenderer::MOAITextRenderer ( ) {
+	
+}
+
+//----------------------------------------------------------------//
+MOAITextRenderer::~MOAITextRenderer () {
+	
+}
+
+//----------------------------------------------------------------//
+void MOAITextRenderer::RegisterLuaClass ( MOAILuaState &state ) {
+	
+}
+
+//----------------------------------------------------------------//
+void MOAITextRenderer::RegisterLuaFuncs ( MOAILuaState &state ) {
+	
+}
+
--- src/moaicore/MOAITextRenderer.h
+++ src/moaicore/MOAITextRenderer.h
@@ -17,12 +17,38 @@ class MOAITextRenderer : public MOAILuaObject {
 private:
 	MOAILuaSharedPtr <MOAIFreeTypeFont>	mFont;
 	
-	static int			_renderTexture			( lua_State* L );
-	static int			_renderTextureSingleLine( lua_State* L );
+	float			mFontSize;
+	
+	float			mWidth;
+	float			mHeight;
+	
+	int				mHorizontalAlignment;
+	int				mVerticalAlignment;
+	int				mWordBreak;
+	
+	bool			mReturnGlyphBounds;
+	
+	
+	static int			_render					( lua_State* L );
+	static int			_renderSingleLine		( lua_State* L );
+	static int			_setAlignment			( lua_State* L );
 	static int			_setFont				( lua_State* L );
+	static int			_setFontSize			( lua_State* L );
+	static int			_setHeight				( lua_State* L );
+	static int			_setReturnGlyphBounds	( lua_State* L );
+	static int			_setWidth				( lua_State* L );
+	static int			_setWordBreak			( lua_State* L );
+	
 	
 public:
 	DECL_LUA_FACTORY ( MOAIFreeTypeFont )
+	
+	
+	MOAITextRenderer ();
+	~MOAITextRenderer ();
+	
+	void				RegisterLuaClass		( MOAILuaState& state );
+	void				RegisterLuaFuncs		( MOAILuaState& state );
 };
 
 #endif /* defined(MOAITEXTRENDERER_H) */
