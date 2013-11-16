a5e1128984de242021e6846f84487d717d58234c
Adam Villalobos <adam@ziplinegames.com>
2013-11-15 17:51:47 -0800

    Fixing missing logs on Android

--- src/moai-core/MOAILogMgr.cpp
+++ src/moai-core/MOAILogMgr.cpp
@@ -14,28 +14,28 @@
 
 //----------------------------------------------------------------//
 void MOAILog ( lua_State *L, u32 messageID, ... ) {
-	
+
 	if ( MOAILogMgr::IsValid ()) {
-	
+
 		va_list args;
 		va_start ( args, messageID );
-		
+
 		MOAILogMgr::Get ().LogVar ( L, messageID, args );
-		
+
 		va_end ( args );
 	}
 }
 
 //----------------------------------------------------------------//
 void MOAIPrint ( cc8* message, ... ) {
-	
+
 	if ( MOAILogMgr::IsValid ()) {
-	
+
 		va_list args;
 		va_start ( args, message );
-		
+
 		MOAILogMgr::Get ().PrintVar ( message, args );
-		
+
 		va_end ( args );
 	}
 }
@@ -47,14 +47,14 @@ void MOAIPrint ( cc8* message, ... ) {
 //----------------------------------------------------------------//
 /**	@name	closeFile
 	@text	Resets log output to stdout.
-	
+
 	@out	nil
 */
 int MOAILogMgr::_closeFile ( lua_State* L ) {
 	UNUSED ( L );
 
 	MOAILogMgr::Get ().CloseFile ();
-	
+
 	return 0;
 }
 
@@ -62,26 +62,26 @@ int MOAILogMgr::_closeFile ( lua_State* L ) {
 /**	@name	isDebugBuild
 	@text	Returns a boolean value indicating whether Moai has been
 			compiles as a debug build or not.
-	
+
 	@out	boolean isDebugBuild
 */
 int MOAILogMgr::_isDebugBuild ( lua_State* L ) {
 
 	MOAILuaState state ( L );
-	
+
 	#ifdef _DEBUG
 		lua_pushboolean ( state, true );
 	#else
 		lua_pushboolean ( state, false );
 	#endif
-	
+
 	return 1;
 }
 
 //----------------------------------------------------------------//
 /**	@name	log
 	@text	Alias for print.
-	
+
 	@in		string message
 	@out	nil
 */
@@ -91,19 +91,21 @@ int MOAILogMgr::_log ( lua_State* L ) {
 	if ( !state.CheckParams ( 1, "S" )) return 0;
 
 	cc8* msg = state.GetValue < cc8* >( 1, "" );
-	
+
 	// TODO: Fix this on Android
 	#ifndef MOAI_OS_ANDROID
 		ZLLog::PrintFile ( ZLLog::CONSOLE, msg );
+	#else
+		ZLLog::Print(msg);
 	#endif
-	
+
 	return 0;
 }
 
 //----------------------------------------------------------------//
 /**	@name	openFile
 	@text	Opens a new file to receive log messages.
-	
+
 	@in		string filename
 	@out	nil
 */
@@ -111,10 +113,10 @@ int MOAILogMgr::_openFile ( lua_State* L ) {
 
 	MOAILuaState state ( L );
 	if ( !state.CheckParams ( 1, "S" )) return 0;
-	
+
 	cc8* filename = state.GetValue < cc8* >( 1, "" );
 	MOAILogMgr::Get ().OpenFile ( filename );
-	
+
 	return 0;
 }
 
@@ -122,7 +124,7 @@ int MOAILogMgr::_openFile ( lua_State* L ) {
 /**	@name	registerLogMessage
 	@text	Register a format string to handle a log message. Register
 			an empty string to hide messages.
-	
+
 	@in		number messageID
 	@opt	string formatString		Default value is an empty string.
 	@opt	number level			One of MOAILogMgr.LOG_ERROR, MOAILogMgr.LOG_WARNING, MOAILogMgr.LOG_STATUS.
@@ -147,7 +149,7 @@ int MOAILogMgr::_registerLogMessage ( lua_State* L ) {
 //----------------------------------------------------------------//
 /**	@name	setLogLevel
 	@text	Set the logging level.
-	
+
 	@in		number logLevel		One of MOAILogMgr LOG_NONE, LOG_ERROR, LOG_WARNING, LOG_STATUS
 	@out	nil
 */
@@ -157,14 +159,14 @@ int MOAILogMgr::_setLogLevel ( lua_State* L ) {
 
 	u32 level = state.GetValue < u32 >( 1, LOG_NONE );
 	MOAILogMgr::Get ().mLevel = level;
-	
+
 	return 0;
 }
 
 //----------------------------------------------------------------//
 /**	@name	setTypeCheckLuaParams
 	@text	Set or clear type checking of parameters passed to lua bound Moai API functions.
-	
+
 	@opt	boolean check		Default value is false.
 	@out	nil
 */
@@ -174,7 +176,7 @@ int MOAILogMgr::_setTypeCheckLuaParams ( lua_State* L ) {
 
 	bool check = state.GetValue < bool >( 1, false );
 	MOAILogMgr::Get ().mTypeCheckLuaParams = check;
-	
+
 	return 0;
 }
 
@@ -194,12 +196,12 @@ void MOAILogMgr::CloseFile () {
 
 //----------------------------------------------------------------//
 void MOAILogMgr::Log ( lua_State *L, u32 messageID, ... ) {
-	
+
 	va_list args;
 	va_start ( args, messageID );
-	
+
 	MOAILogMgr::Get ().LogVar ( L, messageID, args );
-	
+
 	va_end ( args );
 }
 
@@ -210,18 +212,18 @@ void MOAILogMgr::LogVar ( lua_State *L, u32 messageID, va_list args ) {
 
 		MessageMapIt messageMapIt = this->mMessageMap.find ( messageID );
 		if ( messageMapIt != this->mMessageMap.end ()) {
-		
+
 			MOAILogMessage& message = messageMapIt->second;
-			
+
 			if ( message.mLevel <= this->mLevel ) {
-				
+
 				if ( L ) {
 					this->Print ( "----------------------------------------------------------------\n" );
 				}
-				
+
 				this->PrintVar ( message.mFormatString, args );
 				this->Print ( "\n" );
-				
+
 				if ( L ) {
 					MOAILuaState state ( L );
 					state.PrintStackTrace ( this->mFile, 0 );
@@ -264,9 +266,9 @@ void MOAILogMgr::Print ( cc8* message, ... ) {
 
 	va_list args;
 	va_start ( args, message );
-	
+
 	MOAILogMgr::Get ().PrintVar ( message, args );
-	
+
 	va_end ( args );
 }
 
@@ -282,7 +284,7 @@ void MOAILogMgr::RegisterLogMessage ( u32 messageID, u32 level, cc8* formatStrin
 	if ( strlen ( formatString )) {
 
 		MOAILogMessage& message = MOAILogMgr::Get ().mMessageMap [ messageID ];
-	
+
 		message.mLevel = level;
 		message.mFormatString = formatString;
 	}
@@ -298,9 +300,9 @@ void MOAILogMgr::RegisterLuaClass ( MOAILuaState& state ) {
 	state.SetField ( -1, "LOG_ERROR",			( u32 )LOG_ERROR );
 	state.SetField ( -1, "LOG_WARNING",			( u32 )LOG_WARNING );
 	state.SetField ( -1, "LOG_STATUS",			( u32 )LOG_STATUS );
-	
+
 	MOAILogMessages::RegisterLogMessageIDs ( state );
-	
+
 	luaL_Reg regTable[] = {
 		{ "closeFile",					_closeFile },
 		{ "log",						_log },
