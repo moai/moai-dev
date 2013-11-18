506d9e3123e6452c39d392dd8d458ae16609f7ef
Toni Rico <toni@mindsnacks.com>
2013-11-18 11:29:43 -0800

    Enabled ARC, Removed synthesize, Deleted TapJoy library and other auxiliary classes

--- src/aku/AKU-iphone.mm
+++ src/aku/AKU-iphone.mm
@@ -7,8 +7,6 @@
 #import <CoreTelephony/CTTelephonyNetworkInfo.h>
 #import <CoreTelephony/CTCarrier.h>
 
-#import <OpenUDID/MOAIOpenUDID.h>
-
 //-----------------------------------------------------------------//
 void AKUAppDidStartSession ( bool resumed ) {
 
@@ -31,37 +29,6 @@ void AKUAppWillEndSession () {
 	MOAIAppIOS::Get ().WillEndSession ();
 }
 
-//-----------------------------------------------------------------//
-long AKUGetIphoneNetworkReachability ( ) {
-
-	Reachability *reach = [ Reachability reachabilityForInternetConnection ];
-	NetworkStatus status = [ reach currentReachabilityStatus ];
-	
-	if ( status == NotReachable ) {
-		
-		return ( long )CONNECTION_TYPE_NONE;
-		
-	} else if ( status == ReachableViaWWAN ) {
-		
-		MOAIEnvironment& environment = MOAIEnvironment::Get ();
-		
-		// Update network information
-		CTCarrier* carrierInfo = [[[ CTTelephonyNetworkInfo alloc ] init ] subscriberCellularProvider ];
-		
-		environment.SetValue ( MOAI_ENV_carrierISOCountryCode,		[ carrierInfo.isoCountryCode UTF8String ]);
-		environment.SetValue ( MOAI_ENV_carrierMobileCountryCode,	[[ carrierInfo mobileCountryCode ] UTF8String ]);
-		environment.SetValue ( MOAI_ENV_carrierName,				[[ carrierInfo carrierName ] UTF8String ]);
-		environment.SetValue ( MOAI_ENV_carrierMobileNetworkCode,	[[ carrierInfo mobileNetworkCode ] UTF8String ]);
-		
-		return ( long )CONNECTION_TYPE_WWAN;
-		
-	} else if ( status == ReachableViaWiFi ) {
-		
-		return ( long )CONNECTION_TYPE_WIFI;
-	}
-	return ( long )CONNECTION_TYPE_NONE;
-}
-
 //----------------------------------------------------------------//
 void AKUIphoneInit ( UIApplication* application ) {
 
@@ -78,12 +45,10 @@ void AKUIphoneInit ( UIApplication* application ) {
 			
 	// MOAI
 	REGISTER_LUA_CLASS ( MOAIAppIOS )
-	REGISTER_LUA_CLASS ( MOAIBillingIOS )
 	REGISTER_LUA_CLASS ( MOAIDialogIOS )
 	REGISTER_LUA_CLASS ( MOAIGameCenterIOS )
 	REGISTER_LUA_CLASS ( MOAIKeyboardIOS )
 	REGISTER_LUA_CLASS ( MOAIMoviePlayerIOS )
-	REGISTER_LUA_CLASS ( MOAISafariIOS )
 	REGISTER_LUA_CLASS ( MOAIWebViewIOS )
 
 	#ifndef DISABLE_TWITTER
@@ -101,16 +66,10 @@ void AKUIphoneInit ( UIApplication* application ) {
 	#ifndef DISABLE_CRITTERCISM
 		REGISTER_LUA_CLASS ( MOAICrittercismIOS )
 	#endif
-		
-	#ifndef DISABLE_FACEBOOK
-		REGISTER_LUA_CLASS ( MOAIFacebookIOS )
-	#endif
 	
 	// Device properties
 	MOAIEnvironment& environment = MOAIEnvironment::Get ();
-	
-	environment.SetValue ( MOAI_ENV_connectionType, ( int ) AKUGetIphoneNetworkReachability() );
-	
+		
 	environment.SetValue ( MOAI_ENV_appDisplayName,		[[[[ NSBundle mainBundle ] infoDictionary ] objectForKey:@"CFBundleDisplayName" ] UTF8String ]);
 	environment.SetValue ( MOAI_ENV_appID,				[[[[ NSBundle mainBundle ] infoDictionary ] objectForKey:@"CFBundleIdentifier" ] UTF8String ]);
 	environment.SetValue ( MOAI_ENV_appVersion,			[[[[ NSBundle mainBundle ] infoDictionary ] objectForKey:@"CFBundleShortVersionString" ] UTF8String ]);
@@ -126,7 +85,6 @@ void AKUIphoneInit ( UIApplication* application ) {
 	environment.SetValue ( MOAI_ENV_osBrand,			"iOS" );
 	environment.SetValue ( MOAI_ENV_osVersion,			[[ UIDevice currentDevice ].systemVersion UTF8String ]);
 	environment.SetValue ( MOAI_ENV_resourceDirectory,	[[[ NSBundle mainBundle ] resourcePath ] UTF8String ]);
-	environment.SetValue ( MOAI_ENV_openUdid,			[[ MOAIOpenUDID value] UTF8String ]);
 	environment.SetValue ( MOAI_ENV_horizontalResolution, [[ UIScreen mainScreen ] bounds ].size.width * [[ UIScreen mainScreen ] scale ] );
 	environment.SetValue ( MOAI_ENV_verticalResolution, [[ UIScreen mainScreen ] bounds ].size.height * [[ UIScreen mainScreen ] scale ] );
 }
