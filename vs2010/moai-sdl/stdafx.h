e181d2b418de149286017d6693349be35f1d735b
Jetha Chan <jethachan@gmail.com>
2013-12-06 14:20:45 +0800

    More input stuff.

--- vs2010/moai-sdl/stdafx.h
+++ vs2010/moai-sdl/stdafx.h
@@ -403,3 +403,82 @@ struct SledgeJoystick
 	std::vector<vec2<f32>>	sticks;
 	std::vector<bool>	buttons;
 };
+
+
+#ifndef SDLINPUTTYPES
+#define SDLINPUTTYPES
+
+namespace SLEDGE_NAMESPACE {
+	namespace InputDevice {
+		const static char* DeviceName[] = {
+			"device",
+			"pad0",
+			"pad1",
+			"pad2",
+			"pad3",
+			"joy0",
+			"joy1",
+			"joy2",
+			"joy3"
+		};
+		const static InputDeviceType_ID DeviceType[] = {		
+			IDT_DEVICE,
+			IDT_PAD,
+			IDT_PAD,
+			IDT_PAD,
+			IDT_PAD,
+			IDT_JOY,
+			IDT_JOY,
+			IDT_JOY,
+			IDT_JOY
+		};
+	}
+	namespace DeviceSensor {
+		const static char* SensorName[] = {
+			"keyboard",
+			"pointer",
+			"mouseLeft",
+			"mouseMiddle",
+			"mouseRight"
+		};
+	}
+	namespace PadSensorAxes
+	{
+		const static char* SensorName[] = {
+			"stickLeft",
+			"stickRight",
+			"triggers",
+			"buttons"
+		};
+	}
+	namespace JoySensor
+	{
+		const static char* SensorName[] = {
+			"stick",
+			"buttons"
+		};
+	}
+	namespace PadSensor
+	{
+		const static char* ButtonName[] = {
+			"btnA",
+			"btnB",
+			"btnX",
+			"btnY",
+			"start",
+			"back",
+			"stickLeft",
+			"stickRight",
+			"shoulderLeft",
+			"shoulderRight",
+			"dpadUp",
+			"dpadDown",
+			"dpadLeft",
+			"dpadRight"
+		};
+	}
+}
+
+
+
+#endif
