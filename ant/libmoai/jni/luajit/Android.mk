65ff39030e96a4fb1b43a9ee7dc55d9493f725bb
makoto <makotok1123@gmail.com>
2014-02-21 08:24:08 +0900

    Unified to cmake.

--- ant/libmoai/jni/luajit/Android.mk
+++
@@ -1,9 +0,0 @@
-include $(CLEAR_VARS)
-
-LOCAL_MODULE            := luajit
-LOCAL_ARM_MODE          := $(MY_ARM_MODE)
-LOCAL_CFLAGS            := -include $(MY_MOAI_ROOT)/src/zl-vfs/zl_replace.h
-LOCAL_C_INCLUDES        := $(MY_HEADER_SEARCH_PATHS)
-LOCAL_SRC_FILES         := $(MY_MOAI_ROOT)/3rdparty/LuaJIT-2.0.1/android/$(TARGET_ARCH_ABI)/libluajit.a
-
-include $(PREBUILT_STATIC_LIBRARY)
