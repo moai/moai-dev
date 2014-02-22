5a4c9fb65260dcda5f33baa5ea0a239859806af3
makoto <makotok1123@gmail.com>
2014-02-22 12:11:27 +0900

    Organize files
    Move cmake of flascc.
    Replacing run.sh.

--- samples/server/server-basic/run.sh
+++ samples/server/server-basic/run.sh
@@ -1,16 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 #--------------------------------------------------------------------------------------
 # Copyright (c) 2010-2013 Zipline Games, Inc.
 # All Rights Reserved.
 # http://getmoai.com
 #--------------------------------------------------------------------------------------
 
+cd `dirname $0`
+
 # Verify paths
-if [ ! -f $MOAI_BIN/moai ]; then
+if [ ! -f "$MOAI_BIN/moai" ]; then
     echo "---------------------------------------------------------------------------"
     echo "Error: The MOAI_BIN environment variable doesn't exist or its pointing to an"
     echo "invalid path.  Please point it at a folder containing moai executable"
     echo "---------------------------------------------------------------------------"
+    exit 1
 fi
 
 if [ ! -f "$MOAI_CONFIG/config.lua" ]; then
@@ -18,6 +21,7 @@ if [ ! -f "$MOAI_CONFIG/config.lua" ]; then
     echo "WARNING: The MOAI_CONFIG environment variable either doesn't exist or it's"
     echo "pointing to an invalid path.  Please point it at a folder containing config.lua"
     echo "---------------------------------------------------------------------------"
+    exit 1
 fi
 
 # Run moai
