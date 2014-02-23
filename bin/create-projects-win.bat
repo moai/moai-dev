383e51c9c08e15a486801a2929c9f895ac7990d8
Daniel Wolf <dwolf@dannad.de>
2014-02-23 22:45:05 +0100

    VS target version can be selected via command line argument.
    Valid values are vs2008, vs2010, vs2012, vs2013. The output directory is chosen accordingly.
    Default value is vs2012.

--- bin/create-projects-win.bat
+++ bin/create-projects-win.bat
@@ -1,18 +1,35 @@
+@echo off
+
 :: Remember the original working directory
 pushd .
 
 :: Move to cmake directory
 cd "%~dp0%..\cmake"
 
+:: Determine target directory and cmake generator
+set arg1=%1
+if "%arg1%"=="" set arg1=vs2012
+set generator=
+if "%arg1%"=="vs2008" set generator=Visual Studio 9 2008
+if "%arg1%"=="vs2010" set generator=Visual Studio 10
+if "%arg1%"=="vs2012" set generator=Visual Studio 11
+if "%arg1%"=="vs2013" set generator=Visual Studio 12
+if "%generator%"=="" (
+	@echo Unknown argument "%1". Valid values are vs2008, vs2010, vs2012, vs2013. Exiting.
+	goto end
+)
+set targetDir=projects\%arg1%
+
+@echo on
+
 :: Delete and re-create the target directory
-set targetDir=projects\vs2013
 if exist %targetDir% rmdir /s /q %targetDir%
 md %targetDir%
 
 :: Move to target directory, then call cmake from there
 cd %targetDir%
 cmake ^
--G "Visual Studio 11" ^
+-G "%generator%" ^
 -DBUILD_WINDOWS=TRUE ^
 -DMOAI_BOX2D=TRUE ^
 -DMOAI_CHIPMUNK=FALSE ^
@@ -34,6 +51,10 @@ cmake ^
 -DMOAI_UNTZ=TRUE ^
 -DMOAI_LUAJIT=TRUE ^
 ..\..\
+
+:end
+
+@echo off
 
 :: Restore original working directory
 popd
