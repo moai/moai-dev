f9c04f27598fefb08bff4c5af6190aec15561762
David Pershouse <dpershouse@gmail.com>
2014-03-01 05:24:52 +0000

    travis support

---
+++ bin/ci-linux.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+echo "Building Moai-SDK for linux - CI"
+
+echo "Fetching Latest CMake"
+sudo apt-add-repository ppa:kubuntu-ppa/backports -y
+sudo apt-get update
+sudo apt-get install cmake
+
+echo "Fetching build deps"
+sudo apt-get install freeglut3-dev libglew-dev libglu1-mesa-dev libxmu-dev libxi-dev libxxf86vm-dev
+
+echo "Performing build"
+pushd .
+./bin/build-linux-sdl.sh
+if [[ $? -ne 0 ]]; then
+  echo "Build failed!" 
+  exit 1
+fi
+
+popd
+
