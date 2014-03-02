dc921c1d49ee6b7b655cb95f6066c470fd180b83
halfnelson <dpershouse@gmail.com>
2014-03-03 00:03:31 +1000

    try to get travis to use my cmake

--- bin/ci-osx.sh
+++ bin/ci-osx.sh
@@ -4,7 +4,9 @@ pushd .
 cd $TMPDIR
 curl http://www.cmake.org/files/v2.8/cmake-2.8.12.2-Darwin64-universal.tar.gz -o cmake.tar.gz
 tar zxvf cmake.tar.gz
-export PATH=$TMPDIR/cmake-2.8.12.2-Darwin64-universal/CMake\ 2.8-12.app/Contents/bin:$PATH
+export PATH="$TMPDIR/cmake-2.8.12.2-Darwin64-universal/CMake\ 2.8-12.app/Contents/bin":${PATH}
+echo $PATH
+which cmake
 popd                          
 
 echo "Building OSX"
