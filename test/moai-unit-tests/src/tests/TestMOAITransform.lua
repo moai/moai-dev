a77268f6bed22c3b0e7b9d63a4ed9224129a0553
makoto <makotok1123@gmail.com>
2014-03-03 01:43:03 +0900

    Added test code.

--- test/moai-unit-tests/src/tests/TestMOAITransform.lua
+++ test/moai-unit-tests/src/tests/TestMOAITransform.lua
@@ -106,6 +106,133 @@ function TestMOAITransform:test_addScl()
 end
 
 ---
+-- test: modelToWorld
+function TestMOAITransform:test_modelToWorld()
+    local wx, wy, wz = self.transform:modelToWorld()
+    assertEquals(wx, 0)
+    assertEquals(wy, 0)
+    assertEquals(wz, 0)
+    
+    wx, wy, wz = self.transform:modelToWorld(10, 20, 30)
+    assertEquals(wx, 10)
+    assertEquals(wy, 20)
+    assertEquals(wz, 30)
+    
+    self.transform:setLoc(100, 200, 300)
+    wx, wy, wz = self.transform:modelToWorld(10, 20, 30)
+    assertEquals(wx, 10)
+    assertEquals(wy, 20)
+    assertEquals(wz, 30)
+    
+    -- Is not reflected only after Update. Troublesome.
+    self.transform:forceUpdate()
+    wx, wy, wz = self.transform:modelToWorld(10, 20, 30)
+    assertEquals(wx, 110)
+    assertEquals(wy, 220)
+    assertEquals(wz, 330)
+end
+
+---
+-- test: move
+function TestMOAITransform:test_move()
+    local action = self.transform:move(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    assertTrue(action ~= nil)
+    
+    local action = self.transform:move(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, MOAIEaseType.EASE_IN)
+    assertTrue(action ~= nil)
+end
+
+---
+-- test: moveLoc
+function TestMOAITransform:test_moveLoc()
+    local action = self.transform:moveLoc(1, 2, 3, 4)
+    assertTrue(action ~= nil)
+    
+    local action = self.transform:moveLoc(1, 2, 3, 4, MOAIEaseType.EASE_IN)
+    assertTrue(action ~= nil)
+end
+
+---
+-- test: movePiv
+function TestMOAITransform:test_movePiv()
+    local action = self.transform:movePiv(1, 2, 3, 4)
+    assertTrue(action ~= nil)
+    
+    local action = self.transform:movePiv(1, 2, 3, 4, MOAIEaseType.EASE_IN)
+    assertTrue(action ~= nil)
+end
+
+---
+-- test: moveRot
+function TestMOAITransform:test_moveRot()
+    local action = self.transform:moveRot(1, 2, 3, 4)
+    assertTrue(action ~= nil)
+    
+    local action = self.transform:moveRot(1, 2, 3, 4, MOAIEaseType.EASE_IN)
+    assertTrue(action ~= nil)
+end
+
+---
+-- test: moveScl
+function TestMOAITransform:test_moveScl()
+    local action = self.transform:moveScl(1, 2, 3, 4)
+    assertTrue(action ~= nil)
+    
+    local action = self.transform:moveScl(1, 2, 3, 4, MOAIEaseType.EASE_IN)
+    assertTrue(action ~= nil)
+end
+
+---
+-- test: seek
+function TestMOAITransform:test_seek()
+    local action = self.transform:seek(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    assertTrue(action ~= nil)
+    
+    local action = self.transform:seek(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, MOAIEaseType.EASE_IN)
+    assertTrue(action ~= nil)
+end
+
+---
+-- test: seekLoc
+function TestMOAITransform:test_seekLoc()
+    local action = self.transform:seekLoc(1, 2, 3, 4)
+    assertTrue(action ~= nil)
+    
+    local action = self.transform:seekLoc(1, 2, 3, 4, MOAIEaseType.EASE_IN)
+    assertTrue(action ~= nil)
+end
+
+---
+-- test: seekPiv
+function TestMOAITransform:test_seekPiv()
+    local action = self.transform:seekPiv(1, 2, 3, 4)
+    assertTrue(action ~= nil)
+    
+    local action = self.transform:seekPiv(1, 2, 3, 4, MOAIEaseType.EASE_IN)
+    assertTrue(action ~= nil)
+end
+
+---
+-- test: seekRot
+function TestMOAITransform:test_seekRot()
+    local action = self.transform:seekRot(1, 2, 3, 4)
+    assertTrue(action ~= nil)
+    
+    local action = self.transform:seekRot(1, 2, 3, 4, MOAIEaseType.EASE_IN)
+    assertTrue(action ~= nil)
+end
+
+---
+-- test: seekScl
+function TestMOAITransform:test_seekScl()
+    local action = self.transform:seekScl(1, 2, 3, 4)
+    assertTrue(action ~= nil)
+    
+    local action = self.transform:seekScl(1, 2, 3, 4, MOAIEaseType.EASE_IN)
+    assertTrue(action ~= nil)
+end
+
+---
 -- test: getLoc
 function TestMOAITransform:test_getLoc()
     local x, y, z = self.transform:getLoc()
@@ -224,5 +351,32 @@ function TestMOAITransform:test_setScl()
     assertEquals(x, 10)
     assertEquals(y, 20)
     assertEquals(z, 30)
+end
+
+---
+-- test: modelToWorld
+function TestMOAITransform:test_worldToModel()
+    local wx, wy, wz = self.transform:worldToModel()
+    assertEquals(wx, 0)
+    assertEquals(wy, 0)
+    assertEquals(wz, 0)
+    
+    wx, wy, wz = self.transform:worldToModel(10, 20, 30)
+    assertEquals(wx, 10)
+    assertEquals(wy, 20)
+    assertEquals(wz, 30)
+    
+    self.transform:setLoc(100, 200, 300)
+    wx, wy, wz = self.transform:worldToModel(10, 20, 30)
+    assertEquals(wx, 10)
+    assertEquals(wy, 20)
+    assertEquals(wz, 30)
+    
+    -- Is not reflected only after Update. Troublesome.
+    self.transform:forceUpdate()
+    wx, wy, wz = self.transform:worldToModel(10, 20, 30)
+    assertEquals(wx, -90)
+    assertEquals(wy, -180)
+    assertEquals(wz, -270)
 end
 
