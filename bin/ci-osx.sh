13c55aee594a9b285194e4dda47c92beb3f13be5
halfnelson <dpershouse@gmail.com>
2014-03-03 00:09:14 +1000

    $TMPDIR is gone. use /tmp

--- bin/ci-osx.sh
+++ bin/ci-osx.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "Installing CMake 2.8.12"
 pushd .
-cd $TMPDIR
+cd /tmp
 curl http://www.cmake.org/files/v2.8/cmake-2.8.12.2-Darwin64-universal.tar.gz -o cmake.tar.gz
 tar zxvf cmake.tar.gz
-export PATH="$TMPDIR/cmake-2.8.12.2-Darwin64-universal/CMake\ 2.8-12.app/Contents/bin":${PATH}
+export PATH="/tmp/cmake-2.8.12.2-Darwin64-universal/CMake\ 2.8-12.app/Contents/bin":${PATH}
 echo $PATH
 which cmake
 popd                          
