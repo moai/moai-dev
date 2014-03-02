645e8096abb37cc7228e62db36c35a8edca566b6
halfnelson <dpershouse@gmail.com>
2014-03-03 00:15:54 +1000

    don't quote and escape spaces

--- bin/ci-osx.sh
+++ bin/ci-osx.sh
@@ -4,7 +4,7 @@ pushd .
 cd /tmp
 curl http://www.cmake.org/files/v2.8/cmake-2.8.12.2-Darwin64-universal.tar.gz -o cmake.tar.gz
 tar zxvf cmake.tar.gz
-export PATH="/tmp/cmake-2.8.12.2-Darwin64-universal/CMake\ 2.8-12.app/Contents/bin":${PATH}
+export PATH=/tmp/cmake-2.8.12.2-Darwin64-universal/CMake\ 2.8-12.app/Contents/bin:${PATH}
 echo $PATH
 which cmake
 popd                          
