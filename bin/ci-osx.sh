d4c800047334694f300031dd571127089cc0037a
halfnelson <dpershouse@gmail.com>
2014-03-02 23:55:02 +1000

    use cmake 2.8.12.2 by ensuring its path is first

--- bin/ci-osx.sh
+++ bin/ci-osx.sh
@@ -4,7 +4,7 @@ pushd .
 cd $TMPDIR
 curl http://www.cmake.org/files/v2.8/cmake-2.8.12.2-Darwin64-universal.tar.gz -o cmake.tar.gz
 tar zxvf cmake.tar.gz
-export PATH=$PATH:$TMPDIR/cmake-2.8.12.2-Darwin64-universal/CMake\ 2.8-12.app/Contents/bin
+export PATH=$TMPDIR/cmake-2.8.12.2-Darwin64-universal/CMake\ 2.8-12.app/Contents/bin:$PATH
 popd                          
 
 echo "Building OSX"
