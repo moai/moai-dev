deab01810d7174f2cdcf2b4eb7add9c4857ee18e
David Pershouse <dpershouse@gmail.com>
2014-03-11 19:13:03 +1000

    More refactor WIP of moai js host [skip ci]

--- src/host-html/host-template/www/moaihost.js
+++ src/host-html/host-template/www/moaihost.js
@@ -27,6 +27,26 @@ MoaiJS.prototype.initEmscripten = function() {
     	};
 	console.log("MoaiJS Emscripten Init");
 	this.emscripten = window.CreateMoaiRuntime(Opts);
+
+	//bring our emscripten host functions into the normal javascript world
+	var Module = this.emscripten;
+	this.RefreshContext = Module.cwrap('RefreshContext','number',null);
+	this.AKURunString = Module.cwrap('AKURunString','number',['string']);
+	this.AKURunScript = Module.cwrap('AKURunScript','number',['string']);
+	this.AKUSetWorkingDirectory = Module.cwrap('AKUSetWorkingDirectory','number',['string']);
+	this.AKUGetSimStep = Module.cwrap('AKUGetSimStep','number',null);
+	this.AKUEnqueueKeyboardShiftEvent = Module.cwrap('AKUEnqueueKeyboardShiftEvent','number',['number','number','number']);
+	this.onPaint = Module.cwrap('onPaint','number',null);
+	this.onReshape = Module.cwrap('onReshape','number',['number','number']);
+	this.onTimer = Module.cwrap('onTimer','number',null);
+	this.onMouseButton = Module.cwrap('onMouseButton', 'number',['number','number']);
+	this.onMouseMove = Module.cwrap('onMouseMove', 'number',['number','number']);
+	this.onKeyDown = Module.cwrap('onKeyDown', 'number',['number']);
+	this.onKeyUp = Module.cwrap('onKeyUp','number',['number']);
+
+	//callbacks
+	this.emscripten.SetOpenWindowFunc(this.OpenWindowFunc.bind(this));
+    this.emscripten.SetSaveFunc(this.SaveFile.bind(this));
 }
 
 MoaiJS.prototype.loadFileSystem = function(romUrl) {
@@ -50,55 +70,30 @@ MoaiJS.prototype.Run = function(mainLua) {
   	}).rethrow();
 }
 
-function wrapNativeFuncs() {
-	moaijs.RefreshContext = Module.cwrap('RefreshContext','number',null);
 
-	moaijs.AKURunString = Module.cwrap('AKURunString','number',['string']);
-	moaijs.AKURunScript = Module.cwrap('AKURunScript','number',['string']);
-	moaijs.AKUSetWorkingDirectory = Module.cwrap('AKUSetWorkingDirectory','number',['string']);
-	moaijs.AKUGetSimStep = Module.cwrap('AKUGetSimStep','number',null);
-	moaijs.AKUEnqueueKeyboardShiftEvent = Module.cwrap('AKUEnqueueKeyboardShiftEvent','number',['number','number','number']);
-
-
-	moaijs.onPaint = Module.cwrap('onPaint','number',null);
-
-	moaijs.onReshape = Module.cwrap('onReshape','number',['number','number']);
-
-	moaijs.onTimer = Module.cwrap('onTimer','number',null);
-
-	moaijs.onMouseButton = Module.cwrap('onMouseButton', 'number',['number','number']);
-	moaijs.onMouseMove = Module.cwrap('onMouseMove', 'number',['number','number']);
-
-	moaijs.onKeyDown = Module.cwrap('onKeyDown', 'number',['number']);
-	moaijs.onKeyUp = Module.cwrap('onKeyUp','number',['number']);
+MoaiJS.prototype.renderloop = function() {
+	this.onPaint();
+	this.emscripten.requestAnimationFrame(this.renderloop.bind(this));
 }
 
-
-
-
-moaijs.renderloop = function() {
-	moaijs.onPaint();
-	Module.requestAnimationFrame(moaijs.renderloop);
+MoaiJS.prototype.updateloop = function() {
+	this.onTimer();
 }
 
-moaijs.updateloop = function() {
-	moaijs.onTimer();
+MoaiJS.prototype.mousedown = function(e) {
+    this.onMouseButton(0,0); //MOUSE_LEFT, MOUSE_DOWN
 }
 
-moaijs.mousedown = function(e) {
-    moaijs.onMouseButton(0,0); //MOUSE_LEFT, MOUSE_DOWN
+MoaiJS.prototype.mouseup = function(e) {
+    this.onMouseButton(0,1); //MOUSE_LEFT, MOUSE_UP
 }
 
-moaijs.mouseup = function(e) {
-    moaijs.onMouseButton(0,1); //MOUSE_LEFT, MOUSE_UP
-}
-
-moaijs.mousemove = function(e) {
+MoaiJS.prototype.mousemove = function(e) {
 	
-	var can = Module.canvas;
+	var can = this.canvas;
     var canX = e.offsetX || e.layerX; 
     var canY = e.offsetY || e.layerY;
-    moaijs.onMouseMove(Math.floor(canX*moaijs.canvasScale),Math.floor(canY*moaijs.canvasScale));
+    this.onMouseMove(Math.floor(canX*this.canvasScale),Math.floor(canY*this.canvasScale));
 }
 
 
@@ -143,14 +138,12 @@ var getASCIIKey= function(event) {
 
 
 
-moaijs.keydown = function(e) {
-
-	console.log("keydown",e.keyCode);
+MoaiJS.prototype.keydown = function(e) {
 	if (e.keycode == 16) {
-		moaijs.AKUEnqueueKeyboardShiftEvent(0,0,1); //device, keyboard, true
+		this.AKUEnqueueKeyboardShiftEvent(0,0,1); //device, keyboard, true
 	} else {
 		var key = getASCIIKey(e);
-		moaijs.onKeyDown(key?key:e.keyCode);
+		this.onKeyDown(key?key:e.keyCode);
 	}
 	e.preventDefault();
 
@@ -159,54 +152,52 @@ moaijs.keydown = function(e) {
     }
 }
 
-moaijs.keyup = function(e) {
-	console.log("keyup",e.keyCode);
+MoaiJS.prototype.keyup = function(e) {
 	if (e.keyCode == 16) {
-		moaijs.AKUEnqueueKeyboardShiftEvent(0,0,0); //device, keyboard, false
+		this.AKUEnqueueKeyboardShiftEvent(0,0,0); //device, keyboard, false
 	} else {
 		var key = getASCIIKey(e);
-		moaijs.onKeyUp(key?key:e.keyCode);
+		this.onKeyUp(key?key:e.keyCode);
 	}
 	e.preventDefault();
 }
 
-moaijs.keypress = function(e) {
+MoaiJS.prototype.keypress = function(e) {
 	if (e.keyCode == 8) {
 		return false;
 	}
 }
 
 
-moaijs.OpenWindowFunc = function(title,width,height) {
-	var canvas = document.getElementById('MoaiCanvas');
-	var moaititle = document.getElementById('MoaiTitle');
-	moaititle.innerHTML = title;
+MoaiJS.prototype.OpenWindowFunc = function(title,width,height) {
+	var canvas = this.canvas;
+	if (this.onTitleChange) { this.onTitleChange(title); }
 	canvas.width = width;
 	canvas.height = height;
-	moaijs.canvasScale = canvas.width/$(canvas).width();
-	//Module.canvas = canvas;
+	this.canvasScale = canvas.width/$(canvas).width();
+
 	canvas.focus();
-	//Browser.createContext(canvas,true,true);
+
 	//hook mouse
-	canvas.addEventListener("mousedown",moaijs.mousedown,false);
-	canvas.addEventListener("mouseup",moaijs.mouseup,false);
-	canvas.addEventListener("mousemove",moaijs.mousemove,false);
+	canvas.addEventListener("mousedown",this.mousedown.bind(this),false);
+	canvas.addEventListener("mouseup",this.mouseup.bind(this),false);
+	canvas.addEventListener("mousemove",this.mousemove.bind(this),false);
 
 	//grab focus on hover
 	canvas.addEventListener("mouseover",function() { canvas.focus(); },false);
-	canvas.addEventListener("mouseout",function() { canvas.blur(); moaijs.mouseup(); },false);
+	canvas.addEventListener("mouseout",(function() { canvas.blur(); this.mouseup(); }).bind(this),false);
 
 	//grab keys
-	canvas.addEventListener("keydown",moaijs.keydown,false);
-	canvas.addEventListener("keyup",moaijs.keyup,false);
+	canvas.addEventListener("keydown",this.keydown.bind(this),false);
+	canvas.addEventListener("keyup",this.keyup.bind(this),false);
 
-	canvas.addEventListener("keypress",moaijs.keypress, false);
+	canvas.addEventListener("keypress",this.keypress.bind(this), false);
 
 
 	//now start rendering and updationg
-	var step = moaijs.AKUGetSimStep() || ((1000/60)/1000)
-	var moaiInverval = window.setInterval( moaijs.updateloop, step*1000);
-	Module.requestAnimationFrame(moaijs.renderloop);
+	var step = this.AKUGetSimStep() || ((1000/60)/1000)
+	var moaiInverval = window.setInterval( this.updateloop.bind(this), step*1000);
+	this.emscripten.requestAnimationFrame(this.renderloop.bind(this));
 
 	window.addEventListener("resize", resizeThrottler, false);
 
@@ -222,32 +213,28 @@ moaijs.OpenWindowFunc = function(title,width,height) {
 		   }, 66);
 		}
 	  }
-
+	  var that = this;
 	  function resizeHandler() {
-		moaijs.canvasScale = canvas.width/$(canvas).width();
+		that.canvasScale = canvas.width/$(canvas).width();
 	  }
 
 	return canvas;
 };
 
-moaijs.runhost = function() {
-
-    Module.SetOpenWindowFunc(moaijs.OpenWindowFunc);
-    Module.SetSaveFunc(moaijs.SaveFile);
-
-console.log("runhost called");
-	wrapNativeFuncs();
-console.log("restoring save state");
-	moaijs.restoreDocumentDirectory();
-console.log("refreshing context");
-	moaijs.RefreshContext();
-console.log("setting working directory");
-	moaijs.AKUSetWorkingDirectory('/src');
-console.log("setting up canvas");
-	moaijs.AKURunString('MOAIEnvironment.horizontalResolution = '+Module.canvas.width);
-	moaijs.AKURunString('MOAIEnvironment.verticalResolution = '+Module.canvas.height);
-console.log("launching main.lua");
-	moaijs.AKURunScript('main.lua');
+MoaiJS.prototype.runhost = function(mainLua) {
+	console.log("runhost called");
+	console.log("restoring save state");
+	this.restoreDocumentDirectory();
+	console.log("refreshing context");
+	this.RefreshContext();
+	console.log("setting working directory");
+	this.AKUSetWorkingDirectory('/src');
+	console.log("setting up canvas");
+	this.AKURunString('MOAIEnvironment.horizontalResolution = '+this.canvas.width);
+	this.AKURunString('MOAIEnvironment.verticalResolution = '+this.canvas.height);
+	var main = mainLua || 'main.lua'
+	console.log("launching "+main);
+	this.AKURunScript(main);
 }
 
 //Courtesy of Mozilla
@@ -305,7 +292,7 @@ moaijs.restoreDocumentDirectory = function() {
  	}
 }
 
-moaijs.saveToDocumentDirectory = function(path,filedata) {
+MoaiJS.prototype.saveToDocumentDirectory = function(path,filedata) {
 	var docsjson = localStorage.getItem('moai-docs');
 	if (!docsjson) docsjson="{}";
 	var docs = JSON.parse(docsjson);
@@ -313,20 +300,21 @@ moaijs.saveToDocumentDirectory = function(path,filedata) {
 	localStorage.setItem('moai-docs',JSON.stringify(docs));
 }
 
-moaijs.SaveFile = function(path,data) {
+MoaiJS.prototype.SaveFile = function(path,data) {
 	//get data as arraybuffer
 	var dataArray = (data.buffer)? data : new Uint8Array(data);
 	var dataBlob = new Blob([dataArray],{'type':'application/octet-stream'});
 	var reader = new FileReader();
+	var that = this;
 	reader.onload =function(event) {
-		moaijs.saveToDocumentDirectory(path,event.target.result);
+		that.saveToDocumentDirectory(path,event.target.result);
 	};
 	reader.readAsDataURL(dataBlob);
 };
 
 
-
-  
+//TODO replace below with new MoaiJS calls
+ /*
 
 
 function makeProgress(prefix) {
@@ -340,4 +328,4 @@ LoadRom(emscripten,'moaiapp.rom', makeProgress('rom'), makeProgress('file'))
   	   emscripten.addOnPreMain(function(){moaijs.runhost();});
   	   emscripten.run();   
   })
-
+*/
