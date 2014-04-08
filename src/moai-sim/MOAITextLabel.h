19ee3018853c8417a88ce801459a683587f373da
Patrick Meehan <patrick@ziplinegames.com>
2014-04-07 19:37:56 -0700

    added support for text label auto-flip (for Point Inside)

--- src/moai-sim/MOAITextLabel.h
+++ src/moai-sim/MOAITextLabel.h
@@ -151,6 +151,8 @@ protected:
 	
 	STLString			mText;
 	
+	bool				mAutoFlip;
+	
 	//----------------------------------------------------------------//
 	static int			_clearHighlights		( lua_State* L );
 	static int			_getAlignment			( lua_State* L );
@@ -164,6 +166,7 @@ protected:
 	static int			_revealAll				( lua_State* L );
 	static int			_reserveCurves			( lua_State* L );
 	static int			_setAlignment			( lua_State* L );
+	static int			_setAutoFlip			( lua_State* L );
 	static int			_setCurve				( lua_State* L );
 	static int			_setGlyphScale			( lua_State* L );
 	static int			_setHighlight			( lua_State* L );
@@ -186,6 +189,7 @@ protected:
 	#endif
 	
 	//----------------------------------------------------------------//
+	ZLMatrix4x4			GetWorldDrawingMtx		();
 	void				OnDepNodeUpdate			();
 	u32					OnGetModelBounds		( ZLBox& bounds );
 	void				OnUpdate				( float step );
