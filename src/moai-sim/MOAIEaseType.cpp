01751cdd4acb50b6b5b8e74d32c04dbc23c7e044
Patrick Meehan <patrick@ziplinegames.com>
2014-05-12 14:22:59 -0700

    formatting change

--- src/moai-sim/MOAIEaseType.cpp
+++ src/moai-sim/MOAIEaseType.cpp
@@ -13,14 +13,14 @@ void MOAIEaseType::RegisterLuaClass ( MOAILuaState& state ) {
 	
 	state.SetField ( -1, "EASE_IN",					( u32 )ZLInterpolate::kEaseIn );
 	state.SetField ( -1, "EASE_OUT",				( u32 )ZLInterpolate::kEaseOut );
+	state.SetField ( -1, "EXTRA_SHARP_EASE_IN",		( u32 )ZLInterpolate::kExtraSharpEaseIn );
+	state.SetField ( -1, "EXTRA_SHARP_EASE_OUT",	( u32 )ZLInterpolate::kExtraSharpEaseOut );
+	state.SetField ( -1, "EXTRA_SHARP_SMOOTH",		( u32 )ZLInterpolate::kExtraSharpSmooth );
 	state.SetField ( -1, "FLAT",					( u32 )ZLInterpolate::kFlat );
 	state.SetField ( -1, "LINEAR",					( u32 )ZLInterpolate::kLinear );
 	state.SetField ( -1, "SHARP_EASE_IN",			( u32 )ZLInterpolate::kSharpEaseIn );
 	state.SetField ( -1, "SHARP_EASE_OUT",			( u32 )ZLInterpolate::kSharpEaseOut );
 	state.SetField ( -1, "SHARP_SMOOTH",			( u32 )ZLInterpolate::kSharpSmooth );
-	state.SetField ( -1, "EXTRA_SHARP_EASE_IN",		( u32 )ZLInterpolate::kExtraSharpEaseIn );
-	state.SetField ( -1, "EXTRA_SHARP_EASE_OUT",	( u32 )ZLInterpolate::kExtraSharpEaseOut );
-	state.SetField ( -1, "EXTRA_SHARP_SMOOTH",		( u32 )ZLInterpolate::kExtraSharpSmooth );
 	state.SetField ( -1, "SMOOTH",					( u32 )ZLInterpolate::kSmooth );
 	state.SetField ( -1, "SOFT_EASE_IN",			( u32 )ZLInterpolate::kSoftEaseIn );
 	state.SetField ( -1, "SOFT_EASE_OUT",			( u32 )ZLInterpolate::kSoftEaseOut );
