fb9e6d95ddd85b1ccef4dab276ca0bc09cc0e6c6
Patrick Meehan <patrick@ziplinegames.com>
2014-05-12 14:52:51 -0700

    added smooth 'ease out' compound ease curve types

--- src/moai-sim/MOAIEaseType.h
+++ src/moai-sim/MOAIEaseType.h
@@ -10,20 +10,24 @@
 /**	@name	MOAIEaseType
 	@text	Namespace to hold ease modes. Moai ease in/out has opposite meaning of Flash ease in/out.
 	
-	@const	EASE_IN					Quartic ease in - Fast start then slow when approaching value; ease into position.
-	@const	EASE_OUT				Quartic ease out - Slow start then fast when approaching value; ease out of position.
-	@const	EXTRA_SHARP_EASE_IN		t ^ 12
-	@const	EXTRA_SHARP_EASE_OUT	t ^ 12
-	@const	EXTRA_SHARP_SMOOTH		t ^ 12
-	@const	FLAT					Stepped change - Maintain original value until end of ease.
-	@const	LINEAR					Linear interpolation.
-	@const	SHARP_EASE_IN			t ^ 8
-	@const	SHARP_EASE_OUT			t ^ 8
-	@const	SHARP_SMOOTH			t ^ 8
-	@const	SMOOTH					Quartic ease out then ease in.
-	@const	SOFT_EASE_IN			t ^ 2
-	@const	SOFT_EASE_OUT			t ^ 2
-	@const	SOFT_SMOOTH				t ^ 2
+	@const	EASE_IN							t ^ 4 - Fast start then slow when approaching value; ease into position.
+	@const	EASE_OUT						t ^ 4 - Slow start then fast when approaching value; ease out of position.
+	@const	EXTRA_SHARP_EASE_IN				t ^ 16
+	@const	EXTRA_SHARP_EASE_OUT			t ^ 16
+	@const	EXTRA_SHARP_SMOOTH				t ^ 16
+	@const	EXTRA_SHARP_SMOOTH_EASE_OUT		t ^ 16
+	@const	FLAT							Stepped change - Maintain original value until end of ease.
+	@const	LINEAR							Linear interpolation.
+	@const	SHARP_EASE_IN					t ^ 8
+	@const	SHARP_EASE_OUT					t ^ 8
+	@const	SHARP_SMOOTH					t ^ 8
+	@const	SHARP_SMOOTH_EASE_OUT			t ^ 8
+	@const	SMOOTH							t ^ 4 - Ease out then ease in.
+	@const	SMOOTH_EASE_OUT					t ^ 4 - Ease out then ease in scaled by ease out.
+	@const	SOFT_EASE_IN					t ^ 2
+	@const	SOFT_EASE_OUT					t ^ 2
+	@const	SOFT_SMOOTH						t ^ 2
+	@const	SOFT_SMOOTH_EASE_OUT			t ^ 2
 */
 class MOAIEaseType :
 	public MOAIGlobalClass < MOAIEaseType, MOAILuaObject > {
