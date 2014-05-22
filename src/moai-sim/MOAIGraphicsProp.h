c7319e39f946d6278c07e75d50b7f93d1489aa05
Patrick Meehan <patrick@ziplinegames.com>
2014-05-22 02:55:45 -0700

    fixed bug in MOAIGraphicsProp flags

--- src/moai-sim/MOAIGraphicsProp.h
+++ src/moai-sim/MOAIGraphicsProp.h
@@ -154,8 +154,8 @@ public:
 	};
 
 	enum {
-		FLAGS_LOCAL_VISIBLE			= 0x04,
-		FLAGS_VISIBLE				= 0x08, // this is a composite of FLAGS_LOCAL_VISIBLE plus the parent's ATTR_VISIBLE
+		FLAGS_LOCAL_VISIBLE			= 0x08,
+		FLAGS_VISIBLE				= 0x10, // this is a composite of FLAGS_LOCAL_VISIBLE plus the parent's ATTR_VISIBLE
 	};
 
 	enum {
