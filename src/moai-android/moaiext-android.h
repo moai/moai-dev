b817527d1dc48badce3f572785e63bf5950c44b1
Patrick Meehan <patrick@ziplinegames.com>
2014-08-04 22:49:23 -0700

    fixing android build

--- src/moai-android/moaiext-android.h
+++ src/moai-android/moaiext-android.h
@@ -4,18 +4,53 @@
 #include <moai-android/MOAIAppAndroid.h>
 #include <moai-android/MOAIBrowserAndroid.h>
 #include <moai-android/MOAIDialogAndroid.h>
-#include <moai-android/MOAIAdColonyAndroid.h>
-#include <moai-android/MOAIBillingAndroid.h>
-#include <moai-android/MOAIChartBoostAndroid.h>
-#include <moai-android/MOAICrittercismAndroid.h>
-#include <moai-android/MOAIFacebookAndroid.h>
-#include <moai-android/MOAIFlurryAndroid.h>
-#include <moai-android/MOAIGooglePlayServicesAndroid.h>
 #include <moai-android/MOAIKeyboardAndroid.h>
 #include <moai-android/MOAIMoviePlayerAndroid.h>
-#include <moai-android/MOAINotificationsAndroid.h>
-#include <moai-android/MOAITapjoyAndroid.h>
-#include <moai-android/MOAITstoreWallAndroid.h>
-#include <moai-android/MOAITstoreGamecenterAndroid.h>
-#include <moai-android/MOAITwitterAndroid.h>
-#include <moai-android/MOAIVungleAndroid.h>
+
+#ifndef DISABLE_ADCOLONY
+	#include <moai-android-adcolony/MOAIAdColonyAndroid.h>
+#endif
+
+#ifndef DISABLE_BILLING
+	#include <moai-android/MOAIBillingAndroid.h>
+#endif
+
+#ifndef DISABLE_CHARTBOOST
+	#include <moai-android-chartboost/MOAIChartBoostAndroid.h>
+#endif
+
+#ifndef DISABLE_CRITTERCISM
+	#include <moai-android-crittercism/MOAICrittercismAndroid.h>
+#endif
+
+#ifndef DISABLE_FACEBOOK
+	#include <moai-android-facebook/MOAIFacebookAndroid.h>
+#endif
+
+#ifndef DISABLE_FLURRY
+	#include <moai-android-flurry/MOAIFlurryAndroid.h>
+#endif
+
+#ifndef DISABLE_NOTIFICATIONS
+	#include <moai-android/MOAINotificationsAndroid.h>
+#endif
+
+#ifndef DISABLE_TAPJOY
+	#include <moai-android-tapjoy/MOAITapjoyAndroid.h>
+#endif
+
+#ifndef DISABLE_TWITTER
+	#include <moai-android-twitter/MOAITwitterAndroid.h>
+#endif
+
+#ifndef DISABLE_TSTOREWALL
+	#include <moai-android-tstore/MOAITstoreWallAndroid.h>;
+#endif
+
+#ifndef DISABLE_TSTOREGAMECENTER
+	#include <moai-android-tstore/MOAITstoreGamecenterAndroid.h>;
+#endif
+
+#ifndef DISABLE_PLAYSERVICES
+	#include <moai-android-google-play-services/MOAIGooglePlayServicesAndroid.h>;
+#endif
