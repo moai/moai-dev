924ed95d4e6b2f8f00b7adc60ccbd95a0d53bfcf
Toni Rico <a.rico.diez@gmail.com>
2014-10-20 14:37:30 -0700

    Add md5_dgst.c file to android build

--- ant/libmoai/jni/crypto/lists/crypto-c.mk
+++ ant/libmoai/jni/crypto/lists/crypto-c.mk
@@ -3,6 +3,8 @@
 # All Rights Reserved.
 # http://getmoai.com
 #================================================================#
+	
+	LOCAL_SRC_FILES += $(MY_MOAI_ROOT)/3rdparty/openssl-1.0.0d/crypto/md5/md5_dgst.c
 
 	LOCAL_SRC_FILES += $(MY_MOAI_ROOT)/3rdparty/openssl-1.0.0d/crypto/mem_clr.c
 
