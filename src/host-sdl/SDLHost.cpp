cc6d7a0927cda82e051e7858b5f153bbe65ab910
Daniel T. Borelli <daltomi@aol.com>
2014-12-03 19:31:16 -0300

    fix fullscreen

--- src/host-sdl/SDLHost.cpp
+++ src/host-sdl/SDLHost.cpp
@@ -45,6 +45,10 @@ namespace InputSensorID {
 
 static SDL_Window* sWindow = 0;
 
+typedef int ( *DisplayModeFunc ) (int, SDL_DisplayMode *);
+
+static void SetScreenSize ( DisplayModeFunc func);
+
 //================================================================//
 // aku callbacks
 //================================================================//
@@ -131,11 +135,7 @@ void Init ( int argc, char** argv ) {
 
 	AKUSetInputConfigurationName ( "SDL" );
 
-    SDL_DisplayMode dm;
-
-    if ( SDL_GetDesktopDisplayMode( 0, &dm ) == 0 ) {
-    	AKUSetScreenSize(dm.w, dm.h);
-    }
+	SetScreenSize( SDL_GetDesktopDisplayMode );
 
     SetScreenDpi();
 
@@ -178,6 +178,17 @@ void _onMultiButton( int touch_id, float x, float y, int state ) {
 	);
 }
 
+
+
+//----------------------------------------------------------------//
+void SetScreenSize(DisplayModeFunc func ) {
+
+    SDL_DisplayMode dm;
+
+    if ( func != NULL && func( 0, &dm ) == 0 ) {
+    	AKUSetScreenSize(dm.w, dm.h);
+    }
+}
 
 
 //----------------------------------------------------------------//
@@ -307,11 +318,22 @@ void MainLoop () {
                             const Joystick::AXIS_MOTION & axis = joystick0->HandleAxisMotion(sdlEvent);
 					        AKUEnqueueJoystickEvent ( InputDeviceID::DEVICE, InputSensorID::JOYSTICK, axis.x, axis.y );
                         }
-                    break;
+						break;
 
 				case SDL_MOUSEMOTION:
 				
 					AKUEnqueuePointerEvent ( InputDeviceID::DEVICE, InputSensorID::POINTER, sdlEvent.motion.x, sdlEvent.motion.y );
+					break;
+
+				case SDL_WINDOWEVENT:
+
+					if ( sdlEvent.window.event == SDL_WINDOWEVENT_SIZE_CHANGED || sdlEvent.window.event == SDL_WINDOWEVENT_RESIZED ) {
+						
+						//AKUSetViewSize(sdlEvent.window.data1, sdlEvent.window.data2);
+
+						( SDL_GetWindowFlags(sWindow) == SDL_WINDOW_FULLSCREEN ) ? SetScreenSize( SDL_GetDesktopDisplayMode ) : SetScreenSize( SDL_GetCurrentDisplayMode );
+					}
+
 					break;
 
                 case SDL_FINGERDOWN:
