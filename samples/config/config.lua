c11b43cbb7313ce34cda389138b17f8803d964ff
Patrick Meehan <patrick@ziplinegames.com>
2014-12-04 16:14:44 -0800

    starting to move samples to flat structure

--- samples/config/config.lua
+++
@@ -1,7 +0,0 @@
-----------------------------------------------------------------
--- Copyright (c) 2010-2011 Zipline Games, Inc. 
--- All Rights Reserved. 
--- http://getmoai.com
-----------------------------------------------------------------
-
-package.path = package.path .. ';../../../src/lua-modules/?.lua'
