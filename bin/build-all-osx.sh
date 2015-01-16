d4c7e9abd97c1c5de0ae412e3d6576bbb20b7618
halfnelson <dpershouse@gmail.com>
2015-01-17 08:37:33 +1000

    created different release build script for cmake based packages. Prep for binary/template based release

--- bin/build-all-osx.sh
+++ bin/build-all-osx.sh
@@ -12,51 +12,27 @@ cd $MOAIROOT
 
 echo "Building osx libs"
 
-./bin/build-osx.sh
+./bin/build-osx.sh || exit 1
 
 echo "OSX Libs complete"
 
-echo "Creating osx host"
-
-moaiutil host-osx-app
-
-echo "OSX host Complete"
-
 echo "Building ios libs"
 
-./bin/build-ios.sh
+./bin/build-ios.sh || exit 1
 
 echo "iOS libs complete"
 
-echo "Creating iOS Host"
-
-moaiutil host-ios
-
-echo "iOS host complete"
-
 echo "Building android libs"
 
-./bin/build-android.sh
+./bin/build-android.sh || exit 1
 
 echo "Android libs complete"
 
-echo "Creating android host"
-
-moaiutil host-android-gradle
-
-echo "Android host Complete"
-
 echo "Building html lib"
 
-./bin/build-html.sh
+./bin/build-html.sh || exit 1
 
 echo "Creating html lib complete"
 
-echo "Creating html host"
 
-moaiutil host-html
-
-echo "Html host complete"
-
-
-echo "All OSX compatible hosts and libs have been built"
+echo "All OSX compatible libs have been built"
