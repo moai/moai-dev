cae90976f796038680292e72121128f1d681d917
Daniel T. Borelli <daltomi@aol.com>
2015-03-09 04:14:59 -0300

    know that touching is true

--- src/moai-box2d/MOAIBox2DBody.cpp
+++ src/moai-box2d/MOAIBox2DBody.cpp
@@ -447,7 +447,7 @@ int MOAIBox2DBody::_getContactList ( lua_State* L ) {
 	u32 total = 0;
 	for ( b2ContactEdge* ce = body->GetContactList (); ce != NULL; ce = ce->next ) {
 
-		if ( !touching || ( touching && ce->contact->IsTouching ())) {
+		if ( !touching || ce->contact->IsTouching () ) {
 			MOAIBox2DBody* moaiBody = ( MOAIBox2DBody* )ce->other->GetUserData ();
 			if ( moaiBody ) {
 				lua_checkstack ( L, 2 );
