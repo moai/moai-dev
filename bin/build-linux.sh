c49d5acf764f8d09eb03f412a8c94cd9b637aa61
Daniel T. Borelli <daltomi@aol.com>
2015-03-18 20:07:50 -0300

    - fix build curl in some shells. - a little more robust script

--- bin/build-linux.sh
+++ bin/build-linux.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 #
-# Build script for Mac OSX
+# Build script for GNU/Linux
 # Usage: Run from Moai SDK's root directory:
 #
-# build-osx-sdl.sh
+# build-linux.sh
 #
 # You can change the CMake options using -DOPTION=VALUE
 # Check moai-dev/cmake/CMakeLists.txt for all the available options.
 #
-if [ x$1 == x ]; then
+if [ -z $1 ]; then
   libprefix=`dirname $0`/../lib/linux
 else
   libprefix=$1
 fi
+
 mkdir -p $libprefix
 
 libprefix=$(cd $libprefix; pwd)
@@ -22,21 +23,22 @@ cd `dirname $0`/..
 
 moai_root=$(pwd)
 
+BUILD_DIR="build/build-linux"
 
-if ! [ -d "build" ]
-then
-mkdir build
+if ! [ -d ${BUILD_DIR%/*} ]; then
+	mkdir -p $BUILD_DIR
 fi
-cd build
 
-if ! [ -d "build-linux" ]
-then
-mkdir build-linux
+cd $BUILD_DIR
+
+# - This fix, "curl: sed not found in PATH. Cannot continue without sed."
+# - Shells problems: fish,
+if ! [ -e PATH_SEPARATOR ]; then
+	export PATH_SEPARATOR=:
 fi
-cd build-linux
-
 
 set -e
+
 cmake \
 -DBUILD_LINUX=TRUE \
 -DMOAI_SDL=TRUE \
@@ -47,6 +49,6 @@ $moai_root/cmake/hosts/host-linux-sdl
 
 cmake --build . --target install
 
-if [ ! -e "$moai_root/util/moai" ]; then
-   cp $libprefix/bin/moai $moai_root/util/moai
-fi
+cp $libprefix/bin/moai $moai_root/util/moai
+
+exit 0
