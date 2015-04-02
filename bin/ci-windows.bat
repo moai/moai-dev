02b048b4f96848aef06bda4727068977269b5be1
halfnelson <dpershouse@gmail.com>
2015-04-02 17:39:50 +1000

    appveyor fixes

--- bin/ci-windows.bat
+++ bin/ci-windows.bat
@@ -5,16 +5,17 @@ echo "Setting Moai Util path..."
 pushd .
 cd %~dp0%/..
 set UTIL_PATH=%cd%/util
-popd
 
 set PATH=%PATH%;%UTIL_PATH%
 
 
 echo Compiling Windows Libs
-call build-windows.bat
+call bin\build-windows.bat
 
 echo Creating Windows Host
 mkdir hosttest
 cd hosttest
 moaiutil host init
 moaiutil host build windows
+
+popd
