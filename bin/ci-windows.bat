92a2982f2716b888eab32f4dab63cda83632a15a
halfnelson <dpershouse@gmail.com>
2015-04-02 17:55:21 +1000

    more appveyor fixes

--- bin/ci-windows.bat
+++ bin/ci-windows.bat
@@ -8,6 +8,13 @@ set UTIL_PATH=%cd%/util
 
 set PATH=%PATH%;%UTIL_PATH%
 
+if "%VS120COMNTOOLS%"=="" echo Visual Studio not found
+echo "Setting Visual Studio path..."
+
+pushd .
+call "%VS120COMNTOOLS%\VsDevCmd.bat"
+popd
+
 
 echo Compiling Windows Libs
 call bin\build-windows.bat
