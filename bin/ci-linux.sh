03550980b49aa05dca762f8cd39583717ecf1f1b
halfnelson <dpershouse@gmail.com>
2015-04-13 06:00:14 +1000

    try to make build work on travis (it exceeds 50min so chop into 2 steps)

--- bin/ci-linux.sh
+++ bin/ci-linux.sh
@@ -20,32 +20,6 @@ bash build-linux.sh
 echo Linux Build Successful
 popd
 
-if [ -z "$ANDROID_NDK" ]; then
-echo "getting ndk"
-pushd ~
-wget http://dl.google.com/android/ndk/android-ndk-r9c-linux-x86_64.tar.bz2 -O ndk.tgz
-tar -xf ndk.tgz
-export ANDROID_NDK=`pwd`/android-ndk-r9c
-popd
-fi
-
-pushd `dirname $0`
-bash build-android.sh
-echo Android Build Successful
-popd
-
-echo Creating and building android host
-sudo chmod a+x util/moai
-sudo chmod a+x util/moaiutil
-
-export MOAI_ROOT=$(pwd)
-pushd ~
-mkdir testhost
-cd testhost
-cp -R $MOAI_ROOT/samples/hello-moai src/
-moaiutil host init
-moaiutil host build android-gradle
-popd
 
 
 
