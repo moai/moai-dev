daf5727dc17c550d52b39d42ccddf547a8675e94
Patrick Meehan <patrick@ziplinegames.com>
2015-04-21 06:09:06 -0700

    oops

--- src/moai-sim/MOAIVectorEllipse.cpp
+++ src/moai-sim/MOAIVectorEllipse.cpp
@@ -24,7 +24,6 @@ int MOAIVectorEllipse::AddFillContours ( SafeTesselator* tess ) {
 	for ( u32 i = 0; i < steps; ++i, angle += step ) {
 		verts [ i ].mX = this->mLoc.mX + ( Cos ( angle ) * this->mXRad );
 		verts [ i ].mY = this->mLoc.mY + ( Sin ( angle ) * this->mYRad );
-		this->mStyle.GetDrawingToWorld ().Transform ( verts [ i ]);
 	}
 	tess->AddContour ( 2, verts, sizeof ( ZLVec2D ), steps );
 	
