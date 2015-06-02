df62a6911f891daa39dc83dc33aa764105268de4
Patrick Meehan <patrick@ziplinegames.com>
2015-06-02 15:05:05 -0700

    missing files and a few tweaks to xcode build scripts

--- xcode/ios-static/build.sh
+++ xcode/ios-static/build.sh
@@ -2,5 +2,9 @@
 
 set -e # exit on error
 
+pushd ../libmoai/ > /dev/null
+./build-ios.sh
+popd > /dev/null
+
 xcodebuild -derivedDataPath build -configuration Release -project MoaiSample.xcodeproj -scheme moai -sdk iphonesimulator
 xcodebuild -derivedDataPath build -configuration Release -project MoaiSample.xcodeproj -scheme moai -sdk iphoneos
