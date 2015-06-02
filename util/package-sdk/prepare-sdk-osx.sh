7c3c61331f7c8242ec4c947c15cc520035d981a1
Patrick Meehan <patrick@ziplinegames.com>
2015-06-02 16:11:12 -0700

    removed MOAI_SDK_SRC variable from libmoai Xcode project; tweaks to android helper scripts

--- util/package-sdk/prepare-sdk-osx.sh
+++ util/package-sdk/prepare-sdk-osx.sh
@@ -7,35 +7,26 @@ pushd $(dirname "${0}")/../../ > /dev/null
 MOAI_SDK_ROOT=$(pwd)
 echo $MOAI_SDK_ROOT
 
-#cd $MOAI_SDK_ROOT/ant/libmoai/jni
-#ndk-build
+cd $MOAI_SDK_ROOT/ant
+#./libmoai-clean.sh
+./libmoai-build.sh
 
 cd $MOAI_SDK_ROOT/xcode/libmoai
 
-./clean-ios.sh
+#./clean-ios.sh
 ./build-ios.sh
 
-./clean-osx.sh
+#./clean-osx.sh
 ./build-osx.sh
-
-#cd $MOAI_SDK_ROOT/xcode/ios
-
-#./clean.sh
-#./build.sh
 
 #cd $MOAI_SDK_ROOT/xcode/ios-static
 
 #./clean.sh
-#./build.sh
-
-cd $MOAI_SDK_ROOT/xcode/osx
-
-./clean.sh
 ./build.sh
 
 cd $MOAI_SDK_ROOT/xcode/osx-static
 
-./clean.sh
+#./clean.sh
 ./build.sh
 
 popd > /dev/null
