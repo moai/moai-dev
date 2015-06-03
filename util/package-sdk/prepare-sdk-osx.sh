0a61d8189fb7d28a0b76d89a39636996b0830985
Patrick Meehan <patrick@ziplinegames.com>
2015-06-02 18:18:42 -0700

    improved sdks packaging; improved assets sync script

--- util/package-sdk/prepare-sdk-osx.sh
+++ util/package-sdk/prepare-sdk-osx.sh
@@ -7,26 +7,12 @@ pushd $(dirname "${0}")/../../ > /dev/null
 MOAI_SDK_ROOT=$(pwd)
 echo $MOAI_SDK_ROOT
 
-cd $MOAI_SDK_ROOT/ant
+#cd $MOAI_SDK_ROOT/ant
 #./libmoai-clean.sh
-./libmoai-build.sh
+#./libmoai-build.sh
 
 cd $MOAI_SDK_ROOT/xcode/libmoai
-
-#./clean-ios.sh
 ./build-ios.sh
-
-#./clean-osx.sh
 ./build-osx.sh
-
-#cd $MOAI_SDK_ROOT/xcode/ios-static
-
-#./clean.sh
-./build.sh
-
-cd $MOAI_SDK_ROOT/xcode/osx-static
-
-#./clean.sh
-./build.sh
 
 popd > /dev/null
