b40877ecd900df363b0a77e624ffec2deb61bfc1
Conghu Zhao <conghu2001@hotmail.com>
2015-06-26 16:25:58 -0700

    upload sdk for s3

---
+++ util/upload-sdk/main.lua
@@ -0,0 +1,19 @@
+require "lfs"
+
+for i, escape, param, iter in util.iterateCommandLine ( arg or {}) do
+	
+	if param then
+
+		print()
+
+	end
+end
+
+directorypath = "./" -- current working directory
+upload = ""
+for filename in lfs.dir(directorypath) do
+    if filename:match("%.zip$") then -- "%." is an escaped ".", "$" is end of string
+        upload = filename
+    end
+end
+os.execute("s3cmd put "..upload .." s3://moai-downloads/develop" )
