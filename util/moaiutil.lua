d1dd8a5747166c1f237149610ffef0ea1530d513
Jay Vaughan <seclorum@mac.com>
2015-10-14 09:58:02 +0200

    BIGFIX: moaiutil to not explode on poke, supplement awful OSX cp with less-awful ditto

--- util/moaiutil.lua
+++ util/moaiutil.lua
@@ -10,6 +10,13 @@ MOAI_SDK_HOME	= MOAIFileSystem.getAbsoluteDirectoryPath ( arg [ 2 ])
 MOAI_CMD		= arg [ 3 ]
 SCRIPT_DIR		= string.format ( '%sutil/%s/', MOAI_SDK_HOME, MOAI_CMD )
 
+
 MOAIFileSystem.setWorkingDirectory ( SCRIPT_DIR )
 
-dofile ( 'main.lua' )
+if MOAIFileSystem.checkFileExists('main.lua') then
+	dofile ( 'main.lua' )
+else
+	print("moaiutil - utilities for creating and maintaining MOAI projects")
+	print()
+end
+
