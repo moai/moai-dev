298bf7224aa5967b4190572a7cc694cc69aa31ab
Jay Vaughan <seclorum@mac.com>
2015-10-14 10:49:13 +0200

    moaiutil toolbelt and help

--- util/host/main.lua
+++ util/host/main.lua
@@ -1,22 +1,3 @@
-print("\n\n") --give us some room
-
-function usage()
-  print([[
-    
-    Moai Host Utility. Run from project root folder (outside lua source)
-    
-    Usage: moaiutil host <subcommand> <args>
-    
-    Subcommands:
-      host list - Lists available hosts
-      host init - Creates a template host config file used by subsequent commands
-      host build <hostname> - Creates (if it doesn't exist) in hosts folder, and (re)builds the host named <hostname>. 
-      host run <hostname> - Creates (if it doesn't exists) in hosts folder, (re)builds and runs the host named <hostname>. 
-      host create <hostname> - Creates the host in the hosts folder (removing old host) based on latest config settings.
-    ]])
-  
-end
-
 hostsFolder = INVOKE_DIR..'/hosts/'
 
 --TODO probably should iterate over folders and extract this information so we don't need to keep it up to date
