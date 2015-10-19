18abc0326c36312b7dccf91cd21d696be150ff5d
Jay Vaughan <seclorum@mac.com>
2015-10-19 10:13:39 +0200

    mend

---
+++ util/environment/main.lua
@@ -0,0 +1,14 @@
+--==============================================================
+-- setup environment for MOAI and moaiutil
+--==============================================================
+local envFileName = MOAI_SDK_HOME .. "env.sh"
+local envStr = string.format ( 'export MOAI_SDK_HOME=%s\n', MOAI_SDK_HOME ) ..
+               string.format ( 'export PATH=$MOAI_SDK_HOME/util:$PATH\n' ) 
+
+envFile = io.open(envFileName, "w+")
+envFile:write(envStr)
+envFile:close()
+
+print ( "\nA localized 'env.sh' script has been created for your environment.")
+print ( "To set up your environment to use this MOAI_SDK, source the \n" .. MOAI_SDK_HOME .. "/env.sh script in your shell.\ne.g.")
+print (" \t\tsource " .. envFileName .. "\n")
