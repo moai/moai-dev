0d785f1c118362d9c8d87c34b96ae7164144a6a6
ConghuZhao <conghu@CHs-Mac.local>
2015-10-22 15:39:00 -0700

    tempfix for util/moai/Application.mk

--- util/ant-libmoai/Application.mk
+++ util/ant-libmoai/Application.mk
@@ -4,7 +4,11 @@
 # http://getmoai.com
 #================================================================#
 
-	APP_ABI 			:= @MY_ARM_ARCH@
+	#================================================================#
+	#temp fix should use line below just el capitan generate incorrect list of abi
+	#APP_ABI 			:= @MY_ARM_ARCH@  
+	#================================================================#
+	APP_ABI				:= armeabi-v7a x86
 	APP_CFLAGS			:= -w -DANDROID_NDK -DDISABLE_IMPORTGL
 	APP_PLATFORM 		:= @MY_APP_PLATFORM@
 	APP_STL 			:= gnustl_static
