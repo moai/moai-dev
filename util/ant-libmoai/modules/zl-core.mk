3e872f0492ff6d5d83924552e5af6f2560e6f3fb
Patrick Meehan <patrick@ziplinegames.com>
2015-10-23 17:35:40 -0700

    added web support to android; fixed android build

--- util/ant-libmoai/modules/zl-core.mk
+++ util/ant-libmoai/modules/zl-core.mk
@@ -4,14 +4,14 @@
 # http://getmoai.com
 #================================================================#
 
-	include $(CLEAR_VARS)
+include $(CLEAR_VARS)
 
-	LOCAL_MODULE 		:= zl-core
-	LOCAL_ARM_MODE 		:= $(MY_ARM_MODE)
-	LOCAL_CFLAGS		:= -include $(MOAI_SDK_HOME)/src/zl-vfs/zl_replace.h
-	LOCAL_CFLAGS		+= -fvisibility=hidden
+LOCAL_MODULE 		:= zl-core
+LOCAL_ARM_MODE 		:= $(MY_ARM_MODE)
+LOCAL_CFLAGS		:= -include $(MOAI_SDK_HOME)/src/zl-vfs/zl_replace.h
+LOCAL_CFLAGS		+= -fvisibility=hidden
 
-	LOCAL_C_INCLUDES 	:= $(MY_HEADER_SEARCH_PATHS)
-	LOCAL_SRC_FILES 	+= $(wildcard $(MOAI_SDK_HOME)/src/zl-util/*.cpp)
+LOCAL_C_INCLUDES 	:= $(MY_HEADER_SEARCH_PATHS)
+LOCAL_SRC_FILES 	+= $(wildcard $(MOAI_SDK_HOME)/src/zl-util/*.cpp)
 
-	include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
