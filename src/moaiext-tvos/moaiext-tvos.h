d1b09d97b1358598d6218fe354c91f9a84a7274f
Aaron Barrett <aaron@mindsnacks.com>
2015-11-03 13:29:32 -0800

    Xcode: If I check these particular boxes, then the `libmoai-tvos` target *will* compile.  Still need to get the other three working, but this is good.  Probably.

---
+++ src/moaiext-tvos/moaiext-tvos.h
@@ -0,0 +1,18 @@
+//
+//  moaiext-tvos.h
+//  libmoai
+//
+//  Created by Aaron Barrett on 11/3/15.
+//
+//
+
+
+#import <moaiext-iphone/NSArray+MOAILib.h>
+#import <moaiext-iphone/NSData+MOAILib.h>
+#import <moaiext-iphone/NSDate+MOAILib.h>
+#import <moaiext-iphone/NSDictionary+MOAILib.h>
+#import <moaiext-iphone/NSError+MOAILib.h>
+#import <moaiext-iphone/NSNumber+MOAILib.h>
+#import <moaiext-iphone/NSObject+MOAILib.h>
+#import <moaiext-iphone/NSString+MOAILib.h>
+
