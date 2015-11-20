70f384a8f8cc3346f2267e80e5bbdd4e7023886b
Patrick Meehan <patrick@ziplinegames.com>
2015-11-20 01:17:29 -0800

    fixed some compile errors; restored proper architecture selection for android (from config.lua)

--- src/moai-core/MOAILuaRuntime.h
+++ src/moai-core/MOAILuaRuntime.h
@@ -41,7 +41,7 @@ private:
 	typedef STLSet < const void* >							TraversalSet;
 	typedef TraversalSet::iterator							TraversalSetIt;
 
-	typedef STLMap < MOAILuaObject*, StringSet >			ObjectPathMap;
+	typedef STLMap < const MOAILuaObject*, StringSet >		ObjectPathMap;
 	typedef ObjectPathMap::iterator							ObjectPathMapIt;
 
 	TraversalSet		mTraversalStack; // to hold traversed objects
@@ -68,6 +68,7 @@ private:
 
 	typedef STLSet < MOAILuaObject* >						ObjectSet;
 	typedef ObjectSet::iterator								ObjectSetIt;
+	typedef ObjectSet::const_iterator						ObjectSetConstIt;
 
 	typedef STLMap < MOAILuaObject*, MOAILuaObjectInfo >	TrackingMap;
 	typedef TrackingMap::iterator							TrackingMapIt;
