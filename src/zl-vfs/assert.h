426e23555a4c5fab091f0eafb033675d35263a44
Patrick Meehan <patrick@ziplinegames.com>
2015-12-04 17:41:37 -0800

    backed out poorly conceived change to jansson config (undef of NDEBUG)

--- src/zl-vfs/assert.h
+++ src/zl-vfs/assert.h
@@ -35,7 +35,7 @@ extern jmp_buf*				zl_set_assert_jmp_buf		( jmp_buf* env );
 #undef __assert
 #undef assert
 
-#if (defined(NDEBUG) && !defined(MOAI_KEEP_ASSERT))
+#ifdef NDEBUG
 	#define __assert(x) (( void )0 )
 	#define assert(x) (( void )0 )
 #else
