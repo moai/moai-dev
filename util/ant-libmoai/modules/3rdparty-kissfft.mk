722593568c8d7842584ed98955fe62b7de186197
Conghu Zhao <conghu2001@hotmail.com>
2016-01-11 11:52:13 -0800

    android add kissfft kiss_fftr.c

--- util/ant-libmoai/modules/3rdparty-kissfft.mk
+++ util/ant-libmoai/modules/3rdparty-kissfft.mk
@@ -12,6 +12,7 @@ LOCAL_CFLAGS		+= -include $(MOAI_SDK_HOME)/src/zl-vfs/zl_replace.h
 LOCAL_CFLAGS		+= -fvisibility=hidden
 
 LOCAL_C_INCLUDES 	:= $(MY_HEADER_SEARCH_PATHS)
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/kissfft/tools/kiss_fftr.c
 LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/kissfft/kiss_fft.c
 
 include $(BUILD_STATIC_LIBRARY)
