cd552600b30cb22cbb9afb26a794ac9905b719f2
Josh Lytle <joshl@pointinside.com>
2016-03-31 15:24:38 -0700

    Fixed the header search paths in a couple of ant-libmoai modules.

--- util/ant-libmoai/modules/3rdparty-ssl.mk
+++ util/ant-libmoai/modules/3rdparty-ssl.mk
@@ -13,6 +13,9 @@ LOCAL_CFLAGS		+= -include $(MOAI_SDK_HOME)/src/zl-vfs/zl_replace.h
 LOCAL_CFLAGS		+= -fvisibility=hidden
 
 LOCAL_C_INCLUDES 	:= $(MY_HEADER_SEARCH_PATHS)
+LOCAL_C_INCLUDES 	+= '$(MOAI_SDK_HOME)/3rdparty/openssl-1.0.0m'
+LOCAL_C_INCLUDES 	+= '$(MOAI_SDK_HOME)/3rdparty/openssl-1.0.0m/crypto'
+
 LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/openssl-1.0.0m/ssl/bio_ssl.c
 LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/openssl-1.0.0m/ssl/d1_both.c
 LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/openssl-1.0.0m/ssl/d1_clnt.c
--- util/ant-libmoai/modules/moai-http-client.mk
+++ util/ant-libmoai/modules/moai-http-client.mk
@@ -11,6 +11,8 @@ LOCAL_ARM_MODE 		:= $(MY_ARM_MODE)
 LOCAL_CFLAGS		:= $(MY_LOCAL_CFLAGS) -include $(MOAI_SDK_HOME)/src/zl-vfs/zl_replace.h -fvisibility=hidden
 
 LOCAL_C_INCLUDES 	:= $(MY_HEADER_SEARCH_PATHS)
+LOCAL_C_INCLUDES 	+= '$(MOAI_SDK_HOME)/3rdparty/openssl-1.0.0m'
+
 LOCAL_SRC_FILES 	+= $(wildcard $(MOAI_SDK_HOME)/src/moai-http-client/*.cpp)
 
 include $(BUILD_STATIC_LIBRARY)
