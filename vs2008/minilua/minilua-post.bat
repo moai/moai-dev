b98e59585389290f75605f7d666d3f254d8bbdde
Patrick Meehan <pcm@fallguyconsulting.com>
2016-05-16 03:16:14 -0700

    bye-bye to vs2008 and vs2010

--- vs2008/minilua/minilua-post.bat
+++
@@ -1,6 +0,0 @@
-set minilua=%1
-set outdir=%2
-set luajit=..\..\3rdparty\luajit-2.0.1
-
-if not exist %outdir% mkdir -p %outdir%
-%minilua% %luajit%\dynasm\dynasm.lua -LN -D WIN -D JIT -D FFI -o %outdir%\buildvm_arch.h %luajit%\src\vm_x86.dasc
