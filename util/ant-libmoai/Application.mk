b2166c42393f9dcdc099702920b5466f0f70acff
JonButt <jon.butt@fallguyconsulting.com>
2016-08-09 14:10:23 -0700

    reverted c++11 flags

--- util/ant-libmoai/Application.mk
+++ util/ant-libmoai/Application.mk
@@ -7,6 +7,5 @@
 	#================================================================#
 	APP_ABI 			:= @MY_ARM_ARCH@  
 	APP_CFLAGS			:= -w -DANDROID_NDK -DDISABLE_IMPORTGL
-	APP_CPPFLAGS 		+= -std=c++11
 	APP_PLATFORM 		:= @MY_APP_PLATFORM@
 	APP_STL 			:= gnustl_static
