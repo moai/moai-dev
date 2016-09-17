edb477a335e2fad571bd6c2bdf4be5b0809508ab
halfnelson <dpershouse@gmail.com>
2016-09-18 00:22:47 +1000

    Allow javascript to call into lua

--- src/host-html/libmoai-post.js
+++ src/host-html/libmoai-post.js
@@ -8,5 +8,12 @@
       Module.SaveCallback = savefunc;
     }
 
+    Module['FS_mount'] = FS.mount;
+    Module['FS_unmount'] = FS.unmount;
+    Module['FS_mkdir'] = FS.mkdir;
+    Module['FS_filesystems'] = FS.filesystems;
+    Module['FS_chdir'] = FS.chdir;
+    Module['FS_root'] = FS.root;
+    Module['FS_lookupPath'] = FS.lookupPath;
 	return Module;	
 }
