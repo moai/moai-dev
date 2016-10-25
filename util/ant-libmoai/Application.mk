9a46ced4cd645b2a91429f5e0e4eb6fff1c503f3
Josh Lytle <joshl@pointinside.com>
2016-10-25 14:16:52 -0700

    Add support for stc limit macros to AOS NDK build
    The Android NDK build fails to properly find the std c limit macros if
    the define __STDC_LIMIT_MACROS is not declared before including stdint.h.

--- util/ant-libmoai/Application.mk
+++ util/ant-libmoai/Application.mk
@@ -5,7 +5,7 @@
 #================================================================#
 
 	APP_ABI 			    := @MY_ARM_ARCH@  
-	APP_CFLAGS			    := -w -DANDROID_NDK -DDISABLE_IMPORTGL
+	APP_CFLAGS			    := -w -DANDROID_NDK -DDISABLE_IMPORTGL -D__STDC_LIMIT_MACROS
 	APP_PLATFORM 		    := @MY_APP_PLATFORM@
 	APP_STL 			    := gnustl_static
     NDK_TOOLCHAIN_VERSION   := 4.9
