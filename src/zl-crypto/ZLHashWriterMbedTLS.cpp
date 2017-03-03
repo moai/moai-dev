70a0ed626877785bf5f07977a6633f1ee069fa17
David Pershouse <dpershouse@gmail.com>
2017-03-03 12:36:23 +1000

    fix use after free in hashwriter mbedtls fix paths in vs2015 projects

--- src/zl-crypto/ZLHashWriterMbedTLS.cpp
+++ src/zl-crypto/ZLHashWriterMbedTLS.cpp
@@ -15,7 +15,7 @@ void ZLHashWriterMbedTLS::FinalizeHash () {
 //	MD5_Final ( this->mHash, ( MD5_CTX* )this->mAlgorithm );
 	int res = mbedtls_md_finish((mbedtls_md_context_t *) this->mDigestContext, this->mHash);
 	zl_assert(0 == res);
-	mbedtls_md_free((mbedtls_md_context_t *) this->mDigestContext);
+	
 }
 
 
@@ -40,17 +40,8 @@ void ZLHashWriterMbedTLS::HashBytes ( const void* buffer, size_t size ) {
 
 //----------------------------------------------------------------//
 void ZLHashWriterMbedTLS::InitHash () {
-
-	memset ( &this->mHash, 0, sizeof ( this->mHash ));
-	memset ( this->mDigestContext, 0, sizeof ( mbedtls_md_context_t ));
-	//MD5_Init ((mbedtls_md_context_t * )this->mAlgorithm );
-
-	mbedtls_md_context_t *ctx = (mbedtls_md_context_t *) this->mDigestContext;
-	mbedtls_md_info_t *md = (mbedtls_md_info_t *) this->mDigestInfo;
-
-	mbedtls_md_init(ctx);
-	zl_assert(0 == mbedtls_md_setup(ctx, md, 0));
-	zl_assert(0 == mbedtls_md_starts(ctx));
+	int res = mbedtls_md_starts((mbedtls_md_context_t *) this->mDigestContext);
+	zl_assert(0 == res);
 }
 
 //----------------------------------------------------------------//
@@ -63,17 +54,23 @@ ZLHashWriterMbedTLS::ZLHashWriterMbedTLS(cc8 *algo) {
 	this->mDigestInfo = md;
 
 	//init storage for context
-	this->mDigestContext = (mbedtls_md_context_t *)calloc(1, sizeof(mbedtls_md_context_t));
-	
+	mbedtls_md_context_t *ctx = (mbedtls_md_context_t *)calloc(1, sizeof(mbedtls_md_context_t));
+	memset(ctx, 0, sizeof(mbedtls_md_context_t));
+	this->mDigestContext = ctx;
+
+	//init structures
+	mbedtls_md_init(ctx);
+	int res = mbedtls_md_setup(ctx, md, 0);
+	zl_assert(0 == res);
+
 	//init storage for output
-	this->mHash = new u8[mbedtls_md_get_size(md)];
-	
-		//this->mAlgorithm = ( MD5_CTX* )calloc ( 1, sizeof ( MD5_CTX ));
+	this->mHash = new u8[mbedtls_md_get_size(md)]();
 }
 
 //----------------------------------------------------------------//
 ZLHashWriterMbedTLS::~ZLHashWriterMbedTLS() {
 	this->Close ();
+	mbedtls_md_free((mbedtls_md_context_t *) this->mDigestContext);
 	delete[] this->mHash;
 	free ( this->mDigestContext );
 }
