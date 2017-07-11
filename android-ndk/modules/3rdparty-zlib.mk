3b53c98a8683a9cd31a4ab414ad342c4659a350a
Daniel Snider <danielsn@pointinside.com>
2017-07-11 16:59:35 -0700

    zlib: Update zlib build config for android

--- android-ndk/modules/3rdparty-zlib.mk
+++ android-ndk/modules/3rdparty-zlib.mk
@@ -12,20 +12,20 @@ LOCAL_CFLAGS		:= -fvisibility=hidden
 
 LOCAL_C_INCLUDES 	:= $(MY_HEADER_SEARCH_PATHS)
 
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/adler32.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/compress.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/crc32.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/deflate.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/gzclose.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/gzlib.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/gzread.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/gzwrite.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/infback.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/inffast.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/inflate.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/inftrees.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/trees.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/uncompr.c
-LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.8/zutil.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/adler32.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/compress.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/crc32.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/deflate.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/gzclose.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/gzlib.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/gzread.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/gzwrite.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/infback.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/inffast.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/inflate.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/inftrees.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/trees.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/uncompr.c
+LOCAL_SRC_FILES 	+= $(MOAI_SDK_HOME)/3rdparty/zlib-1.2.11/zutil.c
 
 include $(BUILD_STATIC_LIBRARY)
